5086fc11ee1167f8d6a27cbd332fcfe3257bfe70
pendukungms <pendukungms@gmail.com>
2021-01-05 14:16:03 +0700

    Update viewdata.php

--- mysdm/module/tgt_upload_tgt_cabthn/viewdata.php
+++ mysdm/module/tgt_upload_tgt_cabthn/viewdata.php
@@ -15,6 +15,16 @@ if ($pmodule=="caricabangregion") {
     include "../../config/koneksimysqli_ms.php";
     
     echo "<option value=''>-- Pilih --</option>";
+    
+    if (!empty($pidreg)) {
+        if ($pidreg=="B") echo "<option value='B'>Barat</option>";
+        if ($pidreg=="T") echo "<option value='T'>Timur</option>";
+    }else{
+        echo "<option value='N'>Nasional</option>";
+        echo "<option value='B'>Barat</option>";
+        echo "<option value='T'>Timur</option>";
+    }
+    
     $query = "select idcabang, nama from ms.cbgytd where aktif='Y' $nfil_reg order by nama";
     $tampil = mysqli_query($cnms, $query);
     while ($rx= mysqli_fetch_array($tampil)) {

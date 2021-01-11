616843d99dce43c53c0f48efddb54a4f7464561e
pendukungms <pendukungms@gmail.com>
2021-01-11 14:43:37 +0700

    Update logout.php

--- mysdm/logout.php
+++ mysdm/logout.php
@@ -4,6 +4,7 @@
     include 'config/koneksimysqli.php';
     mysqli_query($cnmy, "UPDATE dbmaster.sdm_users_log SET AKTIF='N' WHERE karyawanId='$lccardid' AND SESSION_ID='$_SESSION[IDSESI]'");
     mysqli_query($cnmy, "UPDATE dbmaster.sdm_users SET ONLINE='N' WHERE (karyawanId='$lccardid')");
+    mysqli_query($cnmy, "UPDATE dbmaster.sdm_users_log SET AKTIF='N' WHERE karyawanId='$lccardid' AND SESSION_ID='$_SESSION[IDSESI_VPS]'");
     //echo "<center>Anda telah sukses keluar sistem <b>[LOGOUT]<b>";
     //echo"<h3><a href='index.php'>Kembali Login</a></h3>";
     $ip      = $_SERVER['REMOTE_ADDR']; // Mendapatkan IP komputer user

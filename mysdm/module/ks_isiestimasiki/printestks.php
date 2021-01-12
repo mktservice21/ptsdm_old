2342d270dad08fdba34039854ff7115f92f854b2
pendukungms <pendukungms@gmail.com>
2021-01-12 11:03:49 +0700

    Create printestks.php

---
+++ mysdm/module/ks_isiestimasiki/printestks.php
@@ -0,0 +1,178 @@
+<?PHP
+    session_start();
+
+    if (empty($_SESSION['IDCARD']) AND empty($_SESSION['NAMALENGKAP'])){
+        echo "<center>Maaf, Anda Harus Login Ulang.<br>"; exit;
+    }
+    
+    include "config/koneksimysqli.php";
+    include "config/fungsi_sql.php";
+    include "config/library.php";
+    
+    
+    $printdate= date("d/m/Y");
+    $jamnow=date("H:i:s");
+    
+?>
+
+<HTML>
+<HEAD>
+    <title>Print Estimasi Dan Perkiraan KI <?PHP echo $printdate." ".$jamnow; ?></title>
+    <meta http-equiv="Expires" content="Mon, 01 Sep 2030 1:00:00 GMT">
+    <meta http-equiv="Pragma" content="no-cache">
+    <?php header("Cache-Control: no-cache, must-revalidate"); ?>
+    <link rel="shortcut icon" href="images/icon.ico" />
+    <script>
+        function printContent(el){
+            var restorepage = document.body.innerHTML;
+            var printcontent = document.getElementById(el).innerHTML;
+            document.body.innerHTML = printcontent;
+            window.print();
+            document.body.innerHTML = restorepage;
+        }
+    </script>
+
+    <script>
+        var EventUtil = new Object;
+        EventUtil.formatEvent = function (oEvent) {
+                return oEvent;
+        }
+
+
+        function goto2(pForm_,pPage_) {
+           document.getElementById(pForm_).action = pPage_;
+           document.getElementById(pForm_).submit();
+
+        }
+    </script>
+
+    <style>
+    @page 
+    {
+        /*size: auto;   /* auto is the current printer page size */
+        /*margin: 0mm;  /* this affects the margin in the printer settings */
+        margin-left: 7mm;  /* this affects the margin in the printer settings */
+        margin-right: 7mm;  /* this affects the margin in the printer settings */
+        margin-top: 5mm;  /* this affects the margin in the printer settings */
+        margin-bottom: 5mm;  /* this affects the margin in the printer settings */
+        size: portrait;
+    }
+    </style>
+
+    <style>
+        body {
+            font-family: "Times New Roman", Times, serif;
+            font-size: 13px;
+            border: 0px solid #000;
+        }
+        table.example_2 {
+            color: #000;
+            font-family: Helvetica, Arial, sans-serif;
+            width: 100%;
+            border-collapse:
+            collapse; border-spacing: 0;
+            font-size: 11px;
+            border: 1px solid #000;
+        }
+
+        table.example_2 td, table.example_2 th {
+            border: 1px solid #000; /* No more visible border */
+            height: 28px;
+            transition: all 0.3s;  /* Simple transition for hover effect */
+            padding: 5px;
+        }
+
+        table.example_2 th {
+            background: #DFDFDF;  /* Darken header a bit */
+            font-weight: bold;
+        }
+
+        table.example_2 td {
+            background: #FAFAFA;
+        }
+
+        /* Cells in even rows (2,4,6...) are one color */
+        tr:nth-child(even) td { background: #F1F1F1; }
+
+        /* Cells in odd rows (1,3,5...) are another (excludes header cells)  */
+        tr:nth-child(odd) td { background: #FEFEFE; }
+
+        tr td:hover.biasa { background: #666; color: #FFF; }
+        tr td:hover.left { background: #ccccff; color: #000; }
+
+        tr td.center1, td.center2 { text-align: center; }
+
+        tr td:hover.center1 { background: #666; color: #FFF; text-align: center; }
+        tr td:hover.center2 { background: #ccccff; color: #000; text-align: center; }
+        /* Hover cell effect! */
+
+        table {
+            font-family: "Times New Roman", Times, serif;
+            font-size: 11px;
+        }
+        table.tjudul {
+            font-size: 13px;
+            width: 97%;
+        }
+
+
+        #kotakjudul {
+            border: 0px solid #000;
+            width:100%;
+            height: 1.3cm;
+        }
+        #isikiri {
+            float   : left;
+            width   : 49%;
+            border-left: 0px solid #000;
+        }
+        #isikanan {
+            text-align: right;
+            float   : right;
+            width   : 49%;
+        }
+        h2 {
+            font-size: 15px;
+        }
+        h3 {
+            font-size: 20px;
+        }
+    </style>
+    
+</HEAD>
+
+<BODY>
+    
+    <center>
+        <h3>
+            <?PHP
+                echo "Estimasi Permintaan dan Perkiraan KI";
+            ?>
+        </h3>
+    </center>
+
+    <br/>&nbsp;
+    <table id='example_2' class='table table-striped table-bordered example_2' border="1px">
+        <tbody class='inputdatauc'>
+            <tr>
+                <td>Estimasi Perkiraan</td><td> : </td><td> &nbsp; </td>
+            </tr>
+            <tr>
+                <td>Estimasi Perkiraan Sales 6 Bulan</td><td> : </td><td> &nbsp; </td>
+            </tr>
+            <tr>
+                <td>Estimasi Perkiraan 12 Bulan</td><td> : </td><td> &nbsp; </td>
+            </tr>
+        </tbody>
+    </table>
+
+    
+</BODY>
+
+</HTML>
+
+<?PHP
+hapusdata:
+    
+    mysqli_close($cnmy);
+?>

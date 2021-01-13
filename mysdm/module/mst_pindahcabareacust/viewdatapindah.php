6b344e28c4d08f4b3d030d95c0f06eb9189ea1fc
pendukungms <pendukungms@gmail.com>
2021-01-13 13:19:07 +0700

    Create viewdatapindah.php

---
+++ mysdm/module/mst_pindahcabareacust/viewdatapindah.php
@@ -0,0 +1,51 @@
+<?php
+
+session_start();
+$pmodule="";
+if (isset($_GET['module'])) $pmodule=$_GET['module'];
+
+
+if ($pmodule=="viewdataareacabang") {
+    include "../../config/koneksimysqli_it.php";
+    include "../../config/fungsi_sql.php";
+    
+    $pidcabang = $_POST['ucab'];
+    
+    $pnamacabang= getfieldcnit("select nama as lcfields from MKT.icabang WHERE icabangid='$pidcabang'");
+    
+    echo "<option value='' selected>-- Pilih Area dari : $pnamacabang --</option>";
+    $query = "select icabangid as icabangid, areaid as areaid, nama as nama from MKT.iarea WHERE icabangid='$pidcabang' ";
+    $query .= " Order by nama";
+    $tampil =mysqli_query($cnit, $query);
+    while ($irow=mysqli_fetch_array($tampil)){
+        $pidarea=$irow['areaid'];
+        $pnamaarea=$irow['nama'];
+        $iidarea=(INT)$pidarea;
+        echo "<option value='$pidarea'>$pnamaarea ($iidarea)</option>";
+    }
+                                            
+    mysqli_close($cnit);
+}elseif ($pmodule=="viewdatadariareacabang") {
+    include "../../config/koneksimysqli_it.php";
+    include "../../config/fungsi_sql.php";
+    
+    $pdridcabang = $_POST['udrcab'];
+    
+    $pnamacabang= getfieldcnit("select nama as lcfields from MKT.icabang WHERE icabangid='$pdridcabang'");
+    
+    echo "<option value='' selected>-- Pilih Area dari : $pnamacabang --</option>";
+    $query = "select icabangid as icabangid, areaid as areaid, nama as nama from MKT.iarea WHERE icabangid='$pdridcabang' ";
+    $query .= " Order by nama";
+    $tampil =mysqli_query($cnit, $query);
+    while ($irow=mysqli_fetch_array($tampil)){
+        $pidarea=$irow['areaid'];
+        $pnamaarea=$irow['nama'];
+        $iidarea=(INT)$pidarea;
+        echo "<option value='$pidarea'>$pnamaarea ($iidarea)</option>";
+    }
+                                            
+    mysqli_close($cnit);
+}
+
+?>
+

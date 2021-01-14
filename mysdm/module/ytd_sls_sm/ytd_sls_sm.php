4a560761330c0a46626cbadecbb28d0ff7ef251c
pendukungms <pendukungms@gmail.com>
2021-01-14 12:06:01 +0700

    Update ytd_sls_sm.php

--- mysdm/module/ytd_sls_sm/ytd_sls_sm.php
+++ mysdm/module/ytd_sls_sm/ytd_sls_sm.php
@@ -1,3 +1,6 @@
+<?php
+    include "config/cek_akses_modul.php";
+?>
 <div class="">
 
     <div class="col-md-12 col-sm-12 col-xs-12"><div class="title_left"><h3>Report Sales YTD Per SM</h3></div></div><div class="clearfix"></div>

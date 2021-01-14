0eb5944f59e9b91dfd00704bb0ba1f5c9d21d2c3
pendukungms <pendukungms@gmail.com>
2021-01-14 16:23:24 +0700

    Update viewdatapindah.php

--- mysdm/module/mst_pindahcabareacust/viewdatapindah.php
+++ mysdm/module/mst_pindahcabareacust/viewdatapindah.php
@@ -14,7 +14,7 @@ if ($pmodule=="viewdataareacabang") {
     $pnamacabang= getfieldcnit("select nama as lcfields from MKT.icabang WHERE icabangid='$pidcabang'");
     
     echo "<option value='' selected>-- Pilih Area dari : $pnamacabang --</option>";
-    $query = "select icabangid as icabangid, areaid as areaid, nama as nama from MKT.iarea WHERE icabangid='$pidcabang' ";
+    $query = "select icabangid as icabangid, areaid as areaid, nama as nama from MKT.iarea WHERE icabangid='$pidcabang' AND IFNULL(aktif,'')='Y' ";
     $query .= " Order by nama";
     $tampil =mysqli_query($cnit, $query);
     while ($irow=mysqli_fetch_array($tampil)){
@@ -37,7 +37,7 @@ if ($pmodule=="viewdataareacabang") {
     $query = "select DISTINCT a.icabangid as icabangid, a.areaid as areaid, a.nama as nama "
             . " from MKT.iarea as a JOIN MKT.ecust as b on a.icabangid=b.icabangid AND a.areaid=b.areaid "
             . " JOIN MKT.icust as c on b.icabangid=c.icabangid AND b.areaid=c.areaid AND b.icustid=c.icustid "
-            . " WHERE a.icabangid='$pdridcabang' ";
+            . " WHERE a.icabangid='$pdridcabang' AND IFNULL(a.aktif,'')='Y' ";
     $query .= " Order by nama";
     $tampil =mysqli_query($cnit, $query);
     while ($irow=mysqli_fetch_array($tampil)){

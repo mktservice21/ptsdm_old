15c91a1753fbbaacdc327c84119423fe3497bd9b
pendukungms <pendukungms@gmail.com>
2021-01-15 09:47:14 +0700

    Update mydatacab.php

--- mysdm/module/mst_pindahcabareacust/mydatacab.php
+++ mysdm/module/mst_pindahcabareacust/mydatacab.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include "../../config/koneksimysqli.php";
+include "../../config/koneksimysqli_it.php";
 include "../../config/fungsi_sql.php";
 
 $fkaryawan=$_SESSION['IDCARD'];
@@ -30,7 +30,7 @@ $sql = "select icabangid as icabangid, nama as nama, aktif as aktif "
         . " FROM MKT.icabang ";
 $sql .=" where 1=1 ";
 
-$query=mysqli_query($cnmy, $sql) or die("mydata.php: get data");
+$query=mysqli_query($cnit, $sql) or die("mydata.php: get data");
 $totalData = mysqli_num_rows($query);
 $totalFiltered = $totalData;  // when there is no search parameter then total number rows = total number filtered rows.
 
@@ -41,11 +41,11 @@ if( !empty($requestData['search']['value']) ) {   // if there is a search parame
 }
 
 
-$query=mysqli_query($cnmy, $sql) or die("mydata.php: get data");
+$query=mysqli_query($cnit, $sql) or die("mydata.php: get data");
 $totalFiltered = mysqli_num_rows($query); // when there is a search parameter then we have to modify total number filtered rows as per search result. 
 $sql.=" ORDER BY ". $columns[$requestData['order'][0]['column']]."   ".$requestData['order'][0]['dir']."  LIMIT ".$requestData['start']." ,".$requestData['length']."   ";
 /* $requestData['order'][0]['column'] contains colmun index, $requestData['order'][0]['dir'] contains order such as asc/desc  */	
-$query=mysqli_query($cnmy, $sql) or die("mydata.php: get data");
+$query=mysqli_query($cnit, $sql) or die("mydata.php: get data");
 
 
 
@@ -82,7 +82,7 @@ $json_data = array(
 
 echo json_encode($json_data);  // send data as json format
 
-mysqli_close($cnmy);
+mysqli_close($cnit);
 
 ?>
 

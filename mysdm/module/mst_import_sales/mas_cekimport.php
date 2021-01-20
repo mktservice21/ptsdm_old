9eb3e2a2b6725ea6974a13101dd3f4e706c1c7fe
pendukungms <pendukungms@gmail.com>
2021-01-20 10:28:49 +0700

    Update mas_cekimport.php

--- mysdm/module/mst_import_sales/mas_cekimport.php
+++ mysdm/module/mst_import_sales/mas_cekimport.php
@@ -46,8 +46,8 @@ if (empty($puser)) {
     
     
     //ubah juga di prosesdata_
-    include "../../config/koneksimysqli_it.php";
-    $cnmy=$cnit;
+    include "../../config/koneksimysqli_ms.php";
+    $cnmy=$cnms;
     $dbname = "MKT";
     
     

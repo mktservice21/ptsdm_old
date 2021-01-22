adbfea9b2874f5e12d59ce50ec33beecda61e4e4
pendukungms <pendukungms@gmail.com>
2021-01-22 14:56:37 +0700

    Update bagisalesmanual.php

--- mysdm/module/map_bagisalesmanual/bagisalesmanual.php
+++ mysdm/module/map_bagisalesmanual/bagisalesmanual.php
@@ -46,6 +46,12 @@
     $pidmenu=$_GET['idmenu'];
     if (isset($_GET['act'])) $pact=$_GET['act'];
     
+    $hari_ini = date("Y-m-d");
+    if (!empty($_SESSION['MAPCUSTBAGIBULAN'])) {
+        $hari_ini = date('Y-m-d', strtotime($_SESSION['MAPCUSTBAGIBULAN']));
+    }
+    $pblnpilih = date('F Y', strtotime($hari_ini));
+    $iniharinya=date('d', strtotime($hari_ini));
     
     $piddistpl=$_SESSION['MAPCUSTBAGIDCAB'];
     $pidecabpl=$_SESSION['MAPCUSTBAGIIDARE'];
@@ -78,152 +84,14 @@
         <?php
         switch($pact){
             default:
-                ?>
-        
-                <script>
-                    function RefreshDataTabel() {
-                        KlikDataTabel();
-                    }
-
-                    $(document).ready(function() {
-                        //KlikDataTabel();
-                    } );
-
-                    function KlikDataTabel() {
-                        var ecabang=document.getElementById('cb_dist').value;
-                        var earea=document.getElementById('cb_ecabang').value;
-                        var enamafilter=document.getElementById('e_namafilter').value;
-                        
-                        var myurl = window.location;
-                        var urlku = new URL(myurl);
-                        var module = urlku.searchParams.get("module");
-                        var idmenu = urlku.searchParams.get("idmenu");
-                        var act = urlku.searchParams.get("act");
-            
-                        $("#loading").html("<center><img src='images/loading.gif' width='50px'/></center>");
-                        $.ajax({
-                            type:"post",
-                            url:"module/map_bagisalesmanual/viewdatatabelecustd.php?module="+module+"&idmenu="+idmenu+"&act="+act,
-                            data:"ucabang="+ecabang+"&uarea="+earea+"&unamafilter="+enamafilter,
-                            success:function(data){
-                                $("#c-data").html(data);
-                                $("#loading").html("");
-                            }
-                        });
-                    }
-                    
-                    function ShowDataEcabangDist() {
-                        var ecabang=document.getElementById('cb_dist').value;
-                        
-                        $.ajax({
-                            type:"post",
-                            url:"module/map_bagisalesmanual/viewdatabagi.php?module=caridataecust",
-                            data:"ucabang="+ecabang,
-                            success:function(data){
-                                $("#cb_ecabang").html(data);
-                            }
-                        });
-                    }
-                </script>
-                
-                
-                <div class='col-md-12 col-sm-12 col-xs-12'>
-                    <div class='x_panel'>
-                        
-                        <div hidden class='x_title'>
-                            <h2><input class='btn btn-default' type=button value='Tambah Baru'
-                                onclick="window.location.href='<?PHP echo "?module=$pmodule&idmenu=$pidmenu&act=tambahbaru"; ?>';">
-                                <small></small>
-                            </h2>
-                            <div class='clearfix'></div>
-                        </div>
-                        
-                        <form method='POST' action='<?PHP echo "$aksi?module=$_GET[module]&act=import&idmenu=$_GET[idmenu]"; ?>' 
-                              id='form_data1' name='form1' data-parsley-validate class='form-horizontal form-label-left'  enctype='multipart/form-data'>
-                            
-                            <div class='col-sm-2'>
-                                Distirbutor
-                                <div class="form-group">
-                                    <select class='form-control input-sm' id="cb_dist" name="cb_dist" onchange="ShowDataEcabangDist()">
-                                        <?PHP
-                                            echo "<option value=''>--Piihan--</option>";
-                                            
-                                            $query_aktif ="select distid, nama from MKT.distrib0 ";
-                                            $query_aktif .=" order by nama";
-                                            $tampil= mysqli_query($cnmy, $query_aktif);
-                                            while ($row= mysqli_fetch_array($tampil)) {
-                                                $piddis=$row['distid'];
-                                                $pnmdist=$row['nama'];
-                                                $pditint=(INT)$piddis;
-                                                if ($piddistpl==$piddis)
-                                                    echo "<option value='$piddis' selected>$pnmdist ($pditint)</option>";
-                                                else
-                                                    echo "<option value='$piddis'>$pnmdist ($pditint)</option>";
-                                            }
-                                        ?>
-                                    </select>
-                                </div>
-                            </div>
-                            
-                            <div class='col-sm-2'>
-                                Cabang
-                                <div class="form-group">
-                                    <select class='form-control input-sm' id="cb_ecabang" name="cb_ecabang" onchange="">
-                                        <?PHP
-                                        echo "<option value=''>--All--</option>";
-                                        if (!empty($piddistpl)) {
-                                            $query="SELECT distid, ecabangid, nama from MKT.ecabang where distid='$piddistpl' ";
-                                            $query .=" order by nama";
-                                            $tampil= mysqli_query($cnms, $query);
-                                            while ($row= mysqli_fetch_array($tampil)) {
-                                                $pidecab=$row['ecabangid'];
-                                                $pnmecab=$row['nama'];
-                                                if ($pidecab==$pidecabpl)
-                                                    echo "<option value='$pidecab' selected>$pnmecab ($pidecab)</option>";
-                                                else
-                                                    echo "<option value='$pidecab'>$pnmecab ($pidecab)</option>";
-                                            }
-                                        }
-                                        ?>
-                                    </select>
-                                </div>
-                            </div>
-                            
-                            <div class='col-sm-4'>
-                                No. Faktur
-                                <div class="form-group">
-                                    <input type='text' id='e_namafilter' name='e_namafilter' class='form-control col-md-7 col-xs-12' value='<?PHP echo "$pfilterpl"; ?>'>
-                                </div>
-                            </div>
-                            
-
-
-                            <div class='col-sm-2'>
-                                <small>&nbsp;</small>
-                               <div class="form-group">
-                                   <button type='button' class='btn btn-success btn-xs' onclick='KlikDataTabel()'>View Data</button>
-                               </div>
-                           </div>
-                            
-                        </form>
-                        
-                        <div id='loading'></div>
-                        <div id='c-data'>
-                           
-                        </div>
-                        
-                    </div>
-                </div>
-        
-                <?PHP
-                
+                include "tambah_bagi.php";
             break;
 
             case "tambahbaru":
-                include "tambah_custsdm.php";
+                include "tambah_bagi.php";
             break;
             case "editdata":
-                include "tambah_custsdm.php";
+                include "tambah_bagi.php";
             break;
         
         }

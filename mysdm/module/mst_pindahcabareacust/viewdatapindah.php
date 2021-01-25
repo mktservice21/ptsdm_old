3726521df554e253bda3b5b28b34ec824c5d211d
pendukungms <pendukungms@gmail.com>
2021-01-25 11:44:42 +0700

    Update viewdatapindah.php

--- mysdm/module/mst_pindahcabareacust/viewdatapindah.php
+++ mysdm/module/mst_pindahcabareacust/viewdatapindah.php
@@ -38,7 +38,8 @@ if ($pmodule=="viewdataareacabang") {
     $query = "select DISTINCT a.icabangid as icabangid, a.areaid as areaid, a.nama as nama "
             . " from MKT.iarea as a JOIN MKT.ecust as b on a.icabangid=b.icabangid AND a.areaid=b.areaid "
             . " JOIN MKT.icust as c on b.icabangid=c.icabangid AND b.areaid=c.areaid AND b.icustid=c.icustid "
-            . " WHERE a.icabangid='$pdridcabang' AND IFNULL(a.aktif,'')='Y' ";
+            . " WHERE a.icabangid='$pdridcabang'  ";
+    //$query = "select icabangid as icabangid, areaid as areaid, nama as nama from MKT.iarea WHERE icabangid='$pdridcabang'  ";
     $query .= " Order by nama";
     $tampil =mysqli_query($cnit, $query);
     while ($irow=mysqli_fetch_array($tampil)){

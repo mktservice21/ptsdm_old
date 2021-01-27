8c1ec0f1a208fcde41b7edbd27c7a34b3a075bdf
pendukungms <pendukungms@gmail.com>
2021-01-27 14:52:37 +0700

    Update viewdatatabeleformbagi.php

--- mysdm/module/map_bagisalesmanual/viewdatatabeleformbagi.php
+++ mysdm/module/map_bagisalesmanual/viewdatatabeleformbagi.php
@@ -228,8 +228,8 @@
                     </div>
                 </div>
                 
-                <div  class='form-group'>
-                    <label class='control-label col-md-3 col-sm-3 col-xs-12' for=''>*) Qty Available <span class='required'></span></label>
+                <div hidden class='form-group'>
+                    <label class='control-label col-md-3 col-sm-3 col-xs-12' for=''> Qty Available <span class='required'></span></label>
                     <div class='col-md-4'>
                         <input type='text' id='e_qtysisapil' name='e_qtysisapil' class='form-control col-md-7 col-xs-12' value='<?PHP echo $pqtysisa; ?>' Readonly>
                     </div>

e24a9ae9a3a4fbe5b6f47800f278727d163104c1
pendukungms <pendukungms@gmail.com>
2021-02-04 15:19:51 +0700

    Update pv_cekimport.php

--- mysdm/module/mst_import_sales/pv_cekimport.php
+++ mysdm/module/mst_import_sales/pv_cekimport.php
@@ -262,6 +262,8 @@ if (empty($puser)) {
 			if (empty($pfile1)) $pfile1=$pfile15;
 
     
+            echo "$pfile5, $pfile6, $pfile7, $pfile8, $pfile9, $pfile10<br/>";
+                        
             $query_parts_sls[] = "('$pfile0', '$pfile1', '$pfile2', '$pfile3', "
                     . " '$pfile4', '$pfile5', '$pfile6', '$pfile7', "
                     . " '$pfile8', '$pfile9', '$pfile10', '$pfile11', '$pfile12', '$pfile13', '$pfile14')";

e9298ab77c9c5ccf06061e507d370dca442caf6d
pendukungms <pendukungms@gmail.com>
2021-02-10 16:40:15 +0700

    Update aksi_detailrptreg.php

--- mysdm/module/sls_lapslspersektorreg/aksi_detailrptreg.php
+++ mysdm/module/sls_lapslspersektorreg/aksi_detailrptreg.php
@@ -18,6 +18,7 @@
     $pval=$_GET['qval'];
     $pjns=$_GET['jns'];
     $pncpoth=$_GET['incpoth'];
+    $piddist=$_GET['niddist'];
     
     
     
@@ -94,6 +95,13 @@
         $pnamadivisi_p=$nv['nama'];
     }
     
+    $pnmdistirbutor="";
+    $query = "select nama from MKT.distrib0 where distid='$piddist'";
+    $tampil= mysqli_query($cnms, $query);
+    $rd= mysqli_fetch_array($tampil);
+    $pnmdistirbutor=$rd['nama'];
+    if (empty($piddist)) $pnmdistirbutor="All";
+    
     
     $now=date("mdYhis");
     $puserid=$_SESSION['USERID'];
@@ -151,6 +159,7 @@
         $query .= " AND iprodid NOT IN (select IFNULL(iprodid,'') iprodid from sls.othproduk WHERE divprodid='PEACO')";
     }
     if (!empty($pidprod)) $query .= " AND iprodid='$pidprod'";
+    if (!empty($piddist)) $query .= " AND distid='$piddist' ";
     $query .=" GROUP BY icabangid, areaid, icustid, iprodid ";
     $query = "create TEMPORARY table $tmp01 ($query)"; 
     mysqli_query($cnmy, $query);
@@ -350,6 +359,7 @@
         <table class="tbljudul">
             <tr><td>Region</td><td>:</td><td><?PHP echo "$pnamaam_p"; ?></td></tr>
             <tr><td>Cabang</td><td>:</td><td><?PHP echo "$pnamacabang_p"; ?></td></tr>
+            <tr><td>Distributor</td><td>:</td><td><?PHP echo "$pnmdistirbutor"; ?></td></tr>
             <tr><td>Divisi</td><td>:</td><td><?PHP echo "$pnamadivisi_p"; ?></td></tr>
             <tr><td>Periode</td><td>:</td><td><?PHP echo "$ptgl1 s/d. $ptgl2"; ?></td></tr>
             <?PHP
@@ -376,7 +386,7 @@
         echo "<table>";
         echo "<tr>";
             echo "<td>";
-                echo "<a class='btn button1' href='eksekusi3.php?module=$pmodule&act=input&idmenu=$pidmenu&ket=excel&ipilih=$ppilih&iprd=$pprod&pper1=$iper1&iprd=$pprod&pper2=$iper2&pper2=$iper2&pcb=$icab&pregi=$iregi&idiv=$pdiv&qval=$pval&jns=$pjns&incpoth=$pncpoth' target='_blank'>EXCEL</a>";
+                echo "<a class='btn button1' href='eksekusi3.php?module=$pmodule&act=input&idmenu=$pidmenu&ket=excel&ipilih=$ppilih&iprd=$pprod&pper1=$iper1&iprd=$pprod&pper2=$iper2&pper2=$iper2&pcb=$icab&pregi=$iregi&idiv=$pdiv&qval=$pval&jns=$pjns&incpoth=$pncpoth&niddist=$piddist' target='_blank'>EXCEL</a>";
             echo "</td>";
             echo "<td>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</td>";
             echo "<td>";

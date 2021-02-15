3e1cc45f39d4520e3d2665a98a65c16fb866e376
pendukungms <pendukungms@gmail.com>
2021-02-15 13:12:35 +0700

    Update simpaneditapt.php

--- mysdm/module/ks_lihatks/simpaneditapt.php
+++ mysdm/module/ks_lihatks/simpaneditapt.php
@@ -25,6 +25,7 @@ if ($module=='lihatkseditapt' AND $act=='simpanapt')
 {
     include "../../config/koneksimysqli.php";
     
+    $pkodeid=$_POST['e_id'];
     $pidsr=$_POST['cb_karyawan'];
     $piddokter=$_POST['cb_dokter'];
     $pidtype=$_POST['txt_apttype'];
@@ -32,47 +33,67 @@ if ($module=='lihatkseditapt' AND $act=='simpanapt')
     $pbln=$_POST['txt_bulan'];
     $pidapotik=$_POST['e_apotikid'];
     
-    $myArray = explode(',', $pidtype);
-    $pidtype="";
-    foreach($myArray as $myArray){
-        $pidtype_="";
-        if ($myArray=="1") {
-            $pidtype_="'1',";
-        }else{
-            $pidtype_="'0','',";
+    if ($pidtype=="1") $pidtype="('1')";
+    else $pidtype="('0', '')";
+    
+    //echo "$pkodeid : sr : $pidsr, dokt : $piddokter, tp : $pidtype, idapt : $pidapt, bln : $pbln, $pidapotik"; exit;
+    
+    if ($pkodeid=="2") {
+        
+        $query = "UPDATE hrd.ks1 as a SET a.idapotik='$pidapotik', a.userid='$pidcard' WHERE a.srid='$pidsr' AND a.dokterid='$piddokter' AND "
+                . " IFNULL(a.idapotik,'') IN ('', '0', '0000000000') AND "
+                . " bulan = '$pbln' AND aptid ='$pidapt' AND IFNULL(apttype,'') IN $pidtype";
+        echo $query;
+        //mysqli_query($cnmy, $query);
+        //$erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; mysqli_close($cnmy); exit; }
+        
+        mysqli_close($cnmy);
+        //header('location:../../eksekusi3.php?module=simpaneditapotikks&idmenu='.$idmenu.'&act=complete');
+        
+    }elseif ($pkodeid=="1") {
+        $myArray = explode(',', $pidtype);
+        $pidtype="";
+        foreach($myArray as $myArray){
+            $pidtype_="";
+            if ($myArray=="1") {
+                $pidtype_="'1',";
+            }else{
+                $pidtype_="'0','',";
+            }
+            $pidtype .=$pidtype_;
         }
-        $pidtype .=$pidtype_;
+        if (!empty($pidtype)) $pidtype="(".substr($pidtype, 0, -1).")";
+
+        $myArray = explode(',', $pidapt);
+        $pidapt="";
+        foreach($myArray as $myArray){
+            $pidapt .="'".$myArray."',";
+        }
+        if (!empty($pidapt)) $pidapt="(".substr($pidapt, 0, -1).")";
+
+        $myArray = explode(',', $pbln);
+        $pbln="";
+        foreach($myArray as $myArray){
+            $pbln .="'".$myArray."',";
+        }
+        if (!empty($pbln)) $pbln="(".substr($pbln, 0, -1).")";
+
+        //echo "aptid : $pidapt - apttype : $pidtype - bulan : $pbln";
+
+        $query = "UPDATE hrd.ks1 as a SET a.idapotik='$pidapotik', a.userid='$pidcard' WHERE a.srid='$pidsr' AND a.dokterid='$piddokter' AND "
+                . " IFNULL(a.idapotik,'') IN ('', '0', '0000000000') AND "
+                . " bulan IN $pbln AND aptid IN $pidapt AND IFNULL(apttype,'') IN $pidtype";
+        //echo $query;
+        mysqli_query($cnmy, $query);
+        $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; mysqli_close($cnmy); exit; }
+    
+        mysqli_close($cnmy);
+        header('location:../../eksekusi3.php?module=simpaneditapotikks&idmenu='.$idmenu.'&act=complete');
+    }else{
+        mysqli_close($cnmy);
+        echo "GAGAL...";
     }
-    if (!empty($pidtype)) $pidtype="(".substr($pidtype, 0, -1).")";
     
-    $myArray = explode(',', $pidapt);
-    $pidapt="";
-    foreach($myArray as $myArray){
-        $pidapt .="'".$myArray."',";
-    }
-    if (!empty($pidapt)) $pidapt="(".substr($pidapt, 0, -1).")";
-    
-    $myArray = explode(',', $pbln);
-    $pbln="";
-    foreach($myArray as $myArray){
-        $pbln .="'".$myArray."',";
-    }
-    if (!empty($pbln)) $pbln="(".substr($pbln, 0, -1).")";
-    
-    //echo "aptid : $pidapt - apttype : $pidtype - bulan : $pbln";
-    
-    $query = "UPDATE hrd.ks1 as a SET a.idapotik='$pidapotik', a.userid='$pidcard' WHERE a.srid='$pidsr' AND a.dokterid='$piddokter' AND "
-            . " IFNULL(a.idapotik,'') IN ('', '0', '0000000000') AND "
-            . " bulan IN $pbln AND aptid IN $pidapt AND IFNULL(apttype,'') IN $pidtype";
-    //echo $query;
-    mysqli_query($cnmy, $query);
-    $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; mysqli_close($cnmy); exit; }
-    
-    
-    mysqli_close($cnmy);
-    
-    
-    header('location:../../eksekusi3.php?module=simpaneditapotikks&idmenu='.$idmenu.'&act=complete');
 }
 
 ?>

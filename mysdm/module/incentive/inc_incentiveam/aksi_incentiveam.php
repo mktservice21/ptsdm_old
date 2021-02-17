004a99a3180a22e677a6c0e36c3d19c7ce90ef76
pendukungms <pendukungms@gmail.com>
2021-02-17 16:45:52 +0700

    Update aksi_incentiveam.php

--- mysdm/module/incentive/inc_incentiveam/aksi_incentiveam.php
+++ mysdm/module/incentive/inc_incentiveam/aksi_incentiveam.php
@@ -49,7 +49,7 @@ $tmp03 ="dbtemp.tmprptincmr03_".$puser."_$now$milliseconds";
 include("config/koneksimysqli_ms.php");
 
 
-$query = "select a.karyawanid, a.divisiid, a.divisiid as divprodid, a.aktif, a.icabangid, a.areaid, b.nama as nama_cabang, c.nama as nama_area from sls.imr0 as a JOIN sls.icabang as b on a.icabangid=b.icabangid JOIN "
+$query = "select a.karyawanid, a.divisiid, a.divisiid as divprodid, a.aktif, a.icabangid, a.areaid, b.nama as nama_cabang, c.nama as nama_area from sls.ispv0 as a JOIN sls.icabang as b on a.icabangid=b.icabangid JOIN "
         . " sls.iarea as c on a.icabangid=c.icabangid AND a.areaid=c.areaid WHERE a.karyawanid='$pkaryawanid'";
 $query = "CREATE TEMPORARY TABLE $tmp03 ($query)";
 mysqli_query($cnms, $query); $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "$erropesan"; goto hapusdata; }
@@ -89,13 +89,14 @@ if (!empty($pketdivisi)) {
     $filtercab="('')";
 }
 
-$query = "select jenis, sales, target, ach, incentive from ms.incentive_mr WHERE bulan between '$pbln1' AND '$pbln2' AND karyawanid='$pkaryawanid'";
+$query = "select jenis, sales, target, ach, incentive from ms.incentive_am WHERE bulan between '$pbln1' AND '$pbln2' AND karyawanid='$pkaryawanid'";
 $query = "CREATE TEMPORARY TABLE $tmp01 ($query)";
 mysqli_query($cnms, $query);
 $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "$erropesan"; goto hapusdata; }
 
 $query = "select a.groupp, a.divprodid, a.iprodid, b.nama as nama_produk, 
-        sum(a.qty_sales) as qty_sales, sum(a.value_sales) as value_sales, sum(a.qty_target) as qty_target, sum(a.value_target) as value_target from ms.sales_mr as a JOIN sls.iproduk as b on a.iprodid=b.iprodid
+        sum(a.qty_sales) as qty_sales, sum(a.value_sales) as value_sales, sum(a.qty_target) as qty_target, sum(a.value_target) as value_target 
+        from ms.sales_am as a JOIN sls.iproduk as b on a.iprodid=b.iprodid 
         WHERE a.bulan between '$pbln1' AND '$pbln2' ";
 $query .= " AND a.karyawanid='$pkaryawanid'";
 $query .= " GROUP BY 1,2,3,4";

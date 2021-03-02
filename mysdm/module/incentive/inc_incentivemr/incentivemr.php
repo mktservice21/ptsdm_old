e79131e9344ebd31a83533502f2141dc1ad18efd
myhuspan <huspannasrulloh@gmail.com>
2021-03-02 10:38:12 +0700

    Update incentivemr.php

--- mysdm/module/incentive/inc_incentivemr/incentivemr.php
+++ mysdm/module/incentive/inc_incentivemr/incentivemr.php
@@ -18,6 +18,7 @@
                 $hari_ini = date("Y-m-d");
                 //$tgl_pertama=date('F Y', strtotime('-1 month', strtotime($hari_ini)));
                 $tgl_pertama = date('F Y', strtotime($hari_ini));
+                $pbulan= date('Y-m', strtotime($hari_ini));
                 ?>
                 <script>
                     function disp_confirm(pText)  {
@@ -37,6 +38,45 @@
                         }
                     }
                 </script>
+
+                <script type="text/javascript">
+                    $(document).ready(function() {
+                        $('#cbln01').on('change dp.change', function(e){
+                            showDataKaryawan();
+                        });
+                    });
+
+                    function showDataKaryawan() {
+                        var ibulan = document.getElementById('cbln01').value;
+                        $.ajax({
+                            type:"post",
+                            url:"module/incentive/viewdatainc.php?module=viewdatamridkary",
+                            data:"ubulan="+ibulan,
+                            beforeSend: function () {
+                                //$('select[name=cb_karyawan]').attr('disabled',true)
+                                $('select[name=cb_karyawan]').empty();
+                                $('select[name=cb_karyawan]').append('<option value="loading" disabled selected>Loading...</option>');
+                            },
+                            success:function(data){
+                                $("#cb_karyawan").html(data);
+                            },
+                            complete: function () {
+                                //$('select[name=cb_karyawan]').attr('disabled',false)
+                                $('select[name=cb_karyawan] option[value="loading"]').text('');
+                            },
+                            error: function () {
+                                //$('select[name=cb_karyawan]').attr('disabled',false)
+                                $('select[name=cb_karyawan] option[value="loading"]').text('');
+                                alert('Something wrong. Try Again!')                
+                            }
+                        });
+                    }
+                    
+                    $(document).ready(function() {
+                        showDataKaryawan();
+                    } );
+                </script>
+
                 <div class='col-md-12 col-sm-12 col-xs-12'>
                     <div class='x_panel'>
                         <form name='form1' id='form1' method='POST' action="<?PHP echo "$aksi?module=$_GET[module]&act=input&idmenu=$_GET[idmenu]"; ?>" enctype='multipart/form-data' target="_blank">
@@ -59,7 +99,7 @@
                                             <label class='control-label col-md-3 col-sm-3 col-xs-12' for='cbln01'>Bulan <span class='required'></span></label>
                                             <div class='col-md-8'>
                                                 <div class="form-group">
-                                                    <div class='input-group date' id='cbln01'>
+                                                    <div class='input-group date' id=''>
                                                         <input type='text' id='cbln01' name='bulan' required='required' class='form-control' placeholder='tgl awal' value='<?PHP echo $tgl_pertama; ?>' placeholder='dd mmm yyyy' Readonly>
                                                         <span class="input-group-addon">
                                                            <span class="glyphicon glyphicon-calendar"></span>
@@ -75,58 +115,7 @@
                                             <div class='col-md-9 col-sm-9 col-xs-12'>
                                                 <select class='form-control' name='cb_karyawan' id='cb_karyawan' onchange="">
                                                     <?PHP
-                                                    if ($pidjabatan=="15") {
-                                                        $query_data = "select b.karyawanid as karyawanid, b.nama as nama from ms.karyawan as b WHERE b.karyawanid='$pidkaryawan' ";
-                                                    }elseif ($pidjabatan=="18" OR $pidjabatan=="10") {
-                                                        $query_data = "select distinct a.karyawanid as karyawanid, b.nama as nama 
-                                                                from sls.imr0 as a 
-                                                                JOIN sls.ispv0 as c on a.icabangid=c.icabangid and a.areaid=c.areaid and a.divisiid=c.divisiid join ms.karyawan as b 
-                                                                on a.karyawanid=b.karyawanId WHERE c.karyawanid='$pidkaryawan'";
-                                                    }elseif ($pidjabatan=="08") {
-                                                        $query_data = "select distinct a.karyawanid as karyawanid, b.nama as nama 
-                                                                from sls.imr0 as a 
-                                                                JOIN sls.idm0 as c on a.icabangid=c.icabangid join ms.karyawan as b 
-                                                                on a.karyawanid=b.karyawanId WHERE c.karyawanid='$pidkaryawan'";
-                                                    }elseif ($pidjabatan=="20") {
-                                                        $query_data = "select distinct a.karyawanid as karyawanid, b.nama as nama 
-                                                                from sls.imr0 as a 
-                                                                JOIN sls.ism0 as c on a.icabangid=c.icabangid join ms.karyawan as b 
-                                                                on a.karyawanid=b.karyawanId WHERE c.karyawanid='$pidkaryawan'";
-                                                    }else{
-                                                        if ($pidkaryawan=="0000000158") {
-                                                            $query_data = "select distinct a.karyawanid as karyawanid, b.nama as nama 
-                                                                from sls.imr0 as a join ms.karyawan as b on a.karyawanid=b.karyawanid 
-                                                                JOIN sls.icabang as c on a.icabangid=c.icabangid WHERE c.region='B'";
-                                                        }elseif ($pidkaryawan=="0000000159") {
-                                                            $query_data = "select distinct a.karyawanid as karyawanid, b.nama as nama 
-                                                                from sls.imr0 as a join ms.karyawan as b on a.karyawanid=b.karyawanid 
-                                                                JOIN sls.icabang as c on a.icabangid=c.icabangid WHERE c.region='T'";
-                                                        }else{
-                                                            echo "<option value='' selected>--Pilih--</option>";
-                                                            if ($pidgroup=="1" OR $pidgroup=="24") {
-                                                                $query_data = "select DISTINCT a.karyawanid as karyawanid, b.nama as nama "
-                                                                        . " from sls.imr0 as a JOIN ms.karyawan as b on a.karyawanid=b.karyawanid";
-                                                            }
-                                                        }
-                                                    }
-                                                    if (!empty($query_data)) {
-                                                        
-                                                        $query =$query_data." ORDER BY b.nama";
-                                                        $tampil = mysqli_query($cnms, $query);
-                                                        $ketemu=mysqli_num_rows($tampil);
-                                                        
-                                                        if ((INT)$ketemu<=0) echo "<option value=''>_blank</option>";
-                                                        
-                                                        while ($rx= mysqli_fetch_array($tampil)) {
-                                                            $nidkry=$rx['karyawanid'];
-                                                            $nnmkry=$rx['nama'];
-                                                            if ($nidkry==$pidkaryawan)
-                                                                echo "<option value='$nidkry' selected>$nnmkry</option>";
-                                                            else
-                                                                echo "<option value='$nidkry'>$nnmkry</option>";
-                                                        }
-                                                        
-                                                    }
+                                                    
                                                     ?>
                                                 </select>
                                             </div>

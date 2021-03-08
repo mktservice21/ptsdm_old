9af6d01421b7dc540ca0f5b27549d11c7760ca21
myhuspan <huspannasrulloh@gmail.com>
2021-03-08 12:16:32 +0700

    Update aksi_incentivelap.php

--- mysdm/module/incentive/inc_incentivelap/aksi_incentivelap.php
+++ mysdm/module/incentive/inc_incentivelap/aksi_incentivelap.php
@@ -26,10 +26,10 @@ if ($ppilihrpt=="excel") {
     header("Content-Disposition: attachment; filename=LAPORAN INCENTIVE.xls");
 }
 
-$module=$_GET['module'];
-$act=$_GET['act'];
-$idmenu=$_GET['idmenu'];
-
+$pmodule=$_GET['module'];
+$pact=$_GET['act'];
+$pidmenu=$_GET['idmenu'];
+$aksi="module/incentive/inc_incentivelap/simpan_approveinc";
 
 $pviewdate=date("d/m/Y H:i:s");
 
@@ -70,6 +70,7 @@ if ($pidgroup=="1" OR $pidgroup=="24") {
         $query .= " AND gsm='$pmyidcard' ";
     }
 }
+if (!empty($pregion)) $query .= " AND region='$pregion' ";
 mysqli_query($cnms, $query); $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "$erropesan"; goto hapusdata; }
 
 // AM / SPV
@@ -87,6 +88,7 @@ if ($pidgroup=="1" OR $pidgroup=="24") {
         $query .= " AND gsm='$pmyidcard' ";
     }
 }
+if (!empty($pregion)) $query .= " AND region='$pregion' ";
 mysqli_query($cnms, $query); $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "$erropesan"; goto hapusdata; }
 
 // DM
@@ -104,6 +106,7 @@ if ($pidgroup=="1" OR $pidgroup=="24") {
         $query .= " AND gsm='$pmyidcard' ";
     }
 }
+if (!empty($pregion)) $query .= " AND region='$pregion' ";
 mysqli_query($cnms, $query); $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "$erropesan"; goto hapusdata; }
 
 
@@ -147,6 +150,28 @@ $query = "CREATE TEMPORARY TABLE $tmp03 ($query)";
 mysqli_query($cnms, $query);
 $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "$erropesan"; goto hapusdata; }
 
+$query = "DROP TEMPORARY TABLE $tmp02";
+mysqli_query($cnms, $query); $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "$erropesan"; goto hapusdata; }
+
+//cari data sales
+$query = "select b.region, a.icabangid, b.nama as nama_cabang, sum(a.value_sales) as sales, CAST(0 as DECIMAL(20,2)) as incentive  
+    from sls.sales as a JOIN sls.icabang as b on a.icabangid=b.icabangid 
+    WHERE a.bulan between '$pbln1' AND '$pbln2' ";
+if (!empty($pregion)) {
+    $query .= " AND b.region='$pregion' ";
+}
+$query .= " GROUP BY 1,2,3";
+$query = "CREATE TEMPORARY TABLE $tmp02 ($query)";
+mysqli_query($cnms, $query); $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "$erropesan"; goto hapusdata; }
+
+$query = "UPDATE $tmp02 as a JOIN 
+    (select icabangid, sum(incentive) as incentive from $tmp03 GROUP BY 1) as b 
+    on a.icabangid=b.icabangid SET a.incentive=b.incentive";
+mysqli_query($cnms, $query); $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "$erropesan"; goto hapusdata; }
+
+
+$query = "DELETE $tmp02 FROM $tmp02 JOIN $tmp03 on $tmp02.icabangid=$tmp03.icabangid WHERE IFNULL($tmp03.icabangid,'')=''";
+mysqli_query($cnms, $query); $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "$erropesan"; goto hapusdata; }
 
 ?>
 
@@ -210,15 +235,16 @@ $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "$erropesan"; g
                 if ($ppilihrpt=="excel") {
                     echo "<tr><td colspan=5 width='150px'><b>Laporan Incentive</b></td></tr>";
                     echo "<tr><td colspan=5 width='150px'><b>Bulan : $pbulan</b></td></tr>";
-                    echo "<tr><td colspan=5 width='150px'><b>MR : $pnamaregion</b></td></tr>";
+                    echo "<tr><td colspan=5 width='150px'><b>Region : $pnamaregion</b></td></tr>";
                     
                     echo "<tr><td colspan=5 width='150px'>view date : $pviewdate</td></tr>";
                 }else{
                     echo "<tr><td width='150px'><b><h3>Laporan Incentive</h3></b></td></tr>";
                     echo "<tr><td width='150px'><b>Bulan : $pbulan</b></td></tr>";
-                    echo "<tr><td width='150px'><b>MR : $pnamaregion</b></td></tr>";
+                    echo "<tr><td width='150px'><b>Region : $pnamaregion</b></td></tr>";
                     
                     echo "<tr><td width='150px'><i>view date : $pviewdate</i></td></tr>";
+                    echo "<tr><td width='150px'>Notes : Untuk approve silakan scroll ke paling bawah...</td></tr>";
                 }
                 ?>
             </table>
@@ -231,7 +257,7 @@ $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "$erropesan"; g
     <div class="clearfix"></div>
 
     <hr/>
-    <div class="ijudul"><h2>Cabang</h2></div>
+    <div class="ijudul"><h2>Incentive Per Cabang</h2></div>
     <table id='mydatatable1' class='table table-striped table-bordered' width="100%" border="1px solid black">
         <thead class="header" id="myHeader">
             <tr>
@@ -246,6 +272,8 @@ $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "$erropesan"; g
         <tbody>
             <?PHP
             $pidno=1;
+            $ptotslsregion=0;
+            $ptotincregion=0;
             $query = "select distinct region from $tmp03 Order by region";
             $tampil0= mysqli_query($cnms, $query);
             while ($row0= mysqli_fetch_array($tampil0)) {
@@ -262,7 +290,10 @@ $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "$erropesan"; g
                 echo "<td nowrap align='right'>&nbsp;</td>";
                 echo "</tr>";
 
-                $query = "select icabangid, nama_cabang, SUM(sales) as sales, sum(incentive) as incentive FROM $tmp03 WHERE region='$nregion' ";
+                $ptotslscab=0;
+                $ptotinccab=0;
+
+                $query = "select icabangid, nama_cabang, SUM(sales) as sales, sum(incentive) as incentive FROM $tmp02 WHERE region='$nregion' ";
                 $query .=" GROUP BY 1,2";
                 $query .=" ORDER BY region, nama_cabang";
                 $tampil= mysqli_query($cnms, $query);
@@ -271,6 +302,12 @@ $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "$erropesan"; g
                     $nicabangnm=$row['nama_cabang'];
                     $nsales=$row['sales'];
                     $nincentive=$row['incentive'];
+
+                    $ptotslsregion=(DOUBLE)$ptotslsregion+(DOUBLE)$nsales;
+                    $ptotincregion=(DOUBLE)$ptotincregion+(DOUBLE)$nincentive;
+
+                    $ptotslscab=(DOUBLE)$ptotslscab+(DOUBLE)$nsales;
+                    $ptotinccab=(DOUBLE)$ptotinccab+(DOUBLE)$nincentive;
 
                     $nratio=0;
                     if ($nsales<>"0") {
@@ -308,6 +345,8 @@ $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "$erropesan"; g
                         echo "</tr>";
 
                         $precno=1;
+                        $ptotsaleskry=0;
+                        $ptotinckry=0;
 
                         $query = "select karyawanid, nama_karyawan, sts, tglmasuk, sum(incentive) as incentive FROM $tmp03 WHERE region='$nregion' AND icabangid='$nicabangid' ";
                         $query .=" GROUP BY 1,2,3,4";
@@ -320,7 +359,10 @@ $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "$erropesan"; g
                             $ptglmasuk=$row2['tglmasuk'];
                             $pincentive=$row2['incentive'];
 
+                            $ptotinckry=(DOUBLE)$ptotinckry+(DOUBLE)$pincentive;
+
                             $pincentive=number_format($pincentive,0,",",",");
+                            
 
                             echo "<tr>";
                             echo "<td nowrap>$precno</td>";
@@ -334,6 +376,16 @@ $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "$erropesan"; g
                             
                         }
 
+                        $ptotinckry=number_format($ptotinckry,0,",",",");
+                        
+                        echo "<tr style='font-weight:bold;'>";
+                        echo "<td nowrap>&nbsp;</td>";
+                        echo "<td nowrap>Total : </td>";
+                        echo "<td nowrap>&nbsp;</td>";
+                        echo "<td nowrap>&nbsp;</td>";
+                        echo "<td nowrap align='right'>$ptotinckry</td>";
+                        echo "</tr>";
+
                         echo "</table>";
 
                     echo "</td>";
@@ -342,14 +394,226 @@ $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "$erropesan"; g
                     $pidno++;
                 }
 
+                $nratio=0;
+                if ($ptotslscab<>"0") {
+                    $nratio=ROUND($ptotinccab/$ptotslscab*100,2);
+                }
+
+                $ptotslscab=number_format($ptotslscab,0,",",",");
+                $ptotinccab=number_format($ptotinccab,0,",",",");
+
+                echo "<tr style='font-weight:bold;'>";
+                echo "<td nowrap>&nbsp;</td>";
+                echo "<td nowrap>Tota $nnmregion : </td>";
+                echo "<td nowrap>&nbsp;</td>";
+                echo "<td nowrap align='right'>$ptotslscab</td>";
+                echo "<td nowrap align='right'>$ptotinccab</td>";
+                echo "<td nowrap align='right'>$nratio</td>";
+                echo "</tr>";
+
+
+
             }
+
+            if (empty($pregion)) {
+                $nratio=0;
+                if ($ptotslsregion<>"0") {
+                    $nratio=ROUND($ptotincregion/$ptotslsregion*100,2);
+                }
+
+                $ptotslsregion=number_format($ptotslsregion,0,",",",");
+                $ptotincregion=number_format($ptotincregion,0,",",",");
+
+                echo "<tr style='font-weight:bold;'>";
+                echo "<td nowrap>&nbsp;</td>";
+                echo "<td nowrap>Grand Total : </td>";
+                echo "<td nowrap>&nbsp;</td>";
+                echo "<td nowrap align='right'>$ptotslsregion</td>";
+                echo "<td nowrap align='right'>$ptotincregion</td>";
+                echo "<td nowrap align='right'>$nratio</td>";
+                echo "</tr>";
+            }
+
             ?>
         </tbody>
     </table>
     
-    
+
+    <?PHP
+    $notbl=3;
+    $no=1;
+    $query = "select distinct sts FROM $tmp03 order by sts";
+    $tampil0= mysqli_query($cnms, $query);
+    while ($row0= mysqli_fetch_array($tampil0)) {
+        $njabatan=$row0['sts'];
+
+        $pnamatabel="mydatatable".$notbl;
+    ?>
+
+        <hr/>
+        <div class="ijudul"><h2>Incentive <?PHP echo $njabatan; ?></h2></div>
+        <table id='<?PHP echo $pnamatabel; ?>' class='table table-striped table-bordered' width="100%" border="1px solid black">
+            <thead class="header" id="myHeader">
+                <tr>
+                    <th>No</th>
+                    <th>Karyawan Id</th>
+                    <th>Nama</th>
+                    <th>Incentive</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?PHP
+                $ptotslskry=0;
+                $ptotinckry=0;
+                $no=1;
+                $query = "select karyawanid, nama_karyawan, SUM(sales) as sales, sum(incentive) as incentive FROM $tmp03 WHERE sts='$njabatan' ";
+                $query .=" GROUP BY 1,2";
+                $query .=" ORDER BY nama_karyawan, karyawanid";
+                $tampil= mysqli_query($cnms, $query);
+                while ($row= mysqli_fetch_array($tampil)) {
+
+                    $nkryid=$row['karyawanid'];
+                    $nkrynm=$row['nama_karyawan'];
+                    $nsales=$row['sales'];
+                    $nincentive=$row['incentive'];
+
+                    $ptotslskry=(DOUBLE)$ptotslskry+(DOUBLE)$nsales;
+                    $ptotinckry=(DOUBLE)$ptotinckry+(DOUBLE)$nincentive;
+
+                    $nratio=0;
+                    if ($nsales<>"0") {
+                        $nratio=ROUND($nincentive/$nsales*100,2);
+                    }
+                    
+                    $nsales=number_format($nsales,0,",",",");
+                    $nincentive=number_format($nincentive,0,",",",");
+
+
+                    echo "<tr>";
+                    echo "<td nowrap>$no</td>";
+                    echo "<td nowrap>$nkryid</td>";
+                    echo "<td nowrap>$nkrynm</td>";
+                    //echo "<td nowrap align='right'>$nsales</td>";
+                    echo "<td nowrap align='right'>$nincentive</td>";
+                    //echo "<td nowrap align='right'>$nratio</td>";
+                    echo "</tr>";
+
+                    $no++;
+
+                }
+
+                $nratio=0;
+                if ($ptotslskry<>"0") {
+                    $nratio=ROUND($ptotinckry/$ptotslskry*100,2);
+                }
+
+                $ptotslskry=number_format($ptotslskry,0,",",",");
+                $ptotinckry=number_format($ptotinckry,0,",",",");
+
+                echo "<tr style='font-weight:bold;'>";
+                echo "<td nowrap>&nbsp;</td>";
+                echo "<td nowrap>Total $njabatan : </td>";
+                echo "<td nowrap>&nbsp;</td>";
+                //echo "<td nowrap align='right'>$ptotslskry</td>";
+                echo "<td nowrap align='right'>$ptotinckry</td>";
+                //echo "<td nowrap align='right'>$nratio</td>";
+                echo "</tr>";
+
+                ?>
+            </tbody>
+        </table>
+
+    <?PHP
+        $notbl++;
+    }
+    ?>
+
+
+    <hr/>
+    <div class="ijudul"><h2>Incentive Jabatan</h2></div>
+    <table id='mydatatable6' class='table table-striped table-bordered' width="100%" border="1px solid black">
+        <thead class="header" id="myHeader">
+            <tr>
+                <th>No</th>
+                <th>Jabatan</th>
+                <th>Incentive</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?PHP
+            $no=1;
+            $ptotslssts=0;
+            $ptotincsts=0;
+            $query = "select sts, sum(incentive) as incentive from $tmp03 ";
+            $query .=" GROUP BY 1";
+            $query .=" ORDER BY 1";
+            $tampil0= mysqli_query($cnms, $query);
+            while ($row0= mysqli_fetch_array($tampil0)) {
+                $njabatan=$row0['sts'];
+
+                $nincentive=$row0['incentive'];
+                $ptotincsts=(DOUBLE)$ptotincsts+(DOUBLE)$nincentive;
+
+                $nincentive=number_format($nincentive,0,",",",");
+
+
+                echo "<tr>";
+                echo "<td nowrap>$no</td>";
+                echo "<td nowrap>$njabatan</td>";
+                //echo "<td nowrap align='right'>$nsales</td>";
+                echo "<td nowrap align='right'>$nincentive</td>";
+                //echo "<td nowrap align='right'>$nratio</td>";
+                echo "</tr>";
+
+                $no++;
+            }
+
+            $ptotincsts=number_format($ptotincsts,0,",",",");
+
+            echo "<tr style='font-weight:bold;'>";
+            echo "<td nowrap>&nbsp;</td>";
+            echo "<td nowrap>Total : </td>";
+            //echo "<td nowrap align='right'>$ptotslskry</td>";
+            echo "<td nowrap align='right'>$ptotincsts</td>";
+            //echo "<td nowrap align='right'>$nratio</td>";
+            echo "</tr>";
+            ?>
+        </tbody>
+    </table>
 
     
+    <div class='col-md-12 col-sm-12 col-xs-12'>
+        <div class='x_panel'>
+            <?PHP
+            ?>
+            <form name='form_data1' id='form_data1' method='POST' 
+                action="<?PHP echo "$aksi?module=$pmodule&act=input&idmenu=$pidmenu"; ?>" 
+                enctype='multipart/form-data' target="_blank">
+
+                <table>
+                    <tr>
+                        <td nowrap>Bulan </td>
+                        <td nowrap> : </td>
+                        <td><input type='text' id='e_bulan' name='e_bulan' required='required' class='form-control' placeholder='tgl awal' value='<?PHP echo $tgl01; ?>' placeholder='dd mmm yyyy' Readonly></td>
+                    </tr>
+
+                    <tr>
+                        <td nowrap>Region </td>
+                        <td nowrap> : </td>
+                        <td>
+                            <input type='text' id='e_region' name='e_region' required='required' class='form-control' value='<?PHP echo $pregion; ?>' Readonly>
+                        </td>
+                    </tr>
+
+                </table>
+                
+            </form>
+
+        </div>
+    </div>
+
+    <br/><br/><br/><br/>
+    <br/><br/><br/><br/>
     <br/><br/><br/><br/>
     
     
@@ -458,14 +722,14 @@ $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "$erropesan"; g
             .divnone {
                 display: none;
             }
-            #mydatatable1, #mydatatable2, #mydatatable3, #mydatatable4, #mydatatable5 {
+            #mydatatable1, #mydatatable2, #mydatatable3, #mydatatable4, #mydatatable5, #mydatatable6 {
                 color:#000;
                 font-family: "Arial";
             }
-            #mydatatable1 th, #mydatatable2 th, #mydatatable3 th, #mydatatable4 th, #mydatatable5 th {
+            #mydatatable1 th, #mydatatable2 th, #mydatatable3 th, #mydatatable4 th, #mydatatable5 th, #mydatatable6 th {
                 font-size: 12px;
             }
-            #mydatatable1 td, #mydatatable2 td, #mydatatable3 td, #mydatatable4 td, #mydatatable5 td { 
+            #mydatatable1 td, #mydatatable2 td, #mydatatable3 td, #mydatatable4 td, #mydatatable5 td, #mydatatable6 td { 
                 font-size: 14px;
             }
 
@@ -510,7 +774,23 @@ $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "$erropesan"; g
         }
         // END SCROLL
     
-    
+        var table13 = $('#mydatatable3, #mydatatable4, #mydatatable5').DataTable({
+            fixedHeader: true,
+            "ordering": false,
+            "lengthMenu": [[10, 50, 100, -1], [10, 50, 100, "All"]],
+            "displayLength": -1,
+            "order": [[ 0, "asc" ]],
+            "columnDefs": [
+                { "visible": false },
+                { className: "text-right", "targets": [3] },//right
+                { className: "text-nowrap", "targets": [0,1,2,3] }//nowrap
+
+            ],
+            bFilter: true, bInfo: true, "bLengthChange": false, "bLengthChange": true,
+            "bPaginate": false
+        } );
+
+
         $(document).ready(function() {
             
 

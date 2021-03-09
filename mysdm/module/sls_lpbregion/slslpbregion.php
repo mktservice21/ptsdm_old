aa04411b1d42c0ae4f78043a363fc6c98d97808c
myhuspan <huspannasrulloh@gmail.com>
2021-03-09 16:23:32 +0700

    Update slslpbregion.php

--- mysdm/module/sls_lpbregion/slslpbregion.php
+++ mysdm/module/sls_lpbregion/slslpbregion.php
@@ -1,3 +1,6 @@
+<?PHP
+include "config/cek_akses_modul.php";
+?>
 <div class="">
 
     <div class="col-md-12 col-sm-12 col-xs-12"><div class="title_left"><h3>Report LPB Per Region</h3></div></div><div class="clearfix"></div>

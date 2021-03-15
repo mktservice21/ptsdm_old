1f19a43c36a0946315ed3a0799d1d2117aa5c428
myhuspan <huspannasrulloh@gmail.com>
2021-03-15 14:47:46 +0700

    Update printdataall.php

--- mysdm/module/mod_br_admentryklaim/printdataall.php
+++ mysdm/module/mod_br_admentryklaim/printdataall.php
@@ -159,7 +159,7 @@
             $pjenisklaim=$row['jenisklaim'];
             $pnamajenisklm="";
             if ($pjenisklaim=="S") $pnamajenisklm="SDM ONLINE";
-            elseif ($pjenisklaim=="D") $pnamajenisklm="DISTRIBUTOR ONLINE";
+            elseif ($pjenisklaim=="D") $pnamajenisklm="SKS ONLINE";
 
 
             $pbln=$row['bulan'];

efb48f66e6746adfbcda1388f2942cfefb04868f
myhuspan <huspannasrulloh@gmail.com>
2021-03-24 15:52:59 +0700

    Create viewdatatabeleplan.php

---
+++ mysdm/module/dkd/dkd_wekvisitplan/viewdatatabeleplan.php
@@ -0,0 +1,193 @@
+<?PHP
+    date_default_timezone_set('Asia/Jakarta');
+    ini_set("memory_limit","512M");
+    ini_set('max_execution_time', 0);
+    
+    session_start();
+    
+    $puserid="";
+    if (isset($_SESSION['USERID'])) $puserid=$_SESSION['USERID'];
+
+    if (empty($puserid)) {
+        echo "ANDA HARUS LOGIN ULANG...";
+        exit;
+    }
+
+    $pkaryawanid=$_SESSION['IDCARD'];
+
+    $ptgl1=$_POST['utgl1'];
+    $ptgl2=$_POST['utgl2'];
+
+    $ptgl1 = date('Y-m-d', strtotime($ptgl1));
+    $ptgl2 = date('Y-m-d', strtotime($ptgl2));
+
+    $pmodule=$_GET['module'];
+    $pact=$_GET['act'];
+    $pidmenu=$_GET['idmenu'];
+
+    include "../../../config/koneksimysqli.php";
+
+    $now=date("mdYhis");
+    $tmp01 =" dbtemp.tmpdkdentry01_".$puserid."_$now ";
+    $tmp02 =" dbtemp.tmpdkdentry02_".$puserid."_$now ";
+
+    $sql = "select a.idinput, a.tanggal, a.karyawanid, a.ketid, c.nama as nama_ket,
+        a.compl, a.aktivitas, b.jenis, b.dokterid, d.namalengkap as nama_dokter, b.notes, b.saran 
+        from hrd.dkd_new0 as a left join hrd.dkd_new1 as b on a.idinput=b.idinput 
+        LEFT JOIN hrd.ket as c on a.ketid=c.ketId
+        LEFT JOIN dr.masterdokter as d on b.dokterid=d.id WHERE a.karyawanid='$pkaryawanid' ";
+    $sql .=" AND a.tanggal between '$ptgl1' AND '$ptgl2'";
+    $query = "create TEMPORARY table $tmp01 ($sql)"; 
+    mysqli_query($cnmy, $query);
+    $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; goto hapusdata; }
+
+
+?>
+
+
+<form method='POST' action='<?PHP echo "?module='$pmodule'&act=$pact&idmenu=$pidmenu"; ?>' 
+    id='d-form2' name='form2' data-parsley-validate class='form-horizontal form-label-left'>
+
+    <div class='x_content'>
+        <table id='datatable' class='datatable table nowrap table-striped table-bordered' width="100%">
+            <thead>
+                <tr>
+                    <th width='50px'>No</th>
+                    <th width='50px'>&nbsp;</th>
+                    <th width='100px'>Tanggal</th>
+                    <th width='100px'>Keperluan</th>
+                    <th width='100px'>Compl.</th>
+                    <th width='50px'>Aktivitas</th>
+                    <th width='80px'>Jenis</th>
+                    <th width='100px'>Dokter</th>
+                    <th width='100px'>Notes</th>
+                    <th width='100px'>Saran</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?PHP
+            $no=1;
+            $query = "select * from $tmp01 order by tanggal, jenis, nama_dokter";
+            $tampil=mysqli_query($cnmy, $query);
+            while ($row=mysqli_fetch_array($tampil)) {
+                $ntgl=$row['tanggal'];
+                $nnamaket=$row['nama_ket'];
+                $ncompl=$row['compl'];
+                $naktivitas=$row['aktivitas'];
+                $njenis=$row['jenis'];
+                $nnamadokt=$row['nama_dokter'];
+                $nnotes=$row['notes'];
+                $nsaran=$row['saran'];
+
+                $pedit="";
+                $phapus="";
+
+                echo "<tr>";
+                echo "<td nowrap>$no</td>";
+                echo "<td nowrap>$pedit &nbsp; &nbsp; $phapus</td>";
+                echo "<td nowrap>$ntgl</td>";
+                echo "<td nowrap>$nnamaket</td>";
+                echo "<td nowrap>$ncompl</td>";
+                echo "<td nowrap>$naktivitas</td>";
+                echo "<td nowrap>$njenis</td>";
+                echo "<td nowrap>$nnamadokt</td>";
+                echo "<td nowrap>$nnotes</td>";
+                echo "<td nowrap>$nsaran</td>";
+                echo "</tr>";
+
+                $no++;
+            }
+            ?>
+            </tbody>
+        </table>
+    </div>
+
+</form>
+
+<style>
+        .divnone {
+            display: none;
+        }
+        
+        .form-group, .input-group, .control-label {
+            margin-bottom:2px;
+        }
+        .control-label {
+            font-size:11px;
+        }
+        #datatable input[type=text], #tabelnobr input[type=text] {
+            box-sizing: border-box;
+            color:#000;
+            font-size:11px;
+            height: 25px;
+        }
+        select.soflow {
+            font-size:12px;
+            height: 30px;
+        }
+        .disabledDiv {
+            pointer-events: none;
+            opacity: 0.4;
+        }
+
+        table.datatable, table.tabelnobr {
+            color: #000;
+            font-family: Helvetica, Arial, sans-serif;
+            width: 100%;
+            border-collapse:
+            collapse; border-spacing: 0;
+            font-size: 11px;
+            border: 0px solid #000;
+        }
+
+        table.datatable td, table.tabelnobr td {
+            border: 1px solid #000; /* No more visible border */
+            height: 10px;
+            transition: all 0.1s;  /* Simple transition for hover effect */
+        }
+
+        table.datatable th, table.tabelnobr th {
+            background: #DFDFDF;  /* Darken header a bit */
+            font-weight: bold;
+        }
+
+        table.datatable td, table.tabelnobr td {
+            background: #FAFAFA;
+        }
+
+        /* Cells in even rows (2,4,6...) are one color */
+        tr:nth-child(even) td { background: #F1F1F1; }
+
+        /* Cells in odd rows (1,3,5...) are another (excludes header cells)  */
+        tr:nth-child(odd) td { background: #FEFEFE; }
+
+        tr td:hover.biasa { background: #666; color: #FFF; }
+        tr td:hover.left { background: #ccccff; color: #000; }
+
+        tr td.center1, td.center2 { text-align: center; }
+
+        tr td:hover.center1 { background: #666; color: #FFF; text-align: center; }
+        tr td:hover.center2 { background: #ccccff; color: #000; text-align: center; }
+        /* Hover cell effect! */
+        tr td {
+            padding: -10px;
+        }
+
+        th {
+            background: white;
+            position: sticky;
+            top: 0;
+            box-shadow: 0 2px 2px -1px rgba(0, 0, 0, 0.4);
+            z-index:1;
+        }
+		
+    </style>
+
+
+
+<?PHP
+hapusdata:
+    mysqli_query($cnmy, "drop TEMPORARY table IF EXISTS $tmp01");
+    mysqli_query($cnmy, "drop TEMPORARY table IF EXISTS $tmp02");
+    mysqli_close($cnmy);
+?>

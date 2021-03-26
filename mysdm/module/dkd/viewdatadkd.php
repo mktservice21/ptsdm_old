f259ffbdc2aac2b13309ddfb49dcc0c83b4c0347
myhuspan <huspannasrulloh@gmail.com>
2021-03-26 10:06:53 +0700

    Update viewdatadkd.php

--- mysdm/module/dkd/viewdatadkd.php
+++ mysdm/module/dkd/viewdatadkd.php
@@ -27,5 +27,33 @@ if ($pmodule=="cekdatasudahada") {
 
     echo $boleh;
 
+}elseif ($pmodule=="viewdatadoktercabang") {
+    include "../../config/koneksimysqli.php";
+    $pidcab=$_POST['uidcab'];
+    $pkodeid=$_POST['skode'];
+    $pcabpilih=$_POST['ukdcab'];
+    $pdoktpilih=$_POST['ukddokt'];
+
+    $pkodecabang=$pidcab;
+    if ((INT)$pkodeid==2) $pkodecabang=$pcabpilih;
+
+    $query = "select `id` as iddokter, namalengkap, gelar, spesialis from dr.masterdokter WHERE 1=1 ";
+    $query .=" AND icabangid='$pkodecabang' ";
+    $query .=" order by namalengkap, `id`";
+    $tampilket= mysqli_query($cnmy, $query);
+    $ketemu=mysqli_num_rows($tampilket);
+    if ((INT)$ketemu<=0) echo "<option value='' selected>-- Pilih --</option>";
+    while ($du= mysqli_fetch_array($tampilket)) {
+        $niddokt=$du['iddokter'];
+        $nnmdokt=$du['namalengkap'];
+        $ngelar=$du['gelar'];
+        $nspesial=$du['spesialis'];
+        if ($niddokt==$pdoktpilih)
+            echo "<option value='$niddokt' selected>$nnmdokt ($ngelar), $nspesial</option>";
+        else
+            echo "<option value='$niddokt'>$nnmdokt ($ngelar), $nspesial</option>";
+
+    }
+    mysqli_close($cnmy);
 }
 ?>

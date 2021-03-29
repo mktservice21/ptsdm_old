fc9cb93fe284c93e5491adb9337442e9a642ac2d
myhuspan <huspannasrulloh@gmail.com>
2021-03-29 14:26:34 +0700

    Update fungsi_sql.php

--- mysdm/config/fungsi_sql.php
+++ mysdm/config/fungsi_sql.php
@@ -611,17 +611,5 @@ function CariSelisihPeriodeDua($pperiode1, $pperiode2){
     
 }
 
-function encryptForId( $q ) {
-    $cryptKey  = 'qJB0rGtIn5UB1xG03efyCp';
-    $qEncoded      = base64_encode( mcrypt_encrypt( MCRYPT_RIJNDAEL_256, md5( $cryptKey ), $q, MCRYPT_MODE_CBC, md5( md5( $cryptKey ) ) ) );
-    return( $qEncoded );
-}
-
-function decryptForId( $q ) {
-    $cryptKey  = 'qJB0rGtIn5UB1xG03efyCp';
-    $qDecoded      = rtrim( mcrypt_decrypt( MCRYPT_RIJNDAEL_256, md5( $cryptKey ), base64_decode( $q ), MCRYPT_MODE_CBC, md5( md5( $cryptKey ) ) ), "\0");
-    return( $qDecoded );
-}
-
 ?>
 

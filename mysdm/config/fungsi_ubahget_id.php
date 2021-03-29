94ae5ca2b0337f93767e056365e1ed97e305d736
myhuspan <huspannasrulloh@gmail.com>
2021-03-29 17:29:12 +0700

    Update fungsi_ubahget_id.php

--- mysdm/config/fungsi_ubahget_id.php
+++ mysdm/config/fungsi_ubahget_id.php
@@ -2,7 +2,7 @@
 
 function encryptForId( $q ) {
     return( $q ); exit;
-    
+
     $cryptKey  = 'qJB0rGtIn5UB1xG03efyCp';
     $qEncoded      = base64_encode( mcrypt_encrypt( MCRYPT_RIJNDAEL_256, md5( $cryptKey ), $q, MCRYPT_MODE_CBC, md5( md5( $cryptKey ) ) ) );
     if (!empty($qEncoded)) $qEncoded = str_replace("+", "_a/_", $qEncoded);
@@ -18,4 +18,20 @@ function decryptForId( $q ) {
     return( $qDecoded );
 }
 
+function encodeString($str){
+    for($i=0; $i<5;$i++) {
+        $str=strrev(base64_encode($str)); //apply base64 first and then reverse the string
+    }
+    return $str;
+  }
+  
+  
+  function decodeString($str){
+    for($i=0; $i<5;$i++) {
+        $str=base64_decode(strrev($str)); //apply base64 first and then reverse the string}
+    }
+    return $str;
+  }
+
+  
 ?>

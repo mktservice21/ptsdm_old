fcb0870e0027cb79c910469f7bb780e1896536b9
myhuspan <huspannasrulloh@gmail.com>
2021-03-30 08:54:29 +0700

    Update sks_cekimport.php

--- mysdm/module/mst_import_sales/sks_cekimport.php
+++ mysdm/module/mst_import_sales/sks_cekimport.php
@@ -51,22 +51,10 @@ if (empty($puser)) {
     $cnmy=$cnms;
     $dbname = "MKT";
     
-    $plogit_akses=$_SESSION['PROSESLOGKONEK_IT'];//true or false || status awal true
-    if ($plogit_akses==true) {
-        include "../../config/koneksimysqli_it.php";
-    }
-    
     
     
     mysqli_query($cnmy, "DELETE FROM $dbname.importsks");
     $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { mysqli_close($cnmy); echo "Error DELETE importsks : $erropesan"; exit; }
-    
-    //IT
-    if ($plogit_akses==true) {
-        mysqli_query($cnit, "DELETE FROM $dbname.importsks");
-        $erropesan = mysqli_error($cnit); if (!empty($erropesan)) { mysqli_close($cnit); echo "IT... Error DELETE importsks : $erropesan"; exit; }
-    }
-    //END IT
     
     
     include("../../PHPExcel-1.8/Classes/PHPExcel/IOFactory.php");
@@ -196,13 +184,6 @@ if (empty($puser)) {
 
         mysqli_query($cnmy, $query_ins_pil);
         $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { mysqli_close($cnmy); echo "Error INSERT importsks : $erropesan"; exit; }
-        
-        //IT
-        if ($plogit_akses==true) {
-            mysqli_query($cnit, $query_ins_pil);
-            $erropesan = mysqli_error($cnit); if (!empty($erropesan)) { mysqli_close($cnit); echo "IT... Error INSERT importsks : $erropesan"; exit; }
-        }
-        //END IT
         
         
     }
@@ -385,9 +366,165 @@ if (empty($puser)) {
     
     mysqli_close($cnmy);
     
-    //IT
-    if ($plogit_akses==true) {
-        mysqli_close($cnit);
-    }
+?>
+
+
+<?PHP
+echo "<br/><br/><br/>Import IT....<br/><br/><br/>";
+
+
+
+    include "../../config/koneksimysqli_it.php";
+    $cnmy=$cnit;
+    $dbname = "MKT";
     
+    
+    
+    mysqli_query($cnmy, "DELETE FROM $dbname.importsks");
+    $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { mysqli_close($cnmy); echo "Error DELETE importsks : $erropesan"; exit; }
+    
+    
+    $objPHPExcel = PHPExcel_IOFactory::load($target_dir.$pnmfolder);
+    unset($pinsert_data);//kosongkan array
+    $padasheetkosong=false;
+    $jmlrec=0;
+    foreach ($objPHPExcel->getWorksheetIterator() as $worksheet){
+        $totalrow = $worksheet->getHighestRow();
+        $pnamasheet = $worksheet->getTitle();//get nama sheet
+        //$jmlrec=0;
+        
+        
+        $mynamesheet="";
+        if (strtoupper(TRIM($pnamasheet))=="SBY") $mynamesheet="SBY";
+        elseif (strtoupper(TRIM($pnamasheet))=="SMG") $mynamesheet="SMG";
+        elseif (strtoupper(TRIM($pnamasheet))=="PWKT") $mynamesheet="PWK";
+        elseif (strtoupper(TRIM($pnamasheet))=="MKSR") $mynamesheet="MKS";
+        elseif (strtoupper(TRIM($pnamasheet))=="BALI") $mynamesheet="DPS";
+        elseif (strtoupper(TRIM($pnamasheet))=="TEGAL") $mynamesheet="TGL";
+		elseif (strtoupper(TRIM($pnamasheet))=="PALU") $mynamesheet="PLU";
+        
+        if (!empty($mynamesheet)) {
+            unset($pinsert_data);//kosongkan array
+            for($row=2; $row<=$totalrow; $row++){
+                $pfile0 = mysqli_real_escape_string($cnmy, $worksheet->getCellByColumnAndRow(0, $row)->getValue());
+                $pfile1 = mysqli_real_escape_string($cnmy, $worksheet->getCellByColumnAndRow(1, $row)->getValue());//nopelanggan
+                $pfile2 = mysqli_real_escape_string($cnmy, $worksheet->getCellByColumnAndRow(3, $row)->getValue());//nama faktur pelanggan
+                $pfile3 = mysqli_real_escape_string($cnmy, $worksheet->getCellByColumnAndRow(8, $row)->getValue());//no faktur
+                $pfile4 = mysqli_real_escape_string($cnmy, $worksheet->getCellByColumnAndRow(9, $row)->getValue());//tgl fakrur
+                $pfile5 = mysqli_real_escape_string($cnmy, $worksheet->getCellByColumnAndRow(5, $row)->getValue());//TELP
+                $pfile6 = mysqli_real_escape_string($cnmy, $worksheet->getCellByColumnAndRow(10, $row)->getValue());//kode barang / no barang
+                $pfile7 = mysqli_real_escape_string($cnmy, $worksheet->getCellByColumnAndRow(11, $row)->getValue());//keterangan barang
+                $pfile8 = mysqli_real_escape_string($cnmy, $worksheet->getCellByColumnAndRow(12, $row)->getValue());// qty unit kwantitas kuantitas
+                $pfile9 = mysqli_real_escape_string($cnmy, $worksheet->getCellByColumnAndRow(13, $row)->getValue());//harga satuan
+				
+				$pfile12 = mysqli_real_escape_string($cnmy, $worksheet->getCellByColumnAndRow(6, $row)->getValue());//Nama Group Barang
+				
+				$pfile11 = mysqli_real_escape_string($cnmy, $worksheet->getCellByColumnAndRow(4, $row)->getValue());//alamat pelanggan
+                
+                if (!empty($pfile8)) {
+                    $pfile8=str_replace("=","", $pfile8);
+                    $pcek_data8= explode("*", $pfile8);
+                    
+                    if (isset($pcek_data8)) {
+                        if (isset($pcek_data8[0]) AND isset($pcek_data8[1])) {
+                            $palhasil8=(DOUBLE)$pcek_data8[0]*(DOUBLE)$pcek_data8[1];
+                            if (isset($pcek_data8[2]) OR isset($pcek_data8[3])) {
+                            }else{
+                                $pfile8=$palhasil8;
+                            }
+                        }
+                    }
+                }
+                
+                
+                if (!empty($pfile9)) {
+                    $pfile9=str_replace("=","", $pfile9);
+                    $pfile9=str_replace("(","", $pfile9);
+                    $pfile9=str_replace(")","", $pfile9);
+                    
+                    $pcek_data9= explode("/", $pfile9);
+                    
+                    if (isset($pcek_data9)) {
+                        if (isset($pcek_data9[0]) AND isset($pcek_data9[1])) {
+                            $palhasil9=(DOUBLE)$pcek_data9[0]/(DOUBLE)$pcek_data9[1];
+                            if (isset($pcek_data9[2]) OR isset($pcek_data9[3])) {
+                            }else{
+                                $pfile9=$palhasil9;
+                            }
+                        }
+                    }
+                }
+                
+                
+                
+                $pfile10=$mynamesheet;//cabang
+
+                if (empty($pfile0) AND empty($pfile1) AND empty($pfile2) AND empty($pfile3) AND empty($pfile4) AND empty($pfile5)
+                         AND empty($pfile6) AND empty($pfile7) AND empty($pfile8) AND empty($pfile9)) {
+                    continue;
+                }
+
+                if (!empty($pfile2)) $pfile2 = str_replace("'", " ", $pfile2);
+                if (!empty($pfile7)) $pfile7 = str_replace("'", " ", $pfile7);
+                
+                if (!empty($pfile8)) $pfile8=str_replace(".","", $pfile8);
+                if (!empty($pfile8)) $pfile8=str_replace(",","", $pfile8);
+                
+                if (!empty($pfile9)) $pfile9=str_replace(".","", $pfile9);
+                if (!empty($pfile9)) $pfile9=str_replace(",","", $pfile9);
+                
+                if (!empty($pfile4)) $pfile4 = str_replace("MEI", "May", strtoupper($pfile4));
+                if (!empty($pfile4)) $pfile4 = str_replace("AGU", "Aug", strtoupper($pfile4));
+                if (!empty($pfile4)) $pfile4 = str_replace("AGS", "Aug", strtoupper($pfile4));
+                if (!empty($pfile4)) $pfile4 = str_replace("OKT", "Oct", strtoupper($pfile4));
+                if (!empty($pfile4)) $pfile4 = str_replace("DES", "Dec", strtoupper($pfile4));
+                
+                if (!empty($pfile4)) $pfile4=date('Y-m-d', strtotime($pfile4));
+                
+				if (!empty($pfile12)) $pfile12 = str_replace("'", " ", $pfile12);
+				
+                $pinsert_data[] = "('$pfile3', '$pfile4', '$pfile1', '$pfile2', "
+                        . " '$pfile6', '$pfile7', '$pfile8', '$pfile9', '$pfile10', '$pfile11', '$pfile12')";
+                
+                /*
+                $query = "INSERT INTO $dbname.importsks (`No. Faktur Faktur`, `Tgl Faktur Faktur`, `No. Pelanggan`, `Nama Pelanggan`, "
+                        . " `KODE BARANG`, `Keterangan Barang`, `kuantitas`, `HNA`, `CABANG`, `Alamat 1 Pelanggan`)values"
+                        . " ('$pfile3', '$pfile4', '$pfile1', '$pfile2', "
+                        . " '$pfile6', '$pfile7', '$pfile8', '$pfile9', '$pfile10', '$pfile11')";
+
+                mysqli_query($cnmy, $query);
+                $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { mysqli_close($cnmy); echo "Error INSERT importsks : $erropesan"; exit; }
+                */
+                
+                $jmlrec++;
+            }
+            
+        }else{
+            $padasheetkosong=true;
+        }
+        
+        
+        $query_ins_pil = "INSERT INTO $dbname.importsks (`No. Faktur Faktur`, `Tgl Faktur Faktur`, `No. Pelanggan`, `Nama Pelanggan`, "
+                . " `KODE BARANG`, `Keterangan Barang`, `kuantitas`, `HNA`, `CABANG`, `Alamat 1 Pelanggan`, `HEADER`) values "
+                . " ".implode(', ', $pinsert_data);
+
+        mysqli_query($cnmy, $query_ins_pil);
+        $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { mysqli_close($cnmy); echo "Error INSERT importsks : $erropesan"; exit; }
+        
+        
+    }
+
+    
+    
+    $query = "select * from $dbname.importsks WHERE IFNULL(`Tgl Faktur Faktur`,'')= '' OR IFNULL(`Tgl Faktur Faktur`,'0000-00-00')= '0000-00-00' OR IFNULL(`Tgl Faktur Faktur`,'1970-01-01')= '1970-01-01'";
+    $tampil= mysqli_query($cnmy, $query);
+    $ketemu= mysqli_num_rows($tampil);
+    if ($ketemu>0) {
+        //mysqli_query($cnmy, "DELETE FROM $dbname.importsks");
+        //mysqli_close($cnmy);
+        echo "ERROR SIMPAN... ADA TANGGAL KOSONG atau FORMAT TANGGAL SALAH DI EXCEL NYA....";
+        //exit;
+    }
+
+
 ?>

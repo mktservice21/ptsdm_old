aaa05a0d771474a042d8019038d9467cbe508158
myhuspan <huspannasrulloh@gmail.com>
2021-04-08 13:12:53 +0700

    Update simpan_komentar.php

--- mysdm/module/dkd/dkd_reportpalnreal/simpan_komentar.php
+++ mysdm/module/dkd/dkd_reportpalnreal/simpan_komentar.php
@@ -3,13 +3,22 @@
 session_start();
 //data:"uidkry="+eidkry+"&utgl="+etgl+"&udoktid="+edoktid+"&uiduserinput="+eiduserinput+"&ukomen="+ekomen,
 
+$pidinput=$_POST['uidinput'];
 $pkryid=$_POST['uidkry'];
 $ptgl=$_POST['utgl'];
 $pdoktid=$_POST['udoktid'];
 $puserinput=$_POST['uiduserinput'];
+$pjbtinput=$_POST['uidjbtinput'];
 $pkomentar=$_POST['ukomen'];
 
+$pinpt_user="";
+$pinpt_jbt="";
+if (isset($_SESSION['IDCARD'])) $pinpt_user=$_SESSION['IDCARD'];
+if (isset($_SESSION['JABATANID'])) $pinpt_jbt=$_SESSION['JABATANID'];
 
+if (empty($puserinput)) $puserinput=$pinpt_user;
+if (empty($pjbtinput)) $pjbtinput=$pinpt_jbt;
+    
 if (empty($puserinput)) {
     echo "Anda harus login ulang...";
     exit;
@@ -22,9 +31,12 @@ $pberhasil="Tidak ada data yang disimpan";
 
 include "../../../config/koneksimysqli.php";
 
-if (!empty($pkryid) AND !empty($ptgl) AND !empty($pdoktid)) {
+if (!empty($pidinput) AND !empty($pkryid) AND !empty($ptgl) AND !empty($pdoktid)) {
     $query = "UPDATE hrd.dkd_new_real1 SET komentar='$pkomentar', komen_user='$puserinput', komen_date=NOW() WHERE "
             . " karyawanid='$pkryid' AND tanggal='$ptgl' and dokterid='$pdoktid' LIMIT 1";
+    
+    $query = "INSERT INTO hrd.dkd_new_real1_komen (nourut, jabatanid, komen_user, komen_date, komentar)VALUES"
+            . " ('$pidinput', '$pjbtinput', '$puserinput', NOW(), '$pkomentar')";
     mysqli_query($cnmy, $query); 
     $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; mysqli_close($cnmy); exit; }
     $pberhasil="berhasil input...";

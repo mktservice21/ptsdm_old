da8a715c47bf7bc4389627edaee6a07c574b970d
myhuspan <huspannasrulloh@gmail.com>
2021-04-08 13:13:22 +0700

    Update lihatkomentar.php

--- mysdm/module/dkd/dkd_reportpalnreal/lihatkomentar.php
+++ mysdm/module/dkd/dkd_reportpalnreal/lihatkomentar.php
@@ -86,7 +86,7 @@ if (!empty($pusrkomen)) {
     <div class='modal-content'>
         <div class='modal-header'>
             <button type='button' class='close' data-dismiss='modal'>&times;</button>
-            <h4 class='modal-title'>Notes</h4>
+            <h4 class='modal-title'>Komentar</h4>
         </div>
 
         

7c36689d9a337035baf15667461f9f298097983f
myhuspan <huspannasrulloh@gmail.com>
2021-04-12 09:55:11 +0700

    Update detailformcuti.php

--- mysdm/module/marketing/mkt_formcutieth/detailformcuti.php
+++ mysdm/module/marketing/mkt_formcutieth/detailformcuti.php
@@ -10,9 +10,84 @@
     
     include "config/koneksimysqli.php";
     
+    
 ?>
 
+<HTML>
+<HEAD>
+    <title>Form Cuti</title>
+    <meta http-equiv="Expires" content="Mon, 01 Sep 2030 1:00:00 GMT">
+    <meta http-equiv="Pragma" content="no-cache">
+    <?php header("Cache-Control: no-cache, must-revalidate"); ?>
+    <link rel="shortcut icon" href="images/icon.ico" />
+    
+    <script>
+        function printContent(el){
+            var restorepage = document.body.innerHTML;
+            var printcontent = document.getElementById(el).innerHTML;
+            document.body.innerHTML = printcontent;
+            window.print();
+            document.body.innerHTML = restorepage;
+        }
+    </script>
 
+    <script>
+        var EventUtil = new Object;
+        EventUtil.formatEvent = function (oEvent) {
+                return oEvent;
+        }
+
+
+        function goto2(pForm_,pPage_) {
+           document.getElementById(pForm_).action = pPage_;
+           document.getElementById(pForm_).submit();
+
+        }
+    </script>
+
+    <style>
+    @page 
+    {
+        /*size: auto;   /* auto is the current printer page size */
+        /*margin: 0mm;  /* this affects the margin in the printer settings */
+        margin-left: 7mm;  /* this affects the margin in the printer settings */
+        margin-right: 7mm;  /* this affects the margin in the printer settings */
+        margin-top: 5mm;  /* this affects the margin in the printer settings */
+        margin-bottom: 5mm;  /* this affects the margin in the printer settings */
+        size: portrait;
+    }
+    </style>
+    
+    
+</HEAD>
+
+
+<BODY>
+
+    <div id="div1">
+
+
+        <center>
+            <img src="images/logo_sdm.jpg" height="70px">
+            <h2>PT SURYA DERMATO MEDICA LABORATORIES</h2>
+        </center>
+        <hr/>
+        <center>
+            <h3>
+                <?PHP
+                echo "SURAT PENGAJUAN IZIN/CUTI";
+                ?>
+            </h3>
+        </center>
+        
+        
+        
+        
+    </div>
+    
+</BODY>
+
+</HTML>
 
 <?PHP
 hapusdata:

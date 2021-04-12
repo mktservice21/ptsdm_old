10f7ac263b8ff99b8383d4d39f500b93acb867cc
myhuspan <huspannasrulloh@gmail.com>
2021-04-12 16:28:34 +0700

    Update viewdatamkt.php

--- mysdm/module/marketing/viewdatamkt.php
+++ mysdm/module/marketing/viewdatamkt.php
@@ -69,6 +69,63 @@ if ($pmodule=="viewdatatanggal") {
     mysqli_close($cnmy);
 
     echo $boleh; exit;
+}elseif ($pmodule=="viewdatabulan2") {
+    date_default_timezone_set('Asia/Jakarta');
+    $ptgl1=$_POST['utgl'];
+    
+    $tgl_kedua = date('F Y', strtotime('+1 month', strtotime($ptgl1)));
+    $ptglpilih = date('Y-m-d', strtotime($ptgl1));
+    $ptglpilih02 = date('Y-m-d', strtotime($tgl_kedua));
+    $ctglpilih="";
+    ?>
+        <div id="div_bulan2">
+            <div class='form-group'>
+                <label class='control-label col-md-3 col-sm-3 col-xs-12' for=''>s/d. Bulan <span class='required'></span></label>
+                <div class='col-md-6'>
+                    <div class='input-group date' id='cbln02'>
+                        <input type="text" class="form-control" id='e_bulan02' name='e_bulan02' autocomplete='off' required='required' placeholder='F Y' value='<?PHP echo $tgl_kedua; ?>' Readonly>
+                        <span class='input-group-addon'>
+                            <span class='glyphicon glyphicon-calendar'></span>
+                        </span>
+
+                    </div>
+                </div>
+            </div>
+
+
+            <div class='form-group'>
+                <label class='control-label col-md-3 col-sm-3 col-xs-12' for=''>Tanggal <span class='required'></span></label>
+                <div class='col-xs-9'>
+                    <div id="div_tgl2">
+                        <?PHP
+                            $p_tgl = date('d', strtotime($ptglpilih02));
+                            $p_akh = date('t', strtotime($ptglpilih02));
+
+                            $p_b01 = date('Ym', strtotime($ptglpilih));
+                            $p_b02 = date('Ym', strtotime($ptglpilih02));
+
+                            $pchkpilih="";
+                            if (strpos($ctglpilih, $ptglpilih02)==true AND $p_b01<>$p_b02) $pchkpilih="checked";
+                            echo "<input type='checkbox' name='chktgl[]' value='$ptglpilih02' $pchkpilih> $p_tgl &nbsp; &nbsp; ";
+
+                            $nom=2;
+                            for ($ix=1;$ix<(INT)$p_akh;$ix++) {
+                                $ptglpilih02 = date('Y-m-d', strtotime('+1 days', strtotime($ptglpilih02)));
+
+                                $pchkpilih="";
+                                if (strpos($ctglpilih, $ptglpilih02)==true AND $p_b01<>$p_b02) $pchkpilih="checked";
+
+                                $p_tgl = date('d', strtotime($ptglpilih02));
+                                echo "<input type='checkbox' name='chktgl[]' value='$ptglpilih02' $pchkpilih> $p_tgl &nbsp; &nbsp; ";
+                                if ($nom>5) {echo "<br/>"; $nom=0;}
+                                $nom++;
+                            }
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?PHP
 }elseif ($pmodule=="tampilperiodepilih") {
     date_default_timezone_set('Asia/Jakarta');
     
@@ -138,18 +195,21 @@ if ($pmodule=="viewdatatanggal") {
             </div>
         </div>
 
+        <div id="div_bulan2">
+            
+            <div class='form-group'>
+                <label class='control-label col-md-3 col-sm-3 col-xs-12' for=''>s/d. <span class='required'></span></label>
+                <div class='col-md-6'>
+                    <div class='input-group date' id='tgl02'>
+                        <input type="text" class="form-control" id='e_bulan02' name='e_bulan02' autocomplete='off' required='required' placeholder='d F Y' value='<?PHP echo $tgl_kedua; ?>' Readonly>
+                        <span class='input-group-addon'>
+                            <span class='glyphicon glyphicon-calendar'></span>
+                        </span>
 
-        <div class='form-group'>
-            <label class='control-label col-md-3 col-sm-3 col-xs-12' for=''>s/d. <span class='required'></span></label>
-            <div class='col-md-6'>
-                <div class='input-group date' id='tgl02'>
-                    <input type="text" class="form-control" id='e_bulan02' name='e_bulan02' autocomplete='off' required='required' placeholder='d F Y' value='<?PHP echo $tgl_kedua; ?>' Readonly>
-                    <span class='input-group-addon'>
-                        <span class='glyphicon glyphicon-calendar'></span>
-                    </span>
-
+                    </div>
                 </div>
             </div>
+            
         </div>
         
         <script>
@@ -168,6 +228,34 @@ if ($pmodule=="viewdatatanggal") {
                 allowInputToggle: true,
                 format: 'MMMM YYYY'
             });
+            
+            $(document).ready(function() {
+                $('#cbln01').on('change dp.change', function(e){
+                    ShowTanggalPilih();
+                    var ijenis = document.getElementById('cb_jeniscuti').value;
+                    if (ijenis=="02") {
+                    }else{
+                        ShowBulan2();
+                    }
+                });
+
+                $('#cbln02').on('change dp.change', function(e){
+                    ShowTanggalPilih2();
+                });
+            });
+            function ShowBulan2() {
+                var etgl =document.getElementById('e_bulan01').value;
+
+                $.ajax({
+                    type:"post",
+                    url:"module/marketing/viewdatamkt.php?module=viewdatabulan2",
+                    data:"utgl="+etgl,
+                    success:function(data){
+                        $("#div_bulan2").html(data);
+                    }
+                });
+            }
+    
         </script>
         
         <div class='form-group'>
@@ -212,48 +300,51 @@ if ($pmodule=="viewdatatanggal") {
             </div>
         </div>
         <hr/>
-        <div class='form-group'>
-            <label class='control-label col-md-3 col-sm-3 col-xs-12' for=''>s/d. Bulan <span class='required'></span></label>
-            <div class='col-md-6'>
-                <div class='input-group date' id='cbln02'>
-                    <input type="text" class="form-control" id='e_bulan02' name='e_bulan02' autocomplete='off' required='required' placeholder='F Y' value='<?PHP echo $tgl_kedua; ?>' Readonly>
-                    <span class='input-group-addon'>
-                        <span class='glyphicon glyphicon-calendar'></span>
-                    </span>
+        
+        <div id="div_bulan2">
+            <div class='form-group'>
+                <label class='control-label col-md-3 col-sm-3 col-xs-12' for=''>s/d. Bulan <span class='required'></span></label>
+                <div class='col-md-6'>
+                    <div class='input-group date' id='cbln02X'>
+                        <input type="text" class="form-control" id='e_bulan02' name='e_bulan02' autocomplete='off' required='required' placeholder='F Y' value='<?PHP echo $tgl_kedua; ?>' Readonly>
+                        <span class='input-group-addon'>
+                            <span class='glyphicon glyphicon-calendar'></span>
+                        </span>
 
+                    </div>
                 </div>
             </div>
-        </div>
 
 
-        <div class='form-group'>
-            <label class='control-label col-md-3 col-sm-3 col-xs-12' for=''>Tanggal <span class='required'></span></label>
-            <div class='col-xs-9'>
-                <div id="div_tgl2">
-                    <?PHP
-                        $p_tgl = date('d', strtotime($ptglpilih02));
-                        $p_akh = date('t', strtotime($ptglpilih02));
+            <div class='form-group'>
+                <label class='control-label col-md-3 col-sm-3 col-xs-12' for=''>Tanggal <span class='required'></span></label>
+                <div class='col-xs-9'>
+                    <div id="div_tgl2">
+                        <?PHP
+                            $p_tgl = date('d', strtotime($ptglpilih02));
+                            $p_akh = date('t', strtotime($ptglpilih02));
 
-                        $p_b01 = date('Ym', strtotime($ptglpilih));
-                        $p_b02 = date('Ym', strtotime($ptglpilih02));
-
-                        $pchkpilih="";
-                        if (strpos($ctglpilih, $ptglpilih02)==true AND $p_b01<>$p_b02) $pchkpilih="checked";
-                        echo "<input type='checkbox' name='chktgl[]' value='$ptglpilih02' $pchkpilih> $p_tgl &nbsp; &nbsp; ";
-
-                        $nom=2;
-                        for ($ix=1;$ix<(INT)$p_akh;$ix++) {
-                            $ptglpilih02 = date('Y-m-d', strtotime('+1 days', strtotime($ptglpilih02)));
+                            $p_b01 = date('Ym', strtotime($ptglpilih));
+                            $p_b02 = date('Ym', strtotime($ptglpilih02));
 
                             $pchkpilih="";
                             if (strpos($ctglpilih, $ptglpilih02)==true AND $p_b01<>$p_b02) $pchkpilih="checked";
-
-                            $p_tgl = date('d', strtotime($ptglpilih02));
                             echo "<input type='checkbox' name='chktgl[]' value='$ptglpilih02' $pchkpilih> $p_tgl &nbsp; &nbsp; ";
-                            if ($nom>5) {echo "<br/>"; $nom=0;}
-                            $nom++;
-                        }
-                    ?>
+
+                            $nom=2;
+                            for ($ix=1;$ix<(INT)$p_akh;$ix++) {
+                                $ptglpilih02 = date('Y-m-d', strtotime('+1 days', strtotime($ptglpilih02)));
+
+                                $pchkpilih="";
+                                if (strpos($ctglpilih, $ptglpilih02)==true AND $p_b01<>$p_b02) $pchkpilih="checked";
+
+                                $p_tgl = date('d', strtotime($ptglpilih02));
+                                echo "<input type='checkbox' name='chktgl[]' value='$ptglpilih02' $pchkpilih> $p_tgl &nbsp; &nbsp; ";
+                                if ($nom>5) {echo "<br/>"; $nom=0;}
+                                $nom++;
+                            }
+                        ?>
+                    </div>
                 </div>
             </div>
         </div>

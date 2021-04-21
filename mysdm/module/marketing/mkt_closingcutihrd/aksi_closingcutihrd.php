c9daaacb719a7504876a4b28438cae4c59fee5e2
myhuspan <huspannasrulloh@gmail.com>
2021-04-21 11:50:51 +0700

    Update aksi_closingcutihrd.php

--- mysdm/module/marketing/mkt_closingcutihrd/aksi_closingcutihrd.php
+++ mysdm/module/marketing/mkt_closingcutihrd/aksi_closingcutihrd.php
@@ -1,8 +1,49 @@
-<?php
+<?PHP
+    date_default_timezone_set('Asia/Jakarta');
+    ini_set("memory_limit","512M");
+    ini_set('max_execution_time', 0);
+    
+    session_start();
+    
+    $puserid="";
+    if (isset($_SESSION['USERID'])) $puserid=$_SESSION['USERID'];
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    if (empty($puserid)) {
+        echo "ANDA HARUS LOGIN ULANG...";
+        exit;
+    }
 
+    $pkaryawanid=$_SESSION['IDCARD'];
+    
+    $ptahun=$_POST['utahun'];
+    
+    $_SESSION['CLSCUTITHN']=$ptahun;
+    
+    $pmodule=$_GET['module'];
+    $pact=$_GET['act'];
+    $pidmenu=$_GET['idmenu'];
+
+    include "../../../config/koneksimysqli.php";
+    
+    $query = "CALL hrd.proses_cuti_tahunan('$ptahun')";
+    mysqli_query($cnmy, $query);
+    $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; goto hapusdata; }
+    
+    
+    $query = "UPDATE hrd.karyawan_cuti_close SET userid='$pkaryawanid' WHERE tahun='$ptahun'";
+    mysqli_query($cnmy, $query);
+    $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; goto hapusdata; }
+    
+    echo "data berhasil diproses"; goto berhasil;
+?>
+
+
+<?PHP
+hapusdata:
+    mysqli_close($cnmy);
+    
+    exit;
+berhasil:
+    mysqli_close($cnmy);
+
+?>

d3ecf62d5c36320f316fe7c3e03e0575233dba62
myhuspan <huspannasrulloh@gmail.com>
2021-05-04 13:45:31 +0700

    Create kunjungan_1.php

---
+++ mysdm/module/mod_br_entrybrluarkota/kunjungan_1.php
@@ -0,0 +1,149 @@
+<?PHP
+$pmodule="";
+if (isset($_GET['module'])) $pmodule=$_GET['module'];
+
+if ($pmodule=="getkunjungan"){
+    include "../../config/koneksimysqli.php";
+    include "../../config/fungsi_sql.php";
+    include "../../config/library.php";
+    
+    $date1=$_POST['ubulan'];
+    //$date1 = date("Y-m-d");
+    $tgl1= date("Y-m-01", strtotime($date1));
+    $tgl2= date("Y-m-t", strtotime($date1));
+    $bulan= date("Ym", strtotime($date1));
+    $idkar=$_POST['umr'];
+    $idajukan=$_POST['umr'];
+
+    ?>
+    <div class='x_content'>
+        <table id='datatablercbi' class='table table-striped table-bordered' width='100%'>
+            <thead>
+                <tr>
+                    <th width='10px'></th>
+                    <th width='10px'>Hari / Tanggal</th>
+                    <th>Tujuan / Note</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?PHP
+                    $no=1;
+                    $patasan1="";
+                    $patasan2="";
+                    $patasan3="";
+
+                    while (strtotime($tgl1) <= strtotime($tgl2)) {
+                        $mytgl= date("Ymd", strtotime($tgl1));
+                        $ptgl= date("d/m/Y", strtotime($tgl1));
+                        $pddat= date("Y-m-d", strtotime($tgl1));
+                        $mhari= date("d", strtotime($tgl1));
+                        $phari=date("w", strtotime($tgl1));
+                        
+                        $nmhari=$seminggu[$phari];
+                        $ket="";
+                        $stl="";
+                        if ($phari==0)
+                            $stl="style='background:#FAEBD7;'";
+                        
+                        //$sql = "SELECT * FROM dbmaster.t_planuc_mkt WHERE karyawanid='$idkar' and DATE_FORMAT(tgl, '%Y%m%d') = '$mytgl' order by tgl";
+                        $sql = "select a.karyawanid, a.id_jenis, b.tanggal, a.keperluan FROM hrd.t_cuti0 as a "
+                                . " LEFT JOIN hrd.t_cuti1 as b on a.idcuti=b.idcuti WHERE a.id_jenis IN ('05') AND "
+                                . " a.karyawanid='$idkar' AND DATE_FORMAT(b.tanggal, '%Y%m%d') = '$mytgl' AND IFNULL(a.stsnonaktif,'')<>'Y' "
+                                . " ORDER BY b.tanggal";
+                        $tampil = mysqli_query($cnmy, $sql);
+                        $ketemu = mysqli_num_rows($tampil);
+                        if ($ketemu>0) {
+                            $t= mysqli_fetch_array($tampil);
+                            //$nourut=$t['nourut'];
+                            $ket=$t['keperluan'];
+                            
+                            //if (!empty($t['atasan1'])) $patasan1=$t['atasan1'];
+                            //if (!empty($t['atasan2'])) $patasan2=$t['atasan2'];
+                            //if (!empty($t['atasan3'])) $patasan3=$t['atasan3'];
+                            
+                        }
+                        
+                        echo "<tr $stl>";
+                        echo "<td nowrap>$mhari</td>";
+                        echo "<td nowrap>$nmhari, $ptgl</td>";
+                        echo "<td><input type='hidden' name='txtket$no' id='txtket$no' size='80px' value='$ket'>$ket</td>";
+                        echo "</tr>";
+                        $no++;
+                        $tgl1 = date ("Y-m-d", strtotime("+1 day", strtotime($tgl1)));
+                    }
+                ?>
+            </tbody>
+        </table>
+
+    </div>
+    <style>
+        .divnone {
+            display: none;
+        }
+        #datatablercbi th {
+            font-size: 14px;
+        }
+        #datatablercbi td { 
+            font-size: 13px;
+        }
+        .imgzoom:hover {
+            -ms-transform: scale(3.5); /* IE 9 */
+            -webkit-transform: scale(3.5); /* Safari 3-8 */
+            transform: scale(3.5);
+
+        }
+    </style>
+    <?PHP
+    exit;
+}else{
+    
+    $pnkryid=$_POST['e_idkaryawan'];
+    $pntgl01=$_POST['e_periode01'];
+    $pntgl02=$_POST['e_periode02'];
+    
+    $pntgl01 = str_replace('/', '-', $pntgl01);
+    $pntgl02 = str_replace('/', '-', $pntgl02);
+    
+    $blnpilihuc = date('Y-m', strtotime($pntgl01));
+    $blnpilihuc02 = date('Y-m', strtotime($pntgl02));
+    
+    $ntgl01 = date('Y-m-d', strtotime($pntgl01));
+    $ntgl02 = date('Y-m-d', strtotime($pntgl02));
+    
+    echo "<input type='hidden' id='txt_kryid' name='txt_kryid' value='$pnkryid' Readonly>";
+    echo "<input type='hidden' id='txt_etgl01' name='txt_etgl01' value='$ntgl01' Readonly>";
+    echo "<input type='hidden' id='txt_etgl02' name='txt_etgl02' value='$ntgl02' Readonly>";
+    
+    //echo "$ntgl01 s/d. $ntgl02"; exit;
+}
+?>
+
+
+
+<!-- jQuery -->
+<script src="vendors/jquery/dist/jquery.min.js"></script>
+<script>
+    $(document).ready(function() {
+        
+        var eperi01 =document.getElementById('txt_etgl01').value;
+        var eperi02 =document.getElementById('txt_etgl02').value;
+        var icar =document.getElementById('txt_kryid').value;
+        
+        //var icar = window.opener.document.getElementById('e_idkaryawan').value;
+        //var itgl = window.opener.document.getElementById('e_bulan').value;
+        //var ptgl = itgl.split("/");
+        //var thnbln = ptgl[1]+"-"+ptgl[0]+"-01";
+        $.ajax({
+            type:"post",
+            url:"module/mod_br_entrybrluarkota/kunjungan.php?module=getkunjungan",
+            data:"ubulan="+eperi01+"&ubulan2="+eperi02+"&umr="+icar,
+            success:function(data){
+                $("#c-data").html(data);
+            }
+        });
+    });
+</script>
+
+<div id='c-data'>
+    
+</div>

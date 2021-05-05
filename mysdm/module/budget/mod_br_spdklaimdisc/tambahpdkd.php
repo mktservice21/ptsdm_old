a758ebc84755b28de19747fd869503be7f6c1ecc
myhuspan <huspannasrulloh@gmail.com>
2021-05-05 10:59:59 +0700

    Update tambahpdkd.php

--- mysdm/module/budget/mod_br_spdklaimdisc/tambahpdkd.php
+++ mysdm/module/budget/mod_br_spdklaimdisc/tambahpdkd.php
@@ -1,8 +1,34 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+?>
+
+
+<link href="css/inputselectbox.css" rel="stylesheet" type="text/css" />
+<style>
+    .form-group, .input-group, .control-label {
+        margin-bottom:3px;
+    }
+    .control-label {
+        font-size:12px;
+    }
+    input[type=text] {
+        box-sizing: border-box;
+        color:#000;
+        font-size:12px;
+        height: 30px;
+    }
+    select.soflow {
+        font-size:12px;
+        height: 30px;
+    }
+    .disabledDiv {
+        pointer-events: none;
+        opacity: 0.4;
+    }
+    .btn-primary {
+        width:50px;
+        height:30px;
+        margin-right: 50px;
+    }
+</style>
 

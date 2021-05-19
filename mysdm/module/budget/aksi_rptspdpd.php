98d477844dfdb55daffc0b596ec65f66787b4c10
myhuspan <huspannasrulloh@gmail.com>
2021-05-19 10:43:38 +0700

    Update aksi_rptspdpd.php

--- mysdm/module/budget/aksi_rptspdpd.php
+++ mysdm/module/budget/aksi_rptspdpd.php
@@ -5,7 +5,7 @@ if (isset($_GET['act'])) $pact=$_GET['act'];
 
 if ($pact=="viewrptklaimdist") {
     include "laporanklaimdisc.php";
-}elseif ($pact=="viewrptbradveth" OR $pact=="viewrptbrpcmeth" OR $pact=="viewrptbrklaimeth" OR $pact=="viewrptbrkboneth") {
+}elseif ($pact=="viewrptbradveth" OR $pact=="viewrptbrpcmeth" OR $pact=="viewrptbrklaimeth" OR $pact=="viewrptbrkboneth" OR $pact=="viewrptbradvvsbyeth") {
     include "laporanbrethical.php";
 }
 

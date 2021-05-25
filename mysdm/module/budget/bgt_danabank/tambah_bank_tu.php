8fbc99de7bec9dd46d16f5121ad0c8974429db45
myhuspan <huspannasrulloh@gmail.com>
2021-05-25 10:03:04 +0700

    Update tambah_bank_tu.php

--- mysdm/module/budget/bgt_danabank/tambah_bank_tu.php
+++ mysdm/module/budget/bgt_danabank/tambah_bank_tu.php
@@ -3,7 +3,14 @@
     session_start();
     include "../../../config/koneksimysqli.php";
     include "../../../config/fungsi_combo.php";
+    
+    $fkaryawan="";
+    $fuserid="";
+    if (isset($_SESSION['IDCARD'])) $fkaryawan=$_SESSION['IDCARD'];
+    if (isset($_SESSION['USERID'])) $fuserid=$_SESSION['USERID'];
+    
     $idbr=$_POST['uidinput'];
+    
     
     $query="select * from dbmaster.t_suratdana_bank where idinputbank='$idbr'";
     $tampil= mysqli_query($cnmy, $query);
@@ -12,6 +19,10 @@
     $pjmlsudah=$nx['jumlah'];
     $pkodeid=$nx['kodeid'];
     $psubkode=$nx['subkode'];
+    $pbridinput=$nx['brid'];
+    $pdivisiid=$nx['divisi'];;
+    $pnosliplama=$nx['noslip'];;
+    $nparentidb= $nx["parentidbank"];
     
     $pnoslipbaru="";
     $pketerangan="";
@@ -24,6 +35,23 @@
     $tampil2= mysqli_query($cnmy, $query);
     $nx2= mysqli_fetch_array($tampil2);
     $pigroup=$nx2['igroup'];
+    
+    $pbolehsimpan="";
+    $phiddennoslip="hidden";
+    //cek data input
+    if ($pigroup=="1") {
+        if ($pdivisiid=="OTC") {
+            $query = "select brOtcId as brid from hrd.br_otc WHERE brOtcId='$pbridinput' And karyawanid='$fkaryawan'";
+        }else{
+            $query = "select brId as brid from hrd.br0 WHERE brId='$pbridinput' And LPAD(user1, 10, '0')='$fkaryawan'";
+        }
+        $tampil3= mysqli_query($cnmy, $query);
+        $ketemu3= mysqli_num_rows($tampil3);
+        if ((INT)$ketemu3>0) { $pbolehsimpan="Y"; $phiddennoslip=""; }
+    }
+    
+    $pbuttonsimpan="";
+    if (!empty($nparentidb)) $pbuttonsimpan="hidden";
     
 $act="input";
 $aksi="";
@@ -66,6 +94,12 @@ $aksi="";
                                             <div class='col-md-4'>
                                                 <input type='text' id='e_id' name='e_id' class='form-control col-md-7 col-xs-12' value='<?PHP echo $idbr; ?>' Readonly>
                                                 <input type='hidden' id='e_groupid' name='e_groupid' class='form-control col-md-7 col-xs-12' value='<?PHP echo $pigroup; ?>' Readonly>
+                                                <input type='hidden' id='e_bridinput' name='e_bridinput' class='form-control col-md-7 col-xs-12' value='<?PHP echo $pbridinput; ?>' Readonly>
+                                                <input type='hidden' id='e_nosliplama' name='e_nosliplama' class='form-control col-md-7 col-xs-12' value='<?PHP echo $pnosliplama; ?>' Readonly>
+                                                <input type='hidden' id='e_divisiid' name='e_divisiid' class='form-control col-md-7 col-xs-12' value='<?PHP echo $pdivisiid; ?>' Readonly>
+                                                <input type='hidden' id='e_bolehsimpanbr' name='e_bolehsimpanbr' class='form-control col-md-7 col-xs-12' value='<?PHP echo $pbolehsimpan; ?>' Readonly>
+                                                <input type='hidden' id='e_userinput' name='e_userinput' class='form-control col-md-7 col-xs-12' value='<?PHP echo $fuserid; ?>' Readonly>
+                                                <input type='hidden' id='e_kryinput' name='e_kryinput' class='form-control col-md-7 col-xs-12' value='<?PHP echo $fkaryawan; ?>' Readonly>
                                             </div>
                                         </div>
                                         
@@ -90,7 +124,7 @@ $aksi="";
                                         </div>
                                         
                                         
-                                        <div class='form-group'>
+                                        <div <?PHP echo $phiddennoslip; ?> class='form-group'>
                                             <label class='control-label col-md-3 col-sm-3 col-xs-12' for=''>Noslip Baru <span class='required'></span></label>
                                             <div class='col-md-4'>
                                                 <input type='text' id='e_noslipbaru' name='e_noslipbaru' class='form-control col-md-7 col-xs-12' value='<?PHP echo $pnoslipbaru; ?>'>
@@ -104,7 +138,7 @@ $aksi="";
                                             </div>
                                         </div>
                                         
-                                        <div class='form-group'>
+                                        <div <?PHP echo $pbuttonsimpan; ?> class='form-group'>
                                             <label class='control-label col-md-3 col-sm-3 col-xs-12' for=''> <span class='required'></span></label>
                                             <div class='col-xs-9'>
                                                 <div class="checkbox">
@@ -148,10 +182,16 @@ $aksi="";
     function disp_confirm_keluar_tu(pText_,ket)  {
         var eact=ket;
         var egroup = document.getElementById('e_groupid').value;
+        var ebrid = document.getElementById('e_bridinput').value;
+        var enosliplama = document.getElementById('e_nosliplama').value;
+        var edivisi = document.getElementById('e_divisiid').value;
+        var ebolehsimpanbr = document.getElementById('e_bolehsimpanbr').value;
         var eid = document.getElementById('e_id').value;
         var etglkeluar = document.getElementById("e_tglkeluar").value;
         var eketerangan = document.getElementById("e_ket").value;
         var enoslipbaru = document.getElementById("e_noslipbaru").value;
+        var euserinput = document.getElementById("e_userinput").value;
+        var ekryinput = document.getElementById("e_kryinput").value;
         //alert(etglkeluar); return false;
         ok_ = 1;
         if (ok_) {
@@ -166,7 +206,9 @@ $aksi="";
                 $.ajax({
                     type:"post",
                     url:"module/budget/bgt_danabank/simpan_bank_tu.php?module="+module+"&act="+eact+"&idmenu="+idmenu,
-                    data:"uid="+eid+"&utglkeluar="+etglkeluar+"&unoslipbaru="+enoslipbaru+"&uketerangan="+eketerangan+"&ugroup="+egroup,
+                    data:"uid="+eid+"&utglkeluar="+etglkeluar+"&unoslipbaru="+enoslipbaru+"&uketerangan="+eketerangan+"&ugroup="+egroup+
+                            "&ubrid="+ebrid+"&udivisi="+edivisi+"&ubolehsimpanbr="+ebolehsimpanbr+"&uuserinput="+euserinput+"&ukryinput="+ekryinput+
+                            "&unosliplama="+enosliplama,
                     success:function(data){
                         if (data.length > 2) {
                             alert(data);

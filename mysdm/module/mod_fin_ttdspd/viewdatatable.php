c670fdff0af015cdd3c9a5eff27e6c60de2a4361
myhuspan <huspannasrulloh@gmail.com>
2021-05-28 09:42:27 +0700

    Update viewdatatable.php

--- mysdm/module/mod_fin_ttdspd/viewdatatable.php
+++ mysdm/module/mod_fin_ttdspd/viewdatatable.php
@@ -338,8 +338,15 @@
                         }
                     }
 					
-					if ($pmystsyginput==9) {
+                    if ($pmystsyginput==9) {
                         $pmymodule="module=bgtpdkaskecilcabang&act=input&idmenu=350&ket=bukan&ispd=$idno&bln=$tglbuat";
+                    }
+                    
+                    if ($pjenisrpt=="D" OR $pjenisrpt=="C") {
+                        if ($pkaryawanid=="0000000266" OR $pkaryawanid=="0000000144") {
+                            //$pmymodule="module=saldosuratdana&act=viewbrklaim&idmenu=192&ket=bukan&ispd=$idno&iid=$pmystsyginput";
+                            $pmymodule="module=previewmintadana&act=viewrptklaimdist&idmenu=504ket=bukan&ispd=$idno&iid=$pmystsyginput";
+                        }
                     }
                     
                     //if ($pstsp=="BPJS") {

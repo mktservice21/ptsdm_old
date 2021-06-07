a2ee5f76aebacf5b63aa6b859cd0e755f82c7dd6
myhuspan <huspannasrulloh@gmail.com>
2021-06-07 13:30:55 +0700

    Update tambah.php

--- mysdm/module/mod_br_spdotc/tambah.php
+++ mysdm/module/mod_br_spdotc/tambah.php
@@ -177,7 +177,7 @@ if ($pjnsrpt=="G") $pjens9="selected";
                                 
                                 
                                 
-                                <div  id="jenis_kode">
+                                <div hidden id="jenis_kode">
                                     
                                     <div class='form-group'>
                                         <label class='control-label col-md-3 col-sm-3 col-xs-12' for=''>Kode <span class='required'></span></label>

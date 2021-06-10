9f96bc34b1dd005aa65cb98fe648ea385f25d485
myhuspan <huspannasrulloh@gmail.com>
2021-06-10 10:56:42 +0700

    Update sls_ytd_sm.php

--- mysdm/module/a_new/ytd_sm/sls_ytd_sm.php
+++ mysdm/module/a_new/ytd_sm/sls_ytd_sm.php
@@ -55,6 +55,17 @@ if ($pilihdarims==true) {
 	$namatabel=$tmp00;
 }
 
+if (!empty($idsm)) {
+    $ppilihanmakloya="";
+    $resultsmkl = DB::query("SELECT DISTINCT maklo FROM ms.cbgytd WHERE IFNULL(maklo,'')='Y' AND id_sm='$idsm'");
+    foreach ($resultsmkl as $mk) {
+        if ($mk['maklo']=="Y"); $ppilihanmakloya=$mk['maklo'];
+    }
+    if ($ppilihanmakloya=="Y") {
+    }else{
+        $resultsdel = DB::query("DELETE FROM $namatabel WHERE divprodid='MAKLO'");
+    }
+}
 
 $pprodukmaklo="";
 //$resultsdel = DB::query("DELETE FROM $namatabel WHERE IFNULL(mtd_qty_sales,0)=0 AND IFNULL(mtd_value_sales,0)=0 AND IFNULL(mtd_qty_thnlalu,0)=0 AND IFNULL(mtd_value_thnlalu,0)=0");

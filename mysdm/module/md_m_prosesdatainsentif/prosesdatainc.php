f52f6443f2b9c2e1ce744d1c5b3d66713d918602
myhuspan <huspannasrulloh@gmail.com>
2021-06-14 17:03:45 +0700

    Update prosesdatainc.php

--- mysdm/module/md_m_prosesdatainsentif/prosesdatainc.php
+++ mysdm/module/md_m_prosesdatainsentif/prosesdatainc.php
@@ -20,43 +20,25 @@ function caridatainsentif_query($con, $temp, $bulan, $jabatan, $pdivprod, $pincf
     $now=date("mdYhis");
     $tmp01 ="RTMPPROSINC01_".$_SESSION['USERID']."_$now";
     
-    $query = "SELECT CAST(1 as DECIMAL(10,0)) as urutan,  
-            '$ptgl1' AS bulan,
-            g.divprodid AS divisi,
-                  c.iCabangId icabangid,
-            c.nama AS cabang,
-            'MR' AS jabatan,
-            k.karyawanid,
-            k.nama,
-            c.region,
-            CONVERT(SUM(i.incentive), DEC (0)) AS jumlah
-          FROM
-            ms.incentive_mr i
-            LEFT JOIN
-              (SELECT
-                mr,
-                divprodid
-              FROM
-                ms.mrgp
-              WHERE bulan = '$ptgl1') g
-              ON i.karyawanid = g.mr
-            JOIN ms.karyawan k
-              ON i.karyawanid = k.karyawanId
-            JOIN
-              (SELECT DISTINCT
-                mr,
-                icabangid
-              FROM
-                ms.penempatan_marketing
-              WHERE bulan = '$ptgl1'
-                AND mr <> '000') pm
-              ON i.karyawanid = pm.mr
-            JOIN sls.icabang c
-              ON pm.icabangid = c.iCabangId
-          WHERE bulan = '$ptgl1' $fildivisi $pfilterincfrom 
-          GROUP BY k.nama,
-            c.region,
-            c.nama";
+    if ($pincfrom=="PM") {
+        
+        $query = "SELECT CAST(1 as DECIMAL(10,0)) as urutan, '$ptgl1' AS bulan, i.jenis, g.divprodid AS divisi, c.iCabangId icabangid, c.nama AS cabang, "
+                . "'MR' AS jabatan, k.karyawanid, k.nama, c.region, CONVERT(SUM(i.incentive), DEC (0)) AS jumlah FROM "
+                . " ms.incentive_mr i "
+                . " LEFT JOIN (SELECT mr, divprodid FROM ms.mrgp WHERE bulan = '$ptgl1') g ON i.karyawanid = g.mr JOIN ms.karyawan k ON i.karyawanid = k.karyawanId "
+                . " JOIN (SELECT DISTINCT mr, icabangid FROM ms.penempatan_marketing WHERE bulan = '$ptgl1' AND mr <> '000') pm ON i.karyawanid = pm.mr "
+                . " JOIN sls.icabang c ON pm.icabangid = c.iCabangId WHERE bulan = '$ptgl1' $fildivisi $pfilterincfrom "
+                . " GROUP BY k.nama, c.region, c.nama";
+    }else{
+        
+        $query = "SELECT CAST(1 as DECIMAL(10,0)) as urutan, '$ptgl1' AS bulan, g.divprodid AS divisi, c.iCabangId icabangid, c.nama AS cabang, "
+                . "'MR' AS jabatan, k.karyawanid, k.nama, c.region, CONVERT(SUM(i.incentive), DEC (0)) AS jumlah FROM "
+                . " ms.incentive_mr i "
+                . " LEFT JOIN (SELECT mr, divprodid FROM ms.mrgp WHERE bulan = '$ptgl1') g ON i.karyawanid = g.mr JOIN ms.karyawan k ON i.karyawanid = k.karyawanId "
+                . " JOIN (SELECT DISTINCT mr, icabangid FROM ms.penempatan_marketing WHERE bulan = '$ptgl1' AND mr <> '000') pm ON i.karyawanid = pm.mr "
+                . " JOIN sls.icabang c ON pm.icabangid = c.iCabangId WHERE bulan = '$ptgl1' $fildivisi $pfilterincfrom "
+                . " GROUP BY k.nama, c.region, c.nama";
+    }
     
     //echo "$query";exit;
     $query = "create  table dbtemp.$tmp01 ($query)";
@@ -95,73 +77,68 @@ function caridatainsentif_query($con, $temp, $bulan, $jabatan, $pdivprod, $pincf
 	
 if ($pdivprod=="" OR $pdivprod=="CAN") {
     
-    $query = "INSERT INTO dbtemp.$tmp01 (urutan, bulan, divisi, icabangid, cabang, jabatan, karyawanid, nama, region, jumlah)
-        SELECT 2 as urutan, 
-        '$ptgl1' AS bulan,
-        'CANARY' AS divisi,
-              c.iCabangId,
-        c.nama AS cabang,
-        'AM' AS jabatan,
-        k.karyawanid,
-        k.nama,
-        c.region,
-        CONVERT(SUM(i.incentive), DEC (0)) AS jumlah
-      FROM
-        ms.incentive_am i
-        LEFT JOIN
-          (SELECT DISTINCT
-            icabangid,
-            am
-          FROM
-            ms.penempatan_marketing pm
-          WHERE bulan = '$ptgl1') pm
-          ON i.karyawanid = pm.am
-        JOIN ms.karyawan k
-          ON i.karyawanid = k.karyawanId
-        JOIN sls.icabang c
-          ON pm.icabangid = c.iCabangId
-      WHERE i.bulan = '$ptgl1' $pfilterincfrom 
-      GROUP BY k.nama,
-        c.region,
-        c.nama";
-    mysqli_query($cnmy, $query);
-    $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; goto hapusdata; }
+    if ($pincfrom=="PM") {
+        
+        $query = "INSERT INTO dbtemp.$tmp01 (urutan, bulan, jenis, divisi, icabangid, cabang, jabatan, karyawanid, nama, region, jumlah)
+            SELECT 2 as urutan, '$ptgl1' AS bulan, i.jenis, 'CANARY' AS divisi, c.iCabangId, c.nama AS cabang, "
+                . " 'AM' AS jabatan, k.karyawanid, k.nama, c.region, CONVERT(SUM(i.incentive), DEC (0)) AS jumlah "
+                . " FROM ms.incentive_am i "
+                . " LEFT JOIN (SELECT DISTINCT icabangid, am FROM ms.penempatan_marketing pm WHERE bulan = '$ptgl1') pm ON i.karyawanid = pm.am JOIN ms.karyawan k ON i.karyawanid = k.karyawanId "
+                . " JOIN sls.icabang c ON pm.icabangid = c.iCabangId WHERE i.bulan = '$ptgl1' $pfilterincfrom  "
+                . " GROUP BY k.nama, c.region, c.nama";
+        mysqli_query($cnmy, $query);
+        $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; goto hapusdata; }
+
+        $query="INSERT INTO dbtemp.$tmp01 (urutan, bulan, jenis, divisi, icabangid, cabang, jabatan, karyawanid, nama, region, jumlah) "
+                . " SELECT 3 as urutan, '$ptgl1' AS bulan, i.jenis, 'CANARY' AS divisi, c.icabangid, c.nama AS cabang, "
+                . " 'DM' AS jabatan, k.karyawanid, k.nama, c.region, CONVERT(SUM(i.incentive), DEC (0)) AS jumlah "
+                . " FROM ms.incentive_dm i "
+                . " LEFT JOIN (SELECT DISTINCT icabangid, dm FROM ms.penempatan_marketing pm WHERE bulan = '$ptgl1') pm ON i.karyawanid = pm.dm "
+                . " JOIN ms.karyawan k ON i.karyawanid = k.karyawanId JOIN sls.icabang c ON pm.icabangid = c.iCabangId "
+                . " WHERE i.bulan = '$ptgl1' $pfilterincfrom  "
+                . " GROUP BY k.nama, c.region, c.nama";
+        
+        mysqli_query($cnmy, $query);
+        $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; goto hapusdata; }
+        
+        
+        $query="UPDATE dbtemp.$tmp01 as a JOIN ms.jenisincentivepm as b on a.jenis=b.jenis "
+                . " SET a.divisi=b.divprodid, a.ndivisiid=b.divprodid";
+        mysqli_query($cnmy, $query);
+        $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; goto hapusdata; }
+        
+        
+    }else{
+        
+        $query = "INSERT INTO dbtemp.$tmp01 (urutan, bulan, divisi, icabangid, cabang, jabatan, karyawanid, nama, region, jumlah)
+            SELECT 2 as urutan, '$ptgl1' AS bulan, 'CANARY' AS divisi, c.iCabangId, c.nama AS cabang, "
+                . " 'AM' AS jabatan, k.karyawanid, k.nama, c.region, CONVERT(SUM(i.incentive), DEC (0)) AS jumlah "
+                . " FROM ms.incentive_am i "
+                . " LEFT JOIN (SELECT DISTINCT icabangid, am FROM ms.penempatan_marketing pm WHERE bulan = '$ptgl1') pm ON i.karyawanid = pm.am JOIN ms.karyawan k ON i.karyawanid = k.karyawanId "
+                . " JOIN sls.icabang c ON pm.icabangid = c.iCabangId WHERE i.bulan = '$ptgl1' $pfilterincfrom  "
+                . " GROUP BY k.nama, c.region, c.nama";
+        mysqli_query($cnmy, $query);
+        $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; goto hapusdata; }
+
+        $query="INSERT INTO dbtemp.$tmp01 (urutan, bulan, divisi, icabangid, cabang, jabatan, karyawanid, nama, region, jumlah) "
+                . " SELECT 3 as urutan, '$ptgl1' AS bulan, 'CANARY' AS divisi, c.icabangid, c.nama AS cabang, "
+                . " 'DM' AS jabatan, k.karyawanid, k.nama, c.region, CONVERT(SUM(i.incentive), DEC (0)) AS jumlah "
+                . " FROM ms.incentive_dm i "
+                . " LEFT JOIN (SELECT DISTINCT icabangid, dm FROM ms.penempatan_marketing pm WHERE bulan = '$ptgl1') pm ON i.karyawanid = pm.dm "
+                . " JOIN ms.karyawan k ON i.karyawanid = k.karyawanId JOIN sls.icabang c ON pm.icabangid = c.iCabangId "
+                . " WHERE i.bulan = '$ptgl1' $pfilterincfrom  "
+                . " GROUP BY k.nama, c.region, c.nama";
+
+        mysqli_query($cnmy, $query);
+        $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; goto hapusdata; }
+        
+        $query="UPDATE dbtemp.$tmp01 SET divisi='CAN' WHERE IFNULL(divisi,'')='CANARY'";
+        mysqli_query($cnmy, $query);
+        $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; goto hapusdata; }
     
-    $query="INSERT INTO dbtemp.$tmp01 (urutan, bulan, divisi, icabangid, cabang, jabatan, karyawanid, nama, region, jumlah)
-        SELECT 3 as urutan, 
-        '$ptgl1' AS bulan,
-        'CANARY' AS divisi,
-              c.icabangid,
-        c.nama AS cabang,
-        'DM' AS jabatan,
-        k.karyawanid,
-        k.nama,
-        c.region,
-        CONVERT(SUM(i.incentive), DEC (0)) AS jumlah
-      FROM
-        ms.incentive_dm i
-        LEFT JOIN
-          (SELECT DISTINCT
-            icabangid,
-            dm
-          FROM
-            ms.penempatan_marketing pm
-          WHERE bulan = '$ptgl1') pm
-          ON i.karyawanid = pm.dm
-        JOIN ms.karyawan k
-          ON i.karyawanid = k.karyawanId
-        JOIN sls.icabang c
-          ON pm.icabangid = c.iCabangId
-      WHERE i.bulan = '$ptgl1' $pfilterincfrom 
-      GROUP BY k.nama,
-        c.region,
-        c.nama";
-    mysqli_query($cnmy, $query);
-    $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; goto hapusdata; }
+    }
     
-    $query="UPDATE dbtemp.$tmp01 SET divisi='CAN' WHERE IFNULL(divisi,'')='CANARY'";
-    mysqli_query($cnmy, $query);
-    $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; goto hapusdata; }
+    
 }
 
     return $tmp01;

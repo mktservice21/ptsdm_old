219ff9ada619c4c87f22024d599bfb8fbcbad95d
myhuspan <huspannasrulloh@gmail.com>
2021-06-14 16:40:06 +0700

    Update simpanapproveinc.php

--- mysdm/module/incentive/inc_incentivelap/simpanapproveinc.php
+++ mysdm/module/incentive/inc_incentivelap/simpanapproveinc.php
@@ -8,12 +8,13 @@ if ($pmodule=="simpandataapprove") {
     $pbulan=$_POST['ubulan'];
     $pkryid=$_POST['ukaryawan'];
     $pregion=$_POST['uregion'];
+    $papproveby=$_POST['uapvby'];
 
     if (!empty($pbulan) AND !empty($pkryid)) {
         include "../../../config/koneksimysqli_ms.php";
 
-        $query = "INSERT INTO ms.approve_insentif (bulan, karyawanid, `status`)VALUES
-            ('$pbulan', '$pkryid', 'approve')";
+        $query = "INSERT INTO ms.approve_insentif (bulan, karyawanid, `status`, sts_apv)VALUES
+            ('$pbulan', '$pkryid', 'approve', '$papproveby')";
         mysqli_query($cnms, $query); $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "Tidak ada yang diapprove.... $erropesan"; exit; }
 
         mysqli_close($cnms);

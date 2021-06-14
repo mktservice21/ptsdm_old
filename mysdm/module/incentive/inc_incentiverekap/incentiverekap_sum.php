f7db24989f483f079a1961d5fbea22e7d9377f36
myhuspan <huspannasrulloh@gmail.com>
2021-06-14 16:40:15 +0700

    Update incentiverekap_sum.php

--- mysdm/module/incentive/inc_incentiverekap/incentiverekap_sum.php
+++ mysdm/module/incentive/inc_incentiverekap/incentiverekap_sum.php
@@ -3,7 +3,7 @@
 $query = "select 'MR' as sts, karyawanid, jenis, sales, `target`, ach, incentive 
     from ms.incentive_mr where bulan between '$pbln1' AND '$pbln2' ";
 if ($pincfrom=="GSM") $query .= " AND IFNULL(jenis2,'')='GSM' ";
-elseif ($pincfrom=="PM") $query .= " AND IFNULL(jenis2,'') Not In ('GSM', '') ";
+elseif ($pincfrom=="PM") $query .= " AND IFNULL(jenis2,'')='PM' ";
 if (empty($pjabatan) OR $pjabatan=="MR") {
 }else{
     $query .=" AND karyawanid='NON NONE'";
@@ -17,7 +17,7 @@ if (empty($pjabatan) OR $pjabatan=="AM") {
         select 'AM' as sts, karyawanid, jenis, sales, `target`, ach, incentive 
         from ms.incentive_am where bulan between '$pbln1' AND '$pbln2' ";
     if ($pincfrom=="GSM") $query .= " AND IFNULL(jenis2,'')='GSM' ";
-    elseif ($pincfrom=="PM") $query .= " AND IFNULL(jenis2,'') Not In ('GSM', '') ";
+    elseif ($pincfrom=="PM") $query .= " AND IFNULL(jenis2,'')='PM' ";
     mysqli_query($cnms, $query); $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "$erropesan"; goto hapusdata; }
 }
 
@@ -26,7 +26,7 @@ if (empty($pjabatan) OR $pjabatan=="DM") {
         select 'DM' as sts, karyawanid, jenis, sales, `target`, ach, incentive 
         from ms.incentive_dm where bulan between '$pbln1' AND '$pbln2' ";
     if ($pincfrom=="GSM") $query .= " AND IFNULL(jenis2,'')='GSM' ";
-    elseif ($pincfrom=="PM") $query .= " AND IFNULL(jenis2,'') Not In ('GSM', '') ";
+    elseif ($pincfrom=="PM") $query .= " AND IFNULL(jenis2,'')='PM' ";
     mysqli_query($cnms, $query); $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "$erropesan"; goto hapusdata; }
 }
 
@@ -66,6 +66,17 @@ $query .=" GROUP BY 1,2,3";
 $query = "CREATE TEMPORARY TABLE $tmp02 ($query)";
 mysqli_query($cnms, $query);
 $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "$erropesan"; goto hapusdata; }
+
+
+$tmp06 ="dbtemp.tmprptincrkp06_".$puser."_$now$milliseconds";
+
+$query = "select b.divprodid, sum(incentive) as incentive "
+        . " FROM $tmp01 as a JOIN ms.jenisincentivepm as b on a.jenis=b.jenis";
+$query .=" GROUP BY 1";
+$query = "CREATE TEMPORARY TABLE $tmp06 ($query)";
+mysqli_query($cnms, $query);
+$erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo "$erropesan"; goto hapusdata; }
+
 
 //update sales
 if ($pincfrom=="GSM" OR $pincfrom=="ALL") {
@@ -129,6 +140,66 @@ echo "<center>Incentive From $pincfrom</center><br>";
 if (!empty($pjabatan)) {
     echo "<center>Jabatan : $pjabatan</center><br>";
 }
+
+if ($pincfrom=="PM") {
+    
+    echo "<table id='dttable' border='1' cellspacing='0' cellpadding='1' width='50%'>";
+        echo "<thead>";
+        echo "<tr>";
+            echo "<th align='center'>No</th>";
+            echo "<th align='center'>Divisi</th>";
+            echo "<th align='center'>Incentive</th>";
+        echo "</tr>";
+        echo "</thead>";
+        echo "<tbody>";
+        
+        $no_n=1;
+        $ptotalinc_n=0;
+        $query_n = "select * from  $tmp06 order by divprodid";
+        $tampil_n= mysqli_query($cnms, $query_n);
+        while ($nro= mysqli_fetch_array($tampil_n)) {
+            $ndivpord=$nro['divprodid'];
+            $nincentive=$nro['incentive'];
+            
+            
+            $nnamadiv=$ndivpord;
+            if ($ndivpord=="CAN") $nnamadiv="CANARY";
+            if ($ndivpord=="PEACO") $nnamadiv="PEACOCK";
+            if ($ndivpord=="PIGEO") $nnamadiv="PIGEON";
+            
+            $ptotalinc_n=(DOUBLE)$ptotalinc_n+(DOUBLE)$nincentive;
+
+            if ($ppilihrpt!="excel") {
+                $nincentive=number_format($nincentive,0,",",",");
+            }
+            
+            echo "<tr>";
+            echo "<td nowrap>$no_n</td>";
+            echo "<td nowrap>$nnamadiv</td>";
+            echo "<td nowrap align='right'>$nincentive</td>";
+            echo "</tr>";
+
+            $no_n++;
+            
+        }
+        
+        if ($ppilihrpt!="excel") {
+            $ptotalinc_n=number_format($ptotalinc_n,0,",",",");
+        }
+            
+        echo "<tr style='font-weight:bold;'>";
+        echo "<td nowrap></td>";
+        echo "<td nowrap>TOTAL </td>";
+        echo "<td nowrap align='right'>$ptotalinc_n</td>";
+        echo "</tr>";
+            
+        echo "</tbody>";
+
+    echo "</table>";
+
+    echo "<br/>";
+}
+
 echo "<table id='dttable' border='1' cellspacing='0' cellpadding='1' width='100%'>";
     echo "<thead>";
     echo "<tr>";
@@ -209,9 +280,13 @@ echo "<div><b>Approve</b></div>";
 
 echo "<table id='dttable' border='1' cellspacing='0' cellpadding='1'>";
 echo "<tr><th>Nama</th><th>Status Approve</th><th>Tgl. Approve</th></tr>";
+
 $query = "select a.karyawanid, b.nama, a.status as sts, date_format(a.sys_time,'%d/%m/%Y %H:%i:%s') as sys_time 
     from ms.approve_insentif as a JOIN ms.karyawan as b on a.karyawanid=b.karyawanid 
-    WHERE LEFT(bulan,7)='$pfbln' order by a.sys_time";
+    WHERE LEFT(bulan,7)='$pfbln' ";
+if (!empty($pincfrom) AND $pincfrom<>"ALL") $query .=" AND a.sts_apv='$pincfrom' ";
+$query .=" order by a.sys_time";
+
 $tampil=mysqli_query($cnms, $query);
 while ($row=mysqli_fetch_array($tampil)) {
     $pnmapv=$row['nama'];
@@ -312,5 +387,6 @@ hapusdata:
     mysqli_query($cnms, "DROP TEMPORARY TABLE $tmp01");
     mysqli_query($cnms, "DROP TEMPORARY TABLE $tmp02");
     mysqli_query($cnms, "DROP TEMPORARY TABLE $tmp03");
+    mysqli_query($cnms, "DROP TEMPORARY TABLE $tmp06");
     mysqli_close($cnms);
 ?>

25527de1767a557c8759c1c485e78ba30fc7b7e4
myhuspan <huspannasrulloh@gmail.com>
2021-06-29 09:54:07 +0700

    Update ttd_biayaluarkota.php

--- mysdm/module/mod_br_entrybrluarkota/ttd_biayaluarkota.php
+++ mysdm/module/mod_br_entrybrluarkota/ttd_biayaluarkota.php
@@ -301,10 +301,16 @@ $(document).ready(function() {
             if (aq_totuc=="") aq_totuc="0";
             
             if (adainputuc=="0") {
+                if (aq_totuc=="0") {
+                    alert("Anda belum mengisi Form UC...\n\
+Untuk mengisi Biaya Luar Kota, silakan form cuti/uc diisi terlebih dahulu.\n\
+Apabila sudah input, pastikan sudah diapprove atasan"); return false;
+                }
             }else{
                 if (aq_totuc=="0") {
                     alert("Anda belum mengisi Form UC...\n\
-Untuk mengisi HOTEL form cuti harus diisi terlebih dahulu."); return false;
+Untuk mengisi HOTEL form cuti/uc harus diisi terlebih dahulu.\n\
+Apabila sudah input, pastikan sudah diapprove atasan"); return false;
                 }else{
                     if (parseInt(aq_sdh_input)>parseInt(aq_totuc)) {
                         alert("Jumlah hari (HOTEL), melebihi jumlah UC..."); return false;

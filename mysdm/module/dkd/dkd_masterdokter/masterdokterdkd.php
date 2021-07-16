78d93a34ab8f4e7dac3d2dbcfcdd7ef88a7a1fb5
myhuspan <huspannasrulloh@gmail.com>
2021-07-16 08:17:35 +0700

    Update masterdokterdkd.php

--- mysdm/module/dkd/dkd_masterdokter/masterdokterdkd.php
+++ mysdm/module/dkd/dkd_masterdokter/masterdokterdkd.php
@@ -17,7 +17,7 @@
         <div class="title_left">
             <h3>
                 <?PHP
-                $judul="Master Dokter DKU";
+                $judul="Master User DKU";
                 if ($pact=="tambahbaru")
                     echo "Input $judul";
                 elseif ($pact=="editdata")

95c256158c084804ff8f61595082136f868203df
myhuspan <huspannasrulloh@gmail.com>
2021-07-22 16:06:16 +0700

    Update caridataquery.php

--- mysdm/module/laporan/mod_spg_lapgaji/caridataquery.php
+++ mysdm/module/laporan/mod_spg_lapgaji/caridataquery.php
@@ -157,7 +157,7 @@ function CariDataSPGGajiTJ($bulan, $pidcabang, $pnoid, $status, $periodeins) {
     mysqli_query($cnmy, "drop TEMPORARY table $tmp02");
     
     //insentif
-    $query ="select * from dbmaster.t_spg_incentive WHERE REPLACE(inct_bulan,'-','')='$periodeins' AND "
+    $query ="select * from fe_it.t_spg_incentive WHERE REPLACE(inct_bulan,'-','')='$periodeins' AND "
             . " id_spg IN (SELECT distinct id_spg FROM $tmp01)";
     $query = "create TEMPORARY table $tmp02 ($query)"; 
     mysqli_query($cnmy, $query);
@@ -415,7 +415,7 @@ function CariDataSPG($bulan, $pidcabang, $pnoid, $status, $periodeins) {
     mysqli_query($cnmy, "drop TEMPORARY table $tmp04");
     
     //insentif
-    $query ="select * from dbmaster.t_spg_incentive WHERE REPLACE(inct_bulan,'-','')='$periodeins' AND "
+    $query ="select * from fe_it.t_spg_incentive WHERE REPLACE(inct_bulan,'-','')='$periodeins' AND "
             . " id_spg IN (SELECT distinct id_spg FROM $tmp01)";
     $query = "create TEMPORARY table $tmp02 ($query)"; 
     mysqli_query($cnmy, $query);

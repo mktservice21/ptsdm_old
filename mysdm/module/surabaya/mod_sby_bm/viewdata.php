6ff143231d30dd7a92b1b3ef7e7c9be1d4ce479b
myhuspan <huspannasrulloh@gmail.com>
2021-07-23 15:00:42 +0700

    Update viewdata.php

--- mysdm/module/surabaya/mod_sby_bm/viewdata.php
+++ mysdm/module/surabaya/mod_sby_bm/viewdata.php
@@ -49,6 +49,38 @@ if ($_GET['module']=="viewcoadivisi"){
         echo "<option value=''>-- All --</option>";
     }
     
+    mysqli_close($cnmy);
+}elseif ($_GET['module']=="viwewdatacabang"){
+    include "../../../config/koneksimysqli.php";
+    $mydivisi = $_POST['udivi'];
+    
+    if ($mydivisi=="OTC") {
+        $query = "SELECT * FROM (select icabangid_o as icabangid, nama as nama_cabang FROM MKT.icabang_o WHERE aktif<>'N' ";
+        $query .= "UNION ";
+        $query .= "select cabangid_ho as icabangid, nama as nama_cabang FROM dbmaster.cabang_otc WHERE aktif<>'N') as tcab ";
+        $query .= " ORDER BY nama_cabang";
+        
+        echo "<option value='' selected>-- All CHC --</option>";
+    }else{
+        $query = "select iCabangId as icabangid, nama as nama_cabang FROM MKT.icabang WHERE aktif<>'N' ";
+        $query .= " AND LEFT(nama,5) NOT IN ('OTC -', 'PEA -') ";
+        $query .= " ORDER BY nama";
+        
+        echo "<option value='' selected>-- All Ethical --</option>";
+    }
+
+    $tampil = mysqli_query($cnmy, $query);
+    while ($z= mysqli_fetch_array($tampil)) {
+        $nnicabid=$z['icabangid'];
+        $nnicabnm=$z['nama_cabang'];
+
+        if ($nnicabid==$picabangid)
+            echo "<option value='$nnicabid' selected>$nnicabnm - $nnicabid</option>";
+        else
+            echo "<option value='$nnicabid'>$nnicabnm - $nnicabid</option>";
+    }
+                                            
+    mysqli_close($cnmy);
 }elseif ($_GET['module']=="xxx"){
     
 }

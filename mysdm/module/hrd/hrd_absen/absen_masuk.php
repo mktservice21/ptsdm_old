7192e2b9141e2005a9c313abeb2e5d31a618c638
myhuspan <huspannasrulloh@gmail.com>
2021-08-03 14:09:34 +0700

    Create absen_masuk.php

---
+++ mysdm/module/hrd/hrd_absen/absen_masuk.php
@@ -0,0 +1,18 @@
+
+
+<div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+    <div class="tile-stats">
+
+        <div class="icon">
+            <i class="fa fa-caret-square-o-right"></i>
+
+        </div>
+        <div class="count">08:00</div>
+        <h3><button type='button' class='btn btn-default' id='ibuttonsave' data-toggle='modal' data-target='#myModal' onclick='ShowFormAbsen("1")'>Absen Masuk</button></h3>
+        <p>Silakan klik tombol...</p>
+    </div>
+</div>
+
+<script>
+    
+</script>

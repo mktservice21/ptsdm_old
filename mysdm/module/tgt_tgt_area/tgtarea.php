03616c6c6d9b3caf40a3f517d018de6576cda2c9
myhuspan <huspannasrulloh@gmail.com>
2021-08-04 13:44:15 +0700

    Update tgtarea.php

--- mysdm/module/tgt_tgt_area/tgtarea.php
+++ mysdm/module/tgt_tgt_area/tgtarea.php
@@ -247,6 +247,7 @@
                                 var iMonth = $("#ui-datepicker-div .ui-datepicker-month :selected").val();
                                 var iYear = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
                                 $(this).datepicker('setDate', new Date(iYear, iMonth, 1));
+                                ShowDataArea();
                                 Kosongkan();
                             },
 

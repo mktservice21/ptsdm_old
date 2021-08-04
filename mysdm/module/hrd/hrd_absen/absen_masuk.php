242f5c259f2acce38b9e7693253aeae21d96266e
myhuspan <huspannasrulloh@gmail.com>
2021-08-04 13:43:27 +0700

    Update absen_masuk.php

--- mysdm/module/hrd/hrd_absen/absen_masuk.php
+++ mysdm/module/hrd/hrd_absen/absen_masuk.php
@@ -1,5 +1,5 @@
 
-
+<div class='modal fade' id='myModalAbsen' role='dialog' class='no-print'></div>
 <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
     <div class="tile-stats">
 
@@ -8,11 +8,24 @@
 
         </div>
         <div class="count">08:00</div>
-        <h3><button type='button' class='btn btn-default' id='ibuttonsave' data-toggle='modal' data-target='#myModal' onclick='ShowFormAbsen("1")'>Absen Masuk</button></h3>
-        <p>Silakan klik tombol...</p>
+        <h3>
+            <!--<button type='button' class='btn btn-default' id='ibuttonsave' data-toggle='modal' data-target='#myModalAbsen' onclick='ShowFormAbsen("1")'>Absen Masuk</button>-->
+            <a href='?module=hrdabsenmasuk&act=absenmasuk&idmenu=522&kriteria=Y' class='btn btn-default' id='ibuttonsave'>Absen Masuk</a>
+        </h3>
+        <p>&nbsp;</p>
     </div>
 </div>
 
 <script>
     
+    function ShowFormAbsen(sKey) {
+        $.ajax({
+            type:"post",
+            url:"module/hrd/hrd_absen/upload_webcam.php?module=uploadwebcame",
+            data:"ukey="+sKey,
+            success:function(data){
+                $("#myModalAbsen").html(data);
+            }
+        });
+    }
 </script>

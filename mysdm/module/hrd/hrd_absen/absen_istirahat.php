e23d3ecf1fd7696d8c92dd9e63ec8f5529c75d22
myhuspan <huspannasrulloh@gmail.com>
2021-08-04 21:28:23 +0700

    Update absen_istirahat.php

--- mysdm/module/hrd/hrd_absen/absen_istirahat.php
+++ mysdm/module/hrd/hrd_absen/absen_istirahat.php
@@ -1,22 +1,29 @@
-<?PHP
+<?PHP       
 $ptglnwoabsmsk=date("Y-m-d");
 $pkaryawanabsmsk="";
-if (isset($_SESSION['IDCARD'])) $pkaryawanabsmsk=$_SESSION['IDCARD'];
+$pkryjamist="00:00";
+$pkryjammskist="00:00";
+
+if (isset($_SESSION['IDCARD']))         $pkaryawanabsmsk=$_SESSION['IDCARD'];
+if (isset($_SESSION['J_ISTIRAHAT']))    $pkryjamist=$_SESSION['J_ISTIRAHAT'];
+if (isset($_SESSION['J_MSKISTIRAHAT'])) $pkryjammskist=$_SESSION['J_MSKISTIRAHAT'];
 
 $query = "select jam FROM hrd.t_absen WHERE karyawanid='$pkaryawanabsmsk' AND tanggal='$ptglnwoabsmsk' AND kode_absen='3'";
 $tampilabsist=mysqli_query($cnmy, $query);
 $irow= mysqli_fetch_array($tampilabsist);
-$pjamistabs="<div class='count'>".$irow['jam']."</div>";
-if (empty($pjamistabs)) {
-    $pjamistabs="<div class='count'>12:00</div>";
+$pjmabsen_i=$irow['jam'];
+$pjamistabs="<div class='count'>".$pjmabsen_i."</div>";
+if (empty($pjmabsen_i)) {
+    $pjamistabs="<div class='count' style='color:#C0C0C0'>$pkryjamist</div>";
 }
 
 $query = "select jam FROM hrd.t_absen WHERE karyawanid='$pkaryawanabsmsk' AND tanggal='$ptglnwoabsmsk' AND kode_absen='4'";
 $tampilabsist_msk=mysqli_query($cnmy, $query);
 $irow_m= mysqli_fetch_array($tampilabsist_msk);
-$pjamistabs_msk="<div class='count'>".$irow_m['jam']."</div>";
-if (empty($pjamistabs_msk)) {
-    $pjamistabs_msk="<div class='count'>13:00</div>";
+$pjmabsen_im=$irow_m['jam'];
+$pjamistabs_msk="<div class='count'>".$pjmabsen_im."</div>";
+if (empty($pjmabsen_im)) {
+    $pjamistabs_msk="<div class='count' style='color:#C0C0C0'>$pkryjammskist</div>";
 }
 
 $platitude_home="";

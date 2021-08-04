b2dc001deacfe9aa498b70db33b71e2057294511
myhuspan <huspannasrulloh@gmail.com>
2021-08-04 20:42:54 +0700

    Update absen_masuk.php

--- mysdm/module/hrd/hrd_absen/absen_masuk.php
+++ mysdm/module/hrd/hrd_absen/absen_masuk.php
@@ -1,4 +1,15 @@
-
+<?PHP
+$ptglnwoabsmsk=date("Y-m-d");
+$pkaryawanabsmsk="";
+if (isset($_SESSION['IDCARD'])) $pkaryawanabsmsk=$_SESSION['IDCARD'];
+$query = "select jam FROM hrd.t_absen WHERE karyawanid='$pkaryawanabsmsk' AND tanggal='$ptglnwoabsmsk' AND kode_absen='1'";
+$tampilabsmsk=mysqli_query($cnmy, $query);
+$mrow= mysqli_fetch_array($tampilabsmsk);
+$pjammasukabs="<div class='count'>".$mrow['jam']."</div>";
+if (empty($pjammasukabs)) {
+    $pjammasukabs="<div class='count'>08:00</div>";
+}
+?>
 <div class='modal fade' id='myModalAbsen' role='dialog' class='no-print'></div>
 <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
     <div class="tile-stats">
@@ -7,7 +18,7 @@
             <i class="fa fa-caret-square-o-right"></i>
 
         </div>
-        <div class="count">08:00</div>
+        <?PHP echo $pjammasukabs; ?>
         <h3>
             <!--<button type='button' class='btn btn-default' id='ibuttonsave' data-toggle='modal' data-target='#myModalAbsen' onclick='ShowFormAbsen("1")'>Absen Masuk</button>-->
             <a href='?module=hrdabsenmasuk&act=absenmasuk&idmenu=522&kriteria=Y' class='btn btn-default' id='ibuttonsave'>Absen Masuk</a>

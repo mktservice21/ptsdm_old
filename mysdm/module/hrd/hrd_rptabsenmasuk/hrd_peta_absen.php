44936617494965e299b16309186eebfb6163e755
myhuspan <huspannasrulloh@gmail.com>
2021-08-06 18:02:33 +0700

    Create hrd_peta_absen.php

---
+++ mysdm/module/hrd/hrd_rptabsenmasuk/hrd_peta_absen.php
@@ -0,0 +1,7 @@
+<?PHP
+$platitude=$_POST['ulat'];
+$plongitude=$_POST['ulong'];
+$pnama_karyawan=$_POST['unamakry'];
+?>
+
+<iframe width="100%" height="500" src="https://maps.google.com/maps?q=<?php echo $platitude; ?>,<?php echo $plongitude; ?>&output=embed"></iframe>

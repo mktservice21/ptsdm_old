afcab4ac02f99087ae8447ba065dfc0de59b1d92
myhuspan <huspannasrulloh@gmail.com>
2021-08-06 17:06:18 +0700

    Update aksi_rptabsenmasuk.php

--- mysdm/module/hrd/hrd_rptabsenmasuk/aksi_rptabsenmasuk.php
+++ mysdm/module/hrd/hrd_rptabsenmasuk/aksi_rptabsenmasuk.php
@@ -24,6 +24,8 @@
     $tmp01 =" dbtemp.tmprptabsmasukimg01_".$userid."_$now ";
     $tmp02 =" dbtemp.tmprptabsmasukimg02_".$userid."_$now ";
     $tmp03 =" dbtemp.tmprptabsmasukimg03_".$userid."_$now ";
+    $tmp04 =" dbtemp.tmprptabsmasukimg04_".$userid."_$now ";
+    $tmp05 =" dbtemp.tmprptabsmasukimg05_".$userid."_$now ";
     
     
     $ppilihsts = strtoupper($_POST['eket']);
@@ -34,11 +36,21 @@
     $pbulan1= date("Y-m-01", strtotime($mytgl1));
     $pbulan2= date("Y-m-t", strtotime($mytgl1));
     
+    $pbulan = date('Y-m', strtotime($mytgl1));
+    $nbln = date('m', strtotime($mytgl1));
+    $nthn = date('Y', strtotime($mytgl1));
+    $ptglakhir = date('t', strtotime($mytgl1));
+    
     
     $query = "select a.idabsen, a.karyawanid, b.nama as nama_karyawan, a.kode_absen, a.tanggal, a.jam, a.l_latitude, a.l_longitude "
             . " FROM hrd.t_absen as a JOIN hrd.karyawan as b on a.karyawanid=b.karyawanId "
-            . " WHERE a.kode_absen='$pstsapv' AND "
+            . " WHERE "
             . " a.tanggal BETWEEN '$pbulan1' AND '$pbulan2' ";
+    if ($ppilihsts=="00") {
+        $query .= " AND a.kode_absen IN ('1', '2') ";
+    }else{
+        $query .= " AND a.kode_absen='$pstsapv' ";
+    }
     if (!empty($pkaryawanid)) {
         $query .=" AND a.karyawanid='$pkaryawanid' ";
     }
@@ -48,7 +60,12 @@
     
     $query = "select distinct a.idabsen, a.kode_absen, a.nama FROM dbimages2.img_absen as a "
             . " JOIN $tmp01 as b on a.idabsen=b.idabsen AND a.kode_absen=b.kode_absen "
-            . " WHERE a.kode_absen='$pstsapv'";
+            . " WHERE 1=1 ";
+    if ($ppilihsts=="00") {
+        $query .= " AND a.kode_absen IN ('1', '2') ";
+    }else{
+        $query .= " AND a.kode_absen='$pstsapv' ";
+    }
     $query = "create TEMPORARY table $tmp02 ($query)"; 
     mysqli_query($cnmy, $query);
     $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; goto hapusdata; }
@@ -62,7 +79,94 @@
     mysqli_query($cnmy, $query); $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; goto hapusdata; }
     
     
-    echo "<div class='row'>";
+    $query = "select * from $tmp01 WHERE kode_absen='2'";
+    $query = "create TEMPORARY table $tmp03 ($query)"; 
+    mysqli_query($cnmy, $query);
+    $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; goto hapusdata; }
+    
+    
+    $query = "DELETE FROM $tmp01 WHERE kode_absen<>'1'"; 
+    mysqli_query($cnmy, $query); $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; goto hapusdata; }
+    
+    
+    $query = "ALTER TABLE $tmp01 ADD COLUMN jam_p VARCHAR(50), ADD COLUMN nama_images_p VARCHAR(100), ADD COLUMN l_latitude_p VARCHAR(200), ADD COLUMN l_longitude_p VARCHAR(200)";
+    mysqli_query($cnmy, $query); $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; goto hapusdata; }
+    
+    $query = "UPDATE $tmp01 as a JOIN (select DISTINCT karyawanid, tanggal, nama_images, l_latitude, l_longitude, jam FROM $tmp03 WHERE "
+            . " kode_absen='2') as b on a.karyawanid=b.karyawanid AND a.tanggal=b.tanggal SET "
+            . " a.jam_p=b.jam, a.nama_images_p=b.nama_images, a.l_latitude_p=b.l_latitude, a.l_longitude_p=b.l_longitude";
+    mysqli_query($cnmy, $query); $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; goto hapusdata; }
+    
+    
+    
+    
+    
+    //LIBUR dan JUMLAH HARI KERJA
+    $query = "CREATE TEMPORARY TABLE $tmp04 (tanggal DATE, libur VARCHAR(1) DEFAULT 'N', libur_cmasal VARCHAR(1) DEFAULT 'N')";
+    mysqli_query($cnmy, $query); $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; goto hapusdata; }
+
+    unset($pinsert_data_detail);//kosongkan array
+    $psimpandata=false;
+    for($ix=1; $ix<=(INT)$ptglakhir;$ix++) {
+        $pntgl=$ix;
+        if (strlen($pntgl)<=1) $pntgl="0".$ix;
+
+        $phari = strtoupper(date('l', strtotime($nthn."-".$nbln."-".$pntgl)));
+
+        $npltanggal=$pbulan."-".$pntgl;
+
+        $pcollibur="";
+        $plibur="N";
+        if ($phari=="SATURDAY") { $plibur="Y"; $pcollibur="style='background-color:#ff9999'"; }
+        elseif ($phari=="SUNDAY") { $plibur="Y";$pcollibur="style='background-color:#ff3333'"; }
+
+        $pinsert_data_detail[] = "('$npltanggal', '$plibur')";
+
+        $psimpandata=true;
+        //echo "$pntgl : $phari dan $npltanggal<br/>";
+
+    }
+    
+    
+    if ($psimpandata==true) {
+        
+        
+        $query = "INSERT INTO $tmp04 (tanggal, libur) VALUES ".implode(', ', $pinsert_data_detail);
+        mysqli_query($cnmy, $query);
+        $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) {  goto hapusdata; }
+
+        
+        //CUSTI MASAL
+
+        $query = "SELECT DISTINCT b.tanggal FROM hrd.t_cuti0 as a "
+                . " JOIN hrd.t_cuti1 as b on a.idcuti=b.idcuti where a.id_jenis='00' "
+                . " AND a.karyawanid IN ('ALL', 'ALLHO') AND IFNULL(a.stsnonaktif,'')<>'Y' "
+                . " AND LEFT(b.tanggal,7)= '$pbulan'";
+        $query = "create TEMPORARY table $tmp05 ($query)";
+        mysqli_query($cnmy, $query);
+        $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; goto hapusdata; }
+
+
+                $query = "INSERT INTO $tmp05 (tanggal)values('2021-08-10')";
+                //mysqli_query($cnmy, $query); $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; goto hapusdata; }
+                $query = "INSERT INTO $tmp05 (tanggal)values('2021-08-17')";
+                //mysqli_query($cnmy, $query); $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; goto hapusdata; }
+                
+        $query = "UPDATE $tmp04 as a JOIN $tmp05 as b on a.tanggal=b.tanggal SET a.libur_cmasal='Y'";
+        mysqli_query($cnmy, $query); $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; goto hapusdata; }
+
+    }else{
+        $query = "INSERT INTO $tmp04(tanggal, libur) SELECT distinct tanggal, 'N' as libur FROM  $tmp01";
+        mysqli_query($cnmy, $query); $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo $erropesan; goto hapusdata; }
+    }
+    
+    //END LIBUR dan JUMLAH HARI KERJA
+
+        
+        
+        
+        
+    echo "<div hidden class='row'>";
     
         echo "<div class='col-md-12 col-sm-12 col-xs-12'>";
             echo "<div class='x_panel'>";
@@ -82,52 +186,177 @@
     
     echo "<div class=''>";
         
-        $query = "select * from $tmp01 ORDER BY tanggal, nama_karyawan, karyawanid";
-        $tampil=mysqli_query($cnmy, $query);
-        while ($row= mysqli_fetch_array($tampil)) {
-            $nnmkaryawan=$row['nama_karyawan'];
-            $ntgl=$row['tanggal'];
-            $njam=$row['jam'];
-            $nnamaimg=$row['nama_images'];
-            $nlat=$row['l_latitude'];
-            $nlong=$row['l_longitude'];
+        $query = "select distinct tanggal, libur, libur_cmasal FROM $tmp04 Order by tanggal";
+        $tampil0=mysqli_query($cnmy, $query);
+        while ($row0= mysqli_fetch_array($tampil0)) {
+            $nlibur=$row0['libur'];
+            $nliburcmasal=$row0['libur_cmasal'];
             
-            $ntanggal= date("d/m/Y", strtotime($ntgl));
+            $sel_tgl=$row0['tanggal'];
+            $ntglday= date("d", strtotime($sel_tgl));
             
-            $folderfotofileabs="images/foto_absen/".$nnamaimg;
+            $pbtn_tgl_warna=" btn btn-info ";
+            if ($nlibur=="Y" OR $nliburcmasal=="Y") $pbtn_tgl_warna=" btn btn-danger ";
             
-            $pnamafiles_img="kosong";
-            if (!file_exists($folderfotofileabs)) {
-                $pnamafiles_img="<img src='$folderfotofileabs' width='70px' height='75px' />";
-            }
-            
-            echo "<div class='animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12'>";
-                echo "<div class='tile-stats'>";
-                    echo "<div class='icon'>";
-                        //echo "<i class='glyphicon glyphicon-check'></i>";
-                        echo "$pnamafiles_img";
+            $query = "select DISTINCT karyawanid, nama_karyawan from $tmp01 ORDER BY nama_karyawan, karyawanid";
+            $tampil1=mysqli_query($cnmy, $query);
+            while ($row1= mysqli_fetch_array($tampil1)) {
+                $nidkaryawan=$row1['karyawanid'];
+                $nnmkaryawan=$row1['nama_karyawan'];
+                
+                if (!empty($pkaryawanid)) $nnmkaryawan="";
+                
+                $query = "select * from $tmp01 WHERE tanggal='$sel_tgl' AND karyawanid='$nidkaryawan' ORDER BY tanggal, nama_karyawan, karyawanid";
+                $tampil=mysqli_query($cnmy, $query);
+                $ketemu= mysqli_num_rows($tampil);
+
+                if ((INT)$ketemu<=0) {
+                    $folderfotofileabs_n="images/foto_absen/none_foto.png";
+                    $pnamafiles_img_n="<img src='$folderfotofileabs_n' width='70px' height='75px' />";
+                    echo "<div class='animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12'>";
+                        echo "<div class='tile-stats'>";
+
+                            echo "<p style='color:#000; text-decoration: underline; font-weight:bold; font-size:12px; margin-bottom:5px'>$nnmkaryawan</p>";
+                            echo "<div class='col-md-4'>";
+                                echo "$pnamafiles_img_n";
+                                echo "<h3 style='color:#fff; font-size:20px; margin-top:5px'>00:00</h3>";
+                            echo "</div>";
+
+                            echo "<div class='col-md-4'>";
+                                echo "$pnamafiles_img_n";
+                                echo "<h3 style='color:#fff; font-size:20px; margin-top:5px'>00:00</h3>";
+                            echo "</div>";
+
+                            echo "<div class='icon'>";
+                                echo "<a href='#' class='$pbtn_tgl_warna' >$ntglday</a>";
+                            echo "</div>";
+
+                            echo "<div class='clearfix'></div>";
+
+                            echo "<h3 style='margin-top:5px;'>";
+                                echo "<a href='#' class='btn btn-default btn-xs' style='color:#fff; border-color:#fff;'>&nbsp; &nbsp; </a>";
+                            echo "</h3>";
+
+
+                        echo "</div>";
                     echo "</div>";
 
-                    echo "<div class='count'>$njam</div>";
+                }
 
-                    echo "<h3>";
-                        echo "<a href='#' class='btn btn-default' onclick=\"initMap('$nlat', '$nlong', '$nnmkaryawan');\">$ntanggal</a>";
-                    echo "<h3>";
-                    echo "<p>$nnmkaryawan</p>";
-                echo "</div>";
-            echo "</div>";
-        
+                while ($row= mysqli_fetch_array($tampil)) {
+                    $nkodeabsen=$row['kode_absen'];
+                    $nnmkaryawan=$row['nama_karyawan'];
+                    $ntgl=$row['tanggal'];
+                    $njam=$row['jam'];
+                    $njam_p=$row['jam_p'];
+                    $nnamaimg=$row['nama_images'];
+                    $nnamaimg_p=$row['nama_images_p'];
+                    $nlat=$row['l_latitude'];
+                    $nlat_p=$row['l_latitude_p'];
+                    $nlong=$row['l_longitude'];
+                    $nlong_p=$row['l_longitude_p'];
+
+                    $ntglday= date("d", strtotime($ntgl));
+                    $ntanggal= date("d/m/Y", strtotime($ntgl));
+
+                    $folderfotofileabs="images/foto_absen/".$nnamaimg;
+                    $folderfotofileabs_p="images/foto_absen/".$nnamaimg_p;
+
+                    $pnamafiles_img="kosong";
+                    if (!file_exists($folderfotofileabs) AND !empty($nnamaimg)) {
+                        $pnamafiles_img="<img src='$folderfotofileabs' width='70px' height='75px' class='zoomimg' />";
+                    }
+
+                    $pnamafiles_img_p="";
+                    if (!file_exists($folderfotofileabs_p) AND !empty($nnamaimg_p)) {
+                        $pnamafiles_img_p="<img src='$folderfotofileabs_p' width='70px' height='75px' class='zoomimg' />";
+                    }
+
+                    /*
+                    echo "<div class='animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12'>";
+                        echo "<div class='tile-stats'>";
+                            echo "<div class='icon'>";
+                                //echo "<i class='glyphicon glyphicon-check'></i>";
+                                echo "$pnamafiles_img";
+                            echo "</div>";
+
+                            echo "<div class='count'>$njam</div>";
+
+                            echo "<h3>";
+                                echo "<a href='#' class='btn btn-default' onclick=\"initMap('$nlat', '$nlong', '$nnmkaryawan');\">$ntanggal</a>";
+                            echo "<h3>";
+                            echo "<p>$nnmkaryawan</p>";
+                        echo "</div>";
+                    echo "</div>";
+                    */
+                    
+                    if (!empty($pkaryawanid)) $nnmkaryawan="";
+                    
+                    echo "<div class='animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12'>";
+                        echo "<div class='tile-stats'>";
+
+                            echo "<p style='color:#000; text-decoration: underline; font-weight:bold; font-size:12px; margin-bottom:5px'>$nnmkaryawan</p>";
+                            echo "<div class='col-md-4'>";
+                                echo "$pnamafiles_img";
+                                echo "<h3 style='color:#000; font-size:20px; margin-top:5px'>$njam</h3>";
+                            echo "</div>";
+
+                            echo "<div class='col-md-4'>";
+                                echo "$pnamafiles_img_p";
+                                echo "<h3 style='color:#000; font-size:20px; margin-top:5px'>$njam_p</h3>";
+                            echo "</div>";
+
+                            echo "<div class='icon'>";
+                                echo "<a href='#' class='$pbtn_tgl_warna' >$ntglday</a>";
+                            echo "</div>";
+
+                            echo "<div class='clearfix'></div>";
+
+                            echo "<h3 style='margin-top:5px;'>";
+                                echo "<a href='#' class='btn btn-default btn-xs' onclick=\"initMap('$nlat', '$nlong', '$nnmkaryawan');\">Lihat Peta Lokasi</a>";
+                            echo "</h3>";
+
+
+                        echo "</div>";
+                    echo "</div>";
+
+                }
+                
+            }
+            
         }
     
     echo "</div>";
 ?>
 
+<style>
+    .zoomimg {
+      transition: transform .2s; /* Animation */
+      margin: 0 auto;
+    }
+
+    .zoomimg:hover {
+        -webkit-transform:scale(3.5); /* Safari and Chrome */
+        -moz-transform:scale(3.5); /* Firefox */
+        -ms-transform:scale(3.5); /* IE 9 */
+        -o-transform:scale(3.5); /* Opera */
+        transform:scale(3.5);
+        
+        /*display:block;
+        position:fixed;*/
+        position:relative;
+        z-index: 999;
+        
+    }
+</style>
 
 <?PHP
 hapusdata:
     mysqli_query($cnmy, "drop TEMPORARY table IF EXISTS $tmp01");
     mysqli_query($cnmy, "drop TEMPORARY table IF EXISTS $tmp02");
     mysqli_query($cnmy, "drop TEMPORARY table IF EXISTS $tmp03");
+    mysqli_query($cnmy, "drop TEMPORARY table IF EXISTS $tmp04");
+    mysqli_query($cnmy, "drop TEMPORARY table IF EXISTS $tmp05");
     
     mysqli_close($cnmy);
 ?>

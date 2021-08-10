e3b7d8e59c38c1195d2a417e4b25fd76d64744ca
myhuspan <huspannasrulloh@gmail.com>
2021-08-10 17:18:53 +0700

    Update viewdatahrd.php

--- mysdm/module/hrd/viewdatahrd.php
+++ mysdm/module/hrd/viewdatahrd.php
@@ -5,52 +5,87 @@ if (isset($_GET['module'])) $pmodule=$_GET['module'];
 
 
 
+if (!isset($_SESSION['IDCARD'])) {
+    echo "HARUS LOGIN ULANG...";
+    exit;
+}
+        
 if ($pmodule=="carikaryawanbyatasan") {
-    
-    $fkaryawan=$_SESSION['IDCARD'];
-    $fgroupid=$_SESSION['GROUP'];
         
     include "../../config/koneksimysqli.php";
     
-    $pidatasan=$_POST['uidatasan'];
-    
-    $query = "select karyawanId as karyawanid, nama as nama From hrd.karyawan
-        WHERE 1=1 ";
-    $query .= " AND ( IFNULL(tglkeluar,'')='' OR IFNULL(tglkeluar,'0000-00-00')='0000-00-00' ) ";
-    if (!empty($pidatasan)) {
-        $query .= " AND nama NOT IN ('ACCOUNTING') AND karyawanId NOT IN ('0000002200', '0000002083')";
-        $query .= " AND ( atasanId='$pidatasan' OR atasanId2='$pidatasan' OR karyawanId='$pidatasan' ) ";
-    }else{
-        if ($fgroupid=="24" OR $fgroupid=="1" OR $fgroupid=="57" OR $fgroupid=="47" OR $fgroupid=="29" OR $fgroupid=="46") {
-            $query .= " AND nama NOT IN ('ACCOUNTING') AND karyawanId NOT IN ('0000002200', '0000002083')";
+    $fkaryawan=$_SESSION['IDCARD'];
+    $fnmkaryawan=$_SESSION['NAMALENGKAP'];
+    $fgroupid=$_SESSION['GROUP'];
+
+    $pleader=false;
+    $patasantanpaleader=false;
+    $query = "select karyawanId, leader from dbmaster.t_karyawan_posisi WHERE karyawanId='$fkaryawan'";
+    $tampil= mysqli_query($cnmy, $query);
+    $ketemu=mysqli_num_rows($tampil);
+    if ((INT)$ketemu>0) {
+        $row= mysqli_fetch_array($tampil);
+        $nldr_=$row['leader'];
+
+        if ($nldr_=="Y") {
+            $pleader=true;
+            $patasantanpaleader=true;
         }else{
-            $query .= " AND karyawanId='$fkaryawan'";
+            $query_a = "select karyawanId FROM hrd.karyawan WHERE ( atasanId='$fkaryawan' OR atasanId2='$fkaryawan' ) "
+                    . " AND ( IFNULL(tglkeluar,'')='' OR IFNULL(tglkeluar,'0000-00-00')='0000-00-00' ) "
+                    . " AND IFNULL(aktif,'')<>'N'";
+            $tampil_a= mysqli_query($cnmy, $query_a);
+            $ketemu_a=mysqli_num_rows($tampil_a);
+            if ((INT)$ketemu_a>0) {
+                $patasantanpaleader=true;
+            }
         }
     }
+
+    $pbolehbukall=false;
+    if ($fgroupid=="24" OR $fgroupid=="1" OR $fgroupid=="X57" OR $fgroupid=="47" OR $fgroupid=="29" OR $fgroupid=="46") {
+        $pbolehbukall=true;
+    }
     
-    $query .=" AND LEFT(nama,4) NOT IN ('NN -', 'DR -', 'DM -', 'BDG ', 'OTH.', 'TO. ', 'BGD-', 'JKT ', 'MR -', 'MR S')  "
-            . " and LEFT(nama,7) NOT IN ('NN DM - ', 'MR SBY1')  "
-            . " and LEFT(nama,3) NOT IN ('TO.', 'TO-', 'DR ', 'DR-', 'JKT', 'NN-', 'TO ') "
-            . " AND LEFT(nama,5) NOT IN ('OTH -', 'NN AM', 'NN DR', 'TO - ', 'SBY -', 'RS. P') "
-            . " AND LEFT(nama,6) NOT IN ('SBYTO-', 'MR SBY') ";
     
-    $query .= " AND karyawanId IN (select DISTINCT IFNULL(karyawanId,'') FROM dbmaster.t_karyawan_posisi WHERE IFNULL(`ho`,'')='Y')";
     
-    $query .= " ORDER BY nama";
+    $pidatasan=$_POST['uidatasan'];
+    
+    
+    $query = "select a.karyawanId as karyawanid, a.nama as nama_karyawan From hrd.karyawan as a "
+            . " JOIN dbmaster.t_karyawan_posisi as b on a.karyawanId=b.karyawanId "
+            . " WHERE 1=1 ";
+    $query .= " AND ( IFNULL(a.tglkeluar,'')='' OR IFNULL(a.tglkeluar,'0000-00-00')='0000-00-00' ) ";
+    $query .= " AND IFNULL(b.`ho`,'')='Y' ";
+    if (!empty($pidatasan)) {
+        $query .= " AND ( a.atasanId='$pidatasan' OR a.atasanId2='$pidatasan' OR a.karyawanId='$pidatasan' ) ";
+    }else{
+        if ($pbolehbukall==true) {
+        }else{
+            if ($pleader==true OR $patasantanpaleader==true) {
+                $query .= " AND (a.karyawanId='$fkaryawan' OR a.atasanId='$fkaryawan' OR a.atasanId2='$fkaryawan' ) ";
+            }else{
+                $query .= " AND a.karyawanId='$fkaryawan'";
+            }
+        }
+    }
+    $query .= " ORDER BY a.nama";
 
 
     $tampil = mysqli_query($cnmy, $query);
 
     $ketemu= mysqli_num_rows($tampil);
-    
-    if ($ketemu==0) echo "<option value='' selected>-- Pilih --</option>";
-    
+
     while ($z= mysqli_fetch_array($tampil)) {
         $pkaryid=$z['karyawanid'];
-        $pkarynm=$z['nama'];
+        $pkarynm=$z['nama_karyawan'];
         $pkryid=(INT)$pkaryid;
-        echo "<option value='$pkaryid'>$pkarynm ($pkryid)</option>";
+        if ($pkaryid==$fkaryawan)
+            echo "<option value='$pkaryid' selected>$pkarynm ($pkryid)</option>";
+        else
+            echo "<option value='$pkaryid'>$pkarynm ($pkryid)</option>";
     }
+                                
                                                             
     mysqli_close($cnmy);
 }

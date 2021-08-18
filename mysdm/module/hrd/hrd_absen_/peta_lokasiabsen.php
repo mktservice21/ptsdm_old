311e4c988431c8774a7d41f7fe98983eeb768937
myhuspan <huspannasrulloh@gmail.com>
2021-08-18 08:36:48 +0700

    Create peta_lokasiabsen.php

---
+++ mysdm/module/hrd/hrd_absen_/peta_lokasiabsen.php
@@ -0,0 +1,6 @@
+<?PHP
+$platitude=$_POST['ulat'];
+$plongitude=$_POST['ulong'];
+?>
+
+<iframe width="100%" height="250" src="https://maps.google.com/maps?q=<?php echo $platitude; ?>,<?php echo $plongitude; ?>&output=embed"></iframe>

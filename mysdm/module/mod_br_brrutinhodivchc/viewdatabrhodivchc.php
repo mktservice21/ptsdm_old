9588279b40bdddf14bd60ea467a10672f5bdf0b6
myhuspan <huspannasrulloh@gmail.com>
2021-09-08 08:04:31 +0700

    Update viewdatabrhodivchc.php

--- mysdm/module/mod_br_brrutinhodivchc/viewdatabrhodivchc.php
+++ mysdm/module/mod_br_brrutinhodivchc/viewdatabrhodivchc.php
@@ -8,14 +8,28 @@ if ($pmodule=="cekdatasudahada") {
     $pid=$_POST['uid'];
     $pidkar=$_POST['ukry'];
     $pbln=$_POST['ubln'];
+    $ptgl_pl1=$_POST['up01'];
+    $ptgl_pl2=$_POST['up02'];
     $pkdperiode=$_POST['ukdperiode'];
     
     $pbulan= date("Ym", strtotime($pbln));
+    
+    $ptgl_pl1 = str_replace('/', '-', $ptgl_pl1);
+    $ptgl_pl2 = str_replace('/', '-', $ptgl_pl2);
+    $ptgl1= date("Y-m-d", strtotime($ptgl_pl1));
+    $ptgl2= date("Y-m-d", strtotime($ptgl_pl2));
+    
+    $pbln_pl1=date("Ym", strtotime($ptgl_pl1));
+    $pbln_pl2=date("Ym", strtotime($ptgl_pl2));
+    
     
     $bolehinput="boleh";
     
     include "../../config/koneksimysqli.php";
     
+    if ( ($pbulan<>$pbln_pl1) OR ($pbulan<>$pbln_pl2) ) {
+        echo "Bulan dan Periode tidak sesuai...."; exit;
+    }
     
     $query = "select idrutin from dbmaster.t_brrutin0 WHERE DATE_FORMAT(bulan,'%Y%m')='$pbulan' AND "
             . " karyawanid='$pidkar' AND IFNULL(stsnonaktif,'')<>'Y' AND idrutin<>'$pid'";//AND kodeperiode='$pkdperiode' 
@@ -26,7 +40,7 @@ if ($pmodule=="cekdatasudahada") {
     if ($ketemu>0) {
         $row= mysqli_fetch_array($tampil);
         $nidrutin=$row['idrutin'];
-        if (!empty($nidrutin)) $bolehinput="Data Sudah Ada, dengan ID : $nidrutin";
+        if (!empty($nidrutin)) $bolehinput="GAGAL.... Periode yang dipilih Tidak bisa tersimpan. karena sudah ada inputan, dengan ID : $nidrutin";
     }
     
     mysqli_close($cnmy);
@@ -86,6 +100,116 @@ if ($pmodule=="cekdatasudahada") {
     }
     
     echo "$bln1, $bln2";
+    
+}elseif ($_GET['module']=="caridataabsentotal"){
+    
+    include "../../config/koneksimysqli.php";
+    include "../../config/fungsi_ubahget_id.php";
+    
+    $pkodeperiode=$_POST['ukode'];
+    $pbln=$_POST['ubulan'];
+    $pkaryawanid=$_POST['ukry'];
+    $pnbln =  date("Ym", strtotime($pbln));
+    
+    
+    $ptotalsemua=0;
+    $pjmlwfh=0;
+    $pjmlwfo=0;
+    $pjmlwfo_val=0;
+    $pjmlwfo_inv=0;
+
+    //cari absensi
+    
+    //if ($pkodeperiode=="2") {
+        include "cari_absen_karyawan_hochc.php";
+        $pjumlahabs = CariAbsensiByKaryawan("inc", $pkaryawanid, $pbln);
+
+        $pjmlwfh=$pjumlahabs[0];
+        $pjmlwfo=$pjumlahabs[1];
+        $pjmlwfo_val=$pjumlahabs[2];
+        $pjmlwfo_inv=$pjumlahabs[3];
+    //}
+    
+    //echo "WFH : $pjmlwfh, WFO : $pjmlwfo, WFO val : $pjmlwfo_val, WFO inval: $pjmlwfo_inv<br/>";
+
+    //END cari absensi
+    
+    ?>
+    <script src="js/inputmask.js"></script>
+    <div class='form-group'>
+        <label class='control-label col-md-3 col-sm-3 col-xs-12' for=''>&nbsp; <span class='required'></span></label>
+        <div class='col-md-4'>
+            <?PHP
+            $pkaryidcode=encodeString($pkaryawanid);
+            $bulan_pilih=encodeString($pnbln);
+            $pviewdataabsen = "<a class='btn btn-warning btn-xs' href='eksekusi3.php?module=showdataabsensi&i=$pkaryidcode&b=$bulan_pilih' target='_blank'>List Absensi</a>";
+            echo $pviewdataabsen;
+            ?>
+        </div>
+    </div>
+
+    <div class='form-group'>
+        <label class='control-label col-md-3 col-sm-3 col-xs-12' for=''>Jumlah WFH <span class='required'></span></label>
+        <div class='col-md-4'>
+            <input type='text' id='e_jmlwfh' name='e_jmlwfh' class='form-control col-md-7 col-xs-12 inputmaskrp2' value='<?PHP echo $pjmlwfh; ?>' readonly>
+        </div>
+    </div>
+
+    <div class='form-group'>
+        <label class='control-label col-md-3 col-sm-3 col-xs-12' for=''>Jumlah WFO (Valid) <span class='required'></span></label>
+        <div class='col-md-4'>
+            <input type='hidden' id='e_jmlwfo' name='e_jmlwfo' class='form-control col-md-7 col-xs-12 inputmaskrp2' value='<?PHP echo $pjmlwfo; ?>' readonly>
+            <input type='text' id='e_jmlwfoval' name='e_jmlwfoval' class='form-control col-md-7 col-xs-12 inputmaskrp2' value='<?PHP echo $pjmlwfo_val; ?>' readonly>
+        </div>
+    </div>
+
+    <div class='form-group'>
+        <label class='control-label col-md-3 col-sm-3 col-xs-12' for='' style="color:red;">Jumlah WFO (Invalid) <span class='required'></span></label>
+        <div class='col-md-4'>
+            <input type='text' id='e_jmlwfoinv' name='e_jmlwfoinv' class='form-control col-md-7 col-xs-12 inputmaskrp2' value='<?PHP echo $pjmlwfo_inv; ?>' readonly>
+        </div>
+    </div>
+
+    <div class='form-group'>
+        <label class='control-label col-md-3 col-sm-3 col-xs-12' for=''>Total Rp. <span class='required'></span></label>
+        <div class='col-md-4'>
+            <input type='text' id='e_totalsemua' name='e_totalsemua' class='form-control col-md-7 col-xs-12 inputmaskrp2' value='<?PHP echo $ptotalsemua; ?>' readonly>
+        </div>
+    </div>
+
+    <?PHP
+    
+    mysqli_close($cnmy);
+    
+}elseif ($_GET['module']=="cariinputandetail"){
+    ?><script src="js/inputmask.js"></script><?PHP
+    include "../../config/koneksimysqli.php";
+    
+    $pstsmobile=$_SESSION['MOBILE'];
+    
+    
+    $pjabatanid =$_POST['ujbt'];
+    $pkaryawanid =$_POST['ukry'];
+    $pidrutin =$_POST['uid'];
+    $pdivisi =$_POST['udivisi'];
+    $pidact =$_POST['uact'];
+    
+    $ptotalsemua=$_POST['utotal'];
+    $pjmlwfh=$_POST['ujmlwfo'];
+    $pjmlwfo=$_POST['ujmlwfo'];
+    $pjmlwfo_val=$_POST['ujmlwfo_val'];
+    $pjmlwfo_inv=$_POST['ujmlwfo_inv'];
+    
+    if ($pstsmobile=="Y") {
+        echo "<br/>&nbsp;";
+        echo "<div style='overflow-x:auto;'>";
+            include "inputdetailmobilehodivchc.php";
+        echo "</div>";
+    }else{
+        include "inputdetailbrhodivchc.php";
+    }
+}elseif ($_GET['module']=="xxxx"){
+    
 }
 
 ?>

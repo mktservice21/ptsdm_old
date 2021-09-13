5d2843e588508b12c06936dbb84e98fc291261c5
myhuspan <huspannasrulloh@gmail.com>
2021-09-13 15:54:26 +0700

    Update inputdetailmobileadmcb.php

--- mysdm/module/budget/bgt_brrutin_admcab/inputdetailmobileadmcb.php
+++ mysdm/module/budget/bgt_brrutin_admcab/inputdetailmobileadmcb.php
@@ -299,9 +299,6 @@
         $preadonly_um="";
         if ($pkodeidbr=="04" AND $pabsenrutin==true) {
             $preadonly_um=" readonly ";
-        }
-        
-        if ($pkodeidbr=="04"){
             //$pnilaireadonly="Readonly";//matikan dulu, kalau sudh ok hidupkan
         }
                 

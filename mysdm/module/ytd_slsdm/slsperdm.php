dbc0338ceb68d687f702825e921321ea3b7ed19c
myhuspan <huspannasrulloh@gmail.com>
2021-09-15 11:28:44 +0700

    Update slsperdm.php

--- mysdm/module/ytd_slsdm/slsperdm.php
+++ mysdm/module/ytd_slsdm/slsperdm.php
@@ -76,13 +76,13 @@
                                                     <?PHP
                                                     echo "<option value=''>-- Pilih --</option>";
                                                     if ($pmyjabatanid=="08") {
-                                                        $query= "select b.karyawanid, b.nama from ms.karyawan b where karyawanid='$pmyidcard' ";
+                                                        $query= "select b.karyawanid, b.nama from hrd.karyawan b where karyawanid='$pmyidcard' ";
                                                     }elseif ($pmyjabatanid=="20") {
-                                                        $query =" select DISTINCT a.karyawanid, b.nama from sls.idm0 a join ms.karyawan b on a.karyawanid=b.karyawanid "
+                                                        $query =" select DISTINCT a.karyawanid, b.nama from sls.idm0 a join hrd.karyawan b on a.karyawanid=b.karyawanid "
                                                                 . " JOIN sls.ism0 c on a.icabangid=c.icabangid where c.karyawanid='$pmyidcard' ";
                                                     }else{
                                                         if ($pmyidcard=="0000000158" OR $pmyidcard=="0000000159") {
-                                                            $query =" select DISTINCT a.karyawanid, b.nama from sls.idm0 a join ms.karyawan b on a.karyawanid=b.karyawanid "
+                                                            $query =" select DISTINCT a.karyawanid, b.nama from sls.idm0 a join hrd.karyawan b on a.karyawanid=b.karyawanid "
                                                                     . " JOIN ms.icabang c on a.icabangid=c.icabangid where 1=1 ";
                                                             if ($pmyidcard=="0000000158") {
                                                                 $query .= " AND c.region='B' ";
@@ -90,7 +90,7 @@
                                                                 $query .= " AND c.region='T' ";
                                                             }
                                                         }else{
-                                                            $query =" select DISTINCT a.karyawanid, b.nama from sls.idm0 a join ms.karyawan b on a.karyawanid=b.karyawanid ";
+                                                            $query =" select DISTINCT a.karyawanid, b.nama from sls.idm0 a join hrd.karyawan b on a.karyawanid=b.karyawanid ";
                                                         }
                                                     }
                                                     $query .=" AND LEFT(b.nama,4) NOT IN ('NN -', 'DR -', 'DM -', 'BDG ', 'OTH.')  "

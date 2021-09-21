a54e1f37ca66f907b6c06b1f18f89051b96d4966
myhuspan <huspannasrulloh@gmail.com>
2021-09-21 21:57:16 +0700

    Create ttd_brdcccabang_.php

---
+++ mysdm/module/budget/bgt_brdcccabang/ttd_brdcccabang_.php
@@ -0,0 +1,296 @@
+
+<?PHP
+    $now=date("m/d/Y/h:i:s");
+    $now=date("Ymd");
+    ?>
+
+    <style type="text/css">
+
+        #signatureparent {
+            color:darkblue;
+            color:#000;
+            background-color:darkgrey;
+            /*max-width:600px;*/
+            padding:20px;
+            /*width:400px;*/
+        }
+        /*This is the div within which the signature canvas is fitted*/
+        #signature {
+            border: 2px dotted black;
+            background-color:lightgrey;
+        }
+        .btn_sig{
+            font-size: 16px;
+            background: linear-gradient(#ffbc00 5%, #ffdd7f 100%);
+            border: 1px solid #e5a900;
+            color: #4E4D4B;
+            font-weight: bold;
+            cursor: pointer;
+            width: 100px;
+            border-radius: 5px;
+            padding: 5px 0;
+            outline: none;
+            margin-top: 5px;
+            margin-left: 1%;
+            margin-bottom: 1%;
+        }
+        .btn_sig:hover{
+            background: linear-gradient(#ffdd7f 5%, #ffbc00 100%);
+        }
+    </style>
+
+<div>
+    <div id="content"  class="main">
+        
+        
+        <div><p hidden>Tampil :</p><div id="displayarea"></div></div>
+
+
+        <div id="signatureparent">
+            <div id="signature"></div>
+        </div>
+        <br/>
+        <div id="tools"></div>
+
+    </div>
+    <div id="scrollgrabber"></div>
+</div>
+<script>
+/*  @preserve
+jQuery pub/sub plugin by Peter Higgins (dante@dojotoolkit.org)
+Loosely based on Dojo publish/subscribe API, limited in scope. Rewritten blindly.
+Original is (c) Dojo Foundation 2004-2010. Released under either AFL or new BSD, see:
+http://dojofoundation.org/license for more information.
+*/
+(function($) {
+        var topics = {};
+        $.publish = function(topic, args) {
+            if (topics[topic]) {
+                var currentTopic = topics[topic],
+                args = args || {};
+
+                for (var i = 0, j = currentTopic.length; i < j; i++) {
+                    currentTopic[i].call($, args);
+                }
+            }
+        };
+        $.subscribe = function(topic, callback) {
+            if (!topics[topic]) {
+                topics[topic] = [];
+            }
+            topics[topic].push(callback);
+            return {
+                "topic": topic,
+                "callback": callback
+            };
+        };
+        $.unsubscribe = function(handle) {
+            var topic = handle.topic;
+            if (topics[topic]) {
+                var currentTopic = topics[topic];
+
+                for (var i = 0, j = currentTopic.length; i < j; i++) {
+                    if (currentTopic[i] === handle.callback) {
+                        currentTopic.splice(i, 1);
+                    }
+                }
+            }
+        };
+})(jQuery);
+</script>
+<?php
+include('tanda_tangan_base64/src/mobile.php');
+?>
+
+
+
+    <script src="tanda_tangan_baru/src/jSignature.js"></script>
+    <script src="tanda_tangan_baru/src/plugins/jSignature.CompressorBase30.js"></script>
+    <script src="tanda_tangan_baru/src/plugins/jSignature.CompressorSVG.js"></script>
+    <script src="tanda_tangan_baru/src/plugins/jSignature.UndoButton.js"></script> 
+    <!--<script src="tanda_tangan_baru/src/plugins/signhere/jSignature.SignHere.js"></script> -->
+    
+    
+<script>
+function ReloadTandaTangan(){
+        // This is the part where jSignature is initialized.
+        //var $sigdiv = $("#signature").jSignature({'UndoButton':true})
+        //var $sigdiv = $("#signature").jSignature({ 'UndoButton': true, 'width': 370, 'height': 400 })
+        <?PHP
+        if(mobile_device_detect(true,true,true,true,false,false)) {
+            ?> var $sigdiv = $("#signature").jSignature({ 'UndoButton': true, 'width': 270, 'height': 300 }) <?PHP
+        }else{
+            ?> var $sigdiv = $("#signature").jSignature({ 'UndoButton': true, 'width': 370, 'height': 400 }) <?PHP
+        }
+        ?>
+        
+        
+        // All the code below is just code driving the demo. 
+        , $tools = $('#tools')
+        , $extraarea = $('#displayarea')
+        , pubsubprefix = 'jSignature.demo.'
+
+
+        var export_plugins = $sigdiv.jSignature('listPlugins','export')
+        , chops = ['<span><b></b></span>','']
+        , name
+        for(var i in export_plugins){
+            if (export_plugins.hasOwnProperty(i)){
+                    name = export_plugins[i]
+                    chops.push('')
+
+            }
+        }
+        chops.push('')
+
+        $(chops.join('')).bind('change', function(e){
+            if (e.target.value !== ''){
+                    var data = $sigdiv.jSignature('getData', e.target.value)
+                    $.publish(pubsubprefix + 'formatchanged')
+                    if (typeof data === 'string'){
+                            $('textarea', $tools).val(data)
+                    } else if($.isArray(data) && data.length === 2){
+                            $('textarea', $tools).val(data.join(','))
+                            $.publish(pubsubprefix + data[0], data);
+                    } else {
+                            try {
+                                    $('textarea', $tools).val(JSON.stringify(data))
+                            } catch (ex) {
+                                    $('textarea', $tools).val('Not sure how to stringify this, likely binary, format.')
+                            }
+                    }
+            }
+        }).appendTo($tools)
+
+        $('<input type="button" value="Simpan" class="btn btn-success">').bind('click', function(){
+                Tampilkan("image");
+        }).appendTo($tools)
+
+        $('<input type="button" value="Reset" class="btn btn-default">').bind('click', function(e){
+                $sigdiv.jSignature('reset')
+        }).appendTo($tools)
+        
+
+        $('<div hidden><textarea style="width:100%;height:7em;" name="txtgambar" id="txtgambar"></textarea></div>').appendTo($tools)
+
+        $.subscribe(pubsubprefix + 'formatchanged', function(){
+                $extraarea.html('')
+        })
+
+        function Tampilkan(e){
+            if (e !== ''){                            
+                var data = $sigdiv.jSignature('getData', e)
+                $.publish(pubsubprefix + 'formatchanged')
+                if (typeof data === 'string'){
+                    $('textarea', $tools).val(data)
+                } else if($.isArray(data) && data.length === 2){
+                        $('textarea', $tools).val(data.join(','))
+                        $.publish(pubsubprefix + data[0], data);
+                } else {
+                        try {
+                                $('textarea', $tools).val(JSON.stringify(data))
+                        } catch (ex) {
+                                $('textarea', $tools).val('Not sure how to stringify this, likely binary, format.')
+                        }
+                }
+            }
+        }
+        $.subscribe(pubsubprefix + 'image/png;base64', function(data) {
+            
+            
+            var iid = document.getElementById('e_id').value;
+            var ikry = document.getElementById('e_idcarduser').value;
+            var icabang = document.getElementById('cb_cabang').value;
+            var etotsem = document.getElementById('e_jmlusulan').value;
+            var ebank = document.getElementById('cb_bankreal').value;
+            var enmbank = document.getElementById('e_nmbankreal').value;
+            var enorek = document.getElementById('e_norekbankreal').value;
+            
+            var chkjns1=document.getElementById("chksesuai").checked;
+            
+            if (ikry=="") {
+                alert("Pembuat masih kosong...");
+                return false;
+            }
+            
+            if (icabang=="") {
+                alert("Cabang masih kosong...");
+                return false;
+            }
+            
+            if (chkjns1==true) {
+            }else{
+                var inmreal = document.getElementById('e_nmrealasi').value;
+                if (inmreal=="") {
+                    alert("Nama relasi realisasi harus diisi...");
+                    return false;
+                }
+            }
+            
+            
+            if (ebank=="" || enmbank=="" || enorek=="") {
+                alert("Bank, Nama dan No Rekening harus diisi...");
+                return false;
+            }
+            
+            
+            if (etotsem=="") etotsem="0";
+            
+            if (parseFloat(etotsem)==0) {
+                alert("Jumlah Permintaan Masih Kosong....");
+                return 0;
+            }
+    
+            
+            $.ajax({
+                type:"post",
+                url:"module/budget/viewdatabgt.php?module=cekdatasudahada",
+                data:"uid="+iid+"&ukry="+ikry,
+                success:function(data){
+                    //var tjml = data.length;
+                    //alert(data);
+                    //return false;
+                    
+                    if (data=="boleh") {
+    
+                        //simpan data ke DB
+                        var cmt = confirm('pastikan tanda tangan terisi....!!! jika sudah terisi klik OK');
+                        if (cmt == false) {
+                            return false;
+                        }            
+
+                        var uttd = data;//gambarnya
+
+                        var myurl = window.location;
+                        var urlku = new URL(myurl);
+                        var module = urlku.searchParams.get("module");
+                        var idmenu = urlku.searchParams.get("idmenu");
+                        var act = urlku.searchParams.get("act");
+
+                        var ket="input";
+                        if (act=="editdata") ket="update";
+
+                        document.getElementById("form_data1").action = "module/budget/bgt_brdcccabang/aksi_brdcccabang.php?module="+module+"&act="+ket+"&idmenu="+idmenu;
+                        document.getElementById("form_data1").submit();
+            
+                    }else{
+                        alert(data);
+                    }
+                }
+            });
+            
+            
+        });
+}
+
+$(document).ready(function() {
+    ReloadTandaTangan();
+})
+
+</script>
+
+<br/>&nbsp;
+<!--
+<input class='btn btn-default' type='button' name='buttonreload' value='Reload Tanda Tangan' onClick="ReloadTandaTangan()">
+<div style="color:red;">*) jika AREA tanda tangan tidak muncul klik tombol <b>Reload Tanda Tangan</b></div>
+-->

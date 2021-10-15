c7176db40ae9b8eb61231c80589a02815a3f640c
myhuspan <huspannasrulloh@gmail.com>
2021-10-15 08:31:38 +0700

    Update simpandataproses.php

--- mysdm/module/sls_pdatejkthsopt/simpandataproses.php
+++ mysdm/module/sls_pdatejkthsopt/simpandataproses.php
@@ -17,6 +17,8 @@ if ($pmodule=="simpandataproses") {
     $ptgl3 = date('Y-m-t', strtotime($nbln3));
     $ptgl4 = date('Y-m-01', strtotime($nbln4));
     
+    $pthnproses = date('Y', strtotime($nbln1));
+    
     //$berhasil="$ptgl1, $ptgl2, $ptgl3, $ptgl4";
     
     
@@ -25,6 +27,25 @@ if ($pmodule=="simpandataproses") {
     
     $query = "CALL sls.updatejkthospital('$ptgl1','$ptgl2','$ptgl3','$ptgl4')";
     mysqli_query($cnms, $query); $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo $erropesan; mysqli_close($cnms); exit; }
+    
+    
+    //PROSES BUDGET VS EXPEN
+    
+        $query = "CALL dbproses.0_proses_all_gl('$pthnproses')";
+        mysqli_query($cnms, $query); $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo $erropesan; mysqli_close($cnms); exit; }
+
+        $query = "CALL dbproses.proses_budget('$pthnproses')";
+        mysqli_query($cnms, $query); $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo $erropesan; mysqli_close($cnms); exit; }
+
+        $query = "CALL dbproses.proses_budget_expensis('$pthnproses')";
+        mysqli_query($cnms, $query); $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo $erropesan; mysqli_close($cnms); exit; }
+
+        $query = "CALL dbproses.proses_cabang('$pthnproses')";
+        mysqli_query($cnms, $query); $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo $erropesan; mysqli_close($cnms); exit; }
+
+        $query = "CALL dbproses.proses_coa('$pthnproses')";
+        mysqli_query($cnms, $query); $erropesan = mysqli_error($cnms); if (!empty($erropesan)) { echo $erropesan; mysqli_close($cnms); exit; }
+    
     
     mysqli_close($cnms);
     $berhasil="berhasil....";

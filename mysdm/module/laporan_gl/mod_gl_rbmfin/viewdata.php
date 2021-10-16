9f9e529c485d1fccc0f88816cf88adc2ca888a92
myhuspan <huspannasrulloh@gmail.com>
2021-10-16 10:18:44 +0700

    Update viewdata.php

--- mysdm/module/laporan_gl/mod_gl_rbmfin/viewdata.php
+++ mysdm/module/laporan_gl/mod_gl_rbmfin/viewdata.php
@@ -5,7 +5,92 @@ $pmodule="";
 if (isset($_GET['module'])) $pmodule=$_GET['module'];
 
 
-if ($pmodule=="viewcoadivisichk"){
+if ($pmodule=="viewdivisibytipe"){
+    
+    $fkaryawan=$_SESSION['IDCARD'];
+    $fstsadmin=$_SESSION['STSADMIN'];
+    $flvlposisi=$_SESSION['LVLPOSISI'];
+    $fdivisi=$_SESSION['DIVISI'];
+    $fgroupid=$_SESSION['GROUP'];
+    
+    include "../../../config/koneksimysqli.php";
+    
+    $prpttipe=$_POST['utipe'];
+    
+    if ($prpttipe=="BMB") {
+        echo "<option value='ETH' selected>ETHICAL</option>";
+        echo "<option value='OTC'>CHC</option>";
+        echo "<option value='HO'>HO</option>";
+        echo "<option value='EAGLE'>EAGLE</option>";
+        echo "<option value='PEACO'>PEACOCK</option>";
+        echo "<option value='PIGEO'>PIGEON</option>";
+    }else{
+        
+        $ppilihdivisi="";
+        if ($fkaryawan=="0000000148") $ppilihdivisi = "HO";
+        elseif ($fkaryawan=="0000001043xx") $ppilihdivisi = "EAGLE";
+        else{
+            if ($fdivisi=="OTC" OR $fdivisi=="CHC") {
+                $ppilihdivisi="OTC";
+            }
+        }
+    
+        $pbukasemua=false;
+        $pbukdivisiall=false;
+        $pnot_otc=false;
+        if ($fgroupid=="1" OR $fgroupid=="25" OR $fgroupid=="24" OR $fgroupid=="2" OR $fgroupid=="22" OR $fgroupid=="46" OR $fgroupid=="50") {
+            $pbukasemua=true;
+            $pbukdivisiall=true;
+        }
+
+
+        if (empty($ppilihdivisi)) {
+            $pbukdivisiall=true;
+        }
+
+        if ($fgroupid=="28") {
+            $pnot_otc=true;
+        }
+
+        if ($fkaryawan=="0000001043") $pbukdivisiall=true;
+    
+        $query = "select DivProdId from MKT.divprod WHERE br='Y' AND DivProdId<>'OTHER' ";
+        if ($pbukdivisiall==false) {
+            $query .=" AND DivProdId='$ppilihdivisi' ";
+        }
+
+        if ($pnot_otc == true) {
+            $query .=" AND DivProdId NOT IN ('CHC', 'OTC') ";
+        }
+        $query .=" order by DivProdId";
+        $tampil = mysqli_query($cnmy, $query);
+        if ($pbukdivisiall==true) echo "<option value='' selected>All</option>";
+        while ($z= mysqli_fetch_array($tampil)) {
+            $pgetdivisi=$z['DivProdId'];
+
+            $pdivisinm=$pgetdivisi;
+            if ($pgetdivisi=="CAN") $pdivisinm="CANARY";
+            if ($pgetdivisi=="PIGEO") $pdivisinm="PIGEON";
+            if ($pgetdivisi=="PEACO") $pdivisinm="PEACOCK";
+            if ($pgetdivisi=="OTC") $pdivisinm="CHC";
+
+            if ($pgetdivisi==$ppilihdivisi)
+                echo "<option value='$pgetdivisi' selected>$pdivisinm</option>";
+            else
+                echo "<option value='$pgetdivisi'>$pdivisinm</option>";
+        }
+
+        if ($fgroupid=="1" OR $fgroupid=="24" OR $fgroupid=="61" OR $fgroupid=="28" OR $fgroupid=="25") {
+            echo "<option value='ETH'>ETHICAL</option>";
+        }
+        
+    }
+    
+    
+    mysqli_close($cnmy);
+    
+    
+}elseif ($pmodule=="viewcoadivisichk"){
     $fgroupid=$_SESSION['GROUP'];
     
     include "../../../config/koneksimysqli.php";

cf293f80e0c3a80af5b2fb8e93696ce73e806c5c
myhuspan <huspannasrulloh@gmail.com>
2021-10-29 13:26:42 +0700

    Update tambah_dayactivity.php

--- mysdm/module/dkd/dkd_wekvisitactreal/tambah_dayactivity.php
+++ mysdm/module/dkd/dkd_wekvisitactreal/tambah_dayactivity.php
@@ -165,7 +165,7 @@ $pnamajabatan=$nr['nama'];
                                     </div>
                                 </div>
 
-                                <div class='form-group'>
+                                <div hidden class='form-group'>
                                     <label class='control-label col-md-3 col-sm-3 col-xs-12' for=''>Compl <span class='required'></span></label>
                                     <div class='col-xs-8'>
                                         <input type='text' id='e_compl' name='e_compl' class='form-control col-md-7 col-xs-12' maxlength="150" value='<?PHP echo $pcompl; ?>'>

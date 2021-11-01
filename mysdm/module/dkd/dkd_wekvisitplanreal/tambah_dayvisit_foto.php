976f5e928fade87bc5b04cec2a3c583aa6199a32
myhuspan <huspannasrulloh@gmail.com>
2021-11-01 16:10:45 +0700

    Update tambah_dayvisit_foto.php

--- mysdm/module/dkd/dkd_wekvisitplanreal/tambah_dayvisit_foto.php
+++ mysdm/module/dkd/dkd_wekvisitplanreal/tambah_dayvisit_foto.php
@@ -494,6 +494,13 @@ $pnamajabatan=$nr['nama'];
                                 </div>
 
                                 
+                                <div  class='form-group'>
+                                    <label class='control-label col-md-3 col-sm-3 col-xs-12' for=''>Jabatan <span class='required'></span></label>
+                                    <div class='col-md-6 col-sm-6 col-xs-12'>
+                                        <input type='text' id='e_nmjbt' name='e_nmjbt' class='form-control col-md-7 col-xs-12' value='<?PHP echo $pnamajabatan; ?>' Readonly>
+                                    </div>
+                                </div>
+                                
                                 <div  id="div_atasan">
                                     
                                     <div class='form-group'>

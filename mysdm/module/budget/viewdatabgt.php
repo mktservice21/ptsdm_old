8384386d997730db75942162610789a7e9f1904a
myhuspan <huspannasrulloh@gmail.com>
2021-11-01 16:10:20 +0700

    Update viewdatabgt.php

--- mysdm/module/budget/viewdatabgt.php
+++ mysdm/module/budget/viewdatabgt.php
@@ -793,6 +793,41 @@ if ($pmodule=="viewdatakrybuat") {
     mysqli_close($cnms); exit;
 }elseif ($pmodule=="cekdatasudahada") {
     echo "boleh"; exit;
+}elseif ($pmodule=="caridatacabangbypengajuan") {
+    include "../../config/koneksimysqli.php";
+    
+    $piddivipl=$_POST['udivpl'];
+    
+    $icabangid="";
+    if (!empty($_SESSION['BGTUPDCAB'])) $icabangid=$_SESSION['BGTUPDCAB'];
+    
+    $pketdivipl="ETHICAL";
+    if ($piddivipl=="OTC" OR $piddivipl=="OT" OR $piddivipl=="CHC") {
+        $pketdivipl="CHC";
+        
+        $query = "select icabangid_o as icabangid, nama as nama From dbmaster.v_icabang_o 
+            WHERE IFNULL(aktif,'')<>'N' ";//AND icabangid_o NOT IN ('0000000001') 
+        $query .= " ORDER BY nama";
+    }else{
+        $query = "select icabangid as icabangid, nama as nama From MKT.icabang
+            WHERE IFNULL(aktif,'')<>'N' ";
+        $query .= " AND LEFT(nama,5) NOT IN ('OTC -', 'PEA -')";
+        $query .= " ORDER BY nama";
+    }
+    
+    echo "<option value='' selected>--Pilih--</option>";
+    $tampil = mysqli_query($cnmy, $query);
+    while ($z= mysqli_fetch_array($tampil)) {
+        $pcabangid=$z['icabangid'];
+        $pcabnm=$z['nama'];
+        $pcabid=(INT)$pcabangid;
+        if ($pcabangid==$icabangid)
+            echo "<option value='$pcabangid' selected>$pcabnm ($pcabid) $pketdivipl</option>";
+        else
+            echo "<option value='$pcabangid'>$pcabnm ($pcabid) $pketdivipl</option>";
+    }
+    
+    mysqli_close($cnmy);
 }elseif ($pmodule=="x") {
     
 }

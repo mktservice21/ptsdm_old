c4e35e72125cc667e2a5b0f20008cde53e6ebaaf
myhuspan <huspannasrulloh@gmail.com>
2021-11-07 21:04:54 +0700

    Update simpandataeditbrrutin.php

--- mysdm/module/laporan/mod_lap_rincianbrrutin/simpandataeditbrrutin.php
+++ mysdm/module/laporan/mod_lap_rincianbrrutin/simpandataeditbrrutin.php
@@ -23,12 +23,47 @@ if (($module=='laprincianbrrutin' OR $module=='laprutinrinciotc') AND $act=='inp
     $pidno=$_POST['uinoid'];
     $pcoakode=$_POST['ucoa'];
     
-    //$berhasil = "$pnourut, $pidrtnbr, COA : $pcoakode";
+    $pkryid=$_POST['ukryid'];
+    $pidrtn=$_POST['uidrtn'];
+    $pbrid=$_POST['ubrid'];
+    $pcoalama=$_POST['ucoalama'];
     
-    if (!empty($pnourut) AND !empty($pcoakode)) {
+    //$berhasil = "$pkryid, $pidrtn, $pbrid, COA : $pcoakode";
+    
+    if (!empty($pkryid) AND !empty($pidrtn) AND !empty($pbrid) AND !empty($pcoakode)) {
         
-        $query = "UPDATE dbmaster.t_brrutin1 SET coa='$pcoakode' WHERE nourut='$pnourut' AND idrutin='$pidrtnbr' LIMIT 1";
+        $query = "select c.DIVISI2, c.COA1, d.NAMA1, b.COA2, c.NAMA2, a.COA3, b.NAMA3, a.COA4, a.NAMA4 from dbmaster.coa_level4 as a JOIN dbmaster.coa_level3 as b on a.COA3=b.COA3 JOIN dbmaster.coa_level2 as c on b.COA2=c.COA2 join dbmaster.coa_level1 as d on c.COA1=d.COA1 WHERE a.COA4='$pcoakode'";
+        $tampil=mysqli_query($cnmy, $query);
+        $row= mysqli_fetch_array($tampil);
+        $pcoalvl1=$row['COA1'];
+        $pcoanm1=$row['NAMA1'];
+        $pcoalvl2=$row['COA2'];
+        $pcoanm2=$row['NAMA2'];
+        $pcoalvl3=$row['COA3'];
+        $pcoanm3=$row['NAMA3'];
+        $pcoanm4=$row['NAMA4'];
+        $pcoadivisi=$row['DIVISI2'];
+        
+        //$berhasil = "$pkryid, $pidrtn, $pbrid, COA : $pcoalvl1 ($pcoanm1), $pcoalvl2 ($pcoanm2), $pcoalvl3 ($pcoanm3), $pcoakode ($pcoanm4) - $pcoadivisi, coa lama : $pcoalama";
+        
+        $query = "UPDATE dbmaster.t_brrutin1 SET coa='$pcoakode' WHERE nobrid='$pbrid' AND idrutin='$pidrtn' AND coa='$pcoalama' LIMIT 1";
         mysqli_query($cnmy, $query); $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo "Error Update"; exit; }
+        
+        
+        $query = "UPDATE dbproses.proses_expenses SET coa4='$pcoakode' WHERE kodeinput='5' AND idkodeinput='$pidrtn' AND kodeid='$pbrid' AND coa4='$pcoalama' LIMIT 1";
+        mysqli_query($cnmy, $query); $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo "Error Update proses exp"; exit; }
+        
+        
+        $query = "UPDATE dbmaster.t_proses_data_bm SET divisi_coa='$pcoadivisi', "
+                . " coa='$pcoakode', nama_coa='$pcoanm4', "
+                . " coa_edit='$pcoakode', coa_nama_edit='$pcoanm4', "
+                . " coa2='$pcoalvl2', nama_coa2='$pcoanm2', "
+                . " coa_edit2='$pcoalvl2', coa_nama_edit2='$pcoanm2',  "
+                . " coa3='$pcoalvl3', nama_coa3='$pcoanm3', "
+                . " coa_edit3='$pcoalvl3', coa_nama_edit3='$pcoanm3' WHERE kodeinput='F' AND idkodeinput='$pidrtn' AND nobrid_r='$pbrid' AND coa='$pcoalama' LIMIT 1";
+        mysqli_query($cnmy, $query); $erropesan = mysqli_error($cnmy); if (!empty($erropesan)) { echo "Error Update proses exp2"; exit; }
+        
+        
         
         $berhasil="berhasil";
         

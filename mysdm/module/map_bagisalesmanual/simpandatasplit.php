f5dd85283864a789f3ea3d7be0a4be4edc78679f
myhuspan <huspannasrulloh@gmail.com>
2021-11-11 16:18:25 +0700

    Update simpandatasplit.php

--- mysdm/module/map_bagisalesmanual/simpandatasplit.php
+++ mysdm/module/map_bagisalesmanual/simpandatasplit.php
@@ -83,10 +83,9 @@
             $row=mysqli_fetch_array($tampil);
             $pqtyfaktur=$row['qbeli'];
             
-            $query2 = "select sum(b.qty) as qtysudh "
-                    . " from MKT.msales0 as a LEFT "
-                    . " JOIN MKT.msales1 as b on a.nomsales=b.nomsales WHERE "
-                    . " a.distid='$piddist' and a.ecabangid='$pidecab' and a.fakturid='$pfakturid' AND left(a.tgl,7)='$pbulan' AND b.iprodid='$pidproduk'";
+            $query2 = "select sum(a.qty) as qtysudh "
+                    . " from MKT.msales_new as a WHERE "
+                    . " a.distid='$piddist' and a.ecabangid='$pidecab' and a.fakturid='$pfakturid' AND left(a.tgl,7)='$pbulan' AND a.iprodid='$pidproduk'";
             $tampil2= mysqli_query($cnms, $query2);
             $row2=mysqli_fetch_array($tampil2);
             $pqtysdhsplit=$row2['qtysudh'];
@@ -117,14 +116,11 @@
             
             //echo "$nomsales"; mysqli_close($cnms); exit;
             if ($result) {
-                $query = "INSERT INTO MKT.msales0 (nomsales,icabangid,tgl,distid,ecabangid,fakturid,icustid,ecustid,user1,src) 
-                        VALUES ('$nomsales','$pidcabang','$ptgljual','$piddist','$pidecab','$pfakturid','$pidicust','$pecustid','$puserid','U')";
+                $query = "INSERT INTO MKT.msales_new (nomsales,icabangid,tgl,distid,ecabangid,fakturid,icustid,ecustid,user1,src, 
+                    iprodid,areaid,qty) 
+                        VALUES ('$nomsales','$pidcabang','$ptgljual','$piddist','$pidecab','$pfakturid','$pidicust','$pecustid','$puserid','U', "
+                        . " '$pidproduk','$pidarea','$pqtysplitinput')";
                 $result2 = mysqli_query($cnms, $query);
-                if ($result2) {
-                    $query = "INSERT INTO MKT.msales1 (nomsales,iprodid,areaid,qty,src) 
-                            VALUES ('$nomsales','$pidproduk','$pidarea','$pqtysplitinput','U')";
-                    $result3 = mysqli_query($cnms, $query);
-                }
             }
             
             mysqli_close($cnms);
@@ -138,13 +134,8 @@
             $pidprod=$_POST['uproduk'];
             $pfakturid=$_POST['ufakturid'];
             
-            $query = "DELETE FROM MKT.msales1 WHERE nomsales IN (select nomsales FROM MKT.msales0 WHERE "
-                    . "nomsales='$pkodeinput' AND fakturid='$pfakturid') AND nomsales='$pkodeinput' AND iprodid='$pidprod' LIMIT 1";
+            $query = "DELETE FROM MKT.msales_new WHERE nomsales='$pkodeinput' AND fakturid='$pfakturid' AND iprodid='$pidprod' AND  LIMIT 1";
             $result = mysqli_query($cnms, $query);
-            if ($result) {
-                $query2 = "DELETE FROM MKT.msales0 WHERE nomsales='$pkodeinput' AND fakturid='$pfakturid' LIMIT 1";
-                $result2 = mysqli_query($cnms, $query2);
-            }
             mysqli_close($cnms);
             echo "berhasil";
             exit;
@@ -161,7 +152,7 @@
             $pfakturid=$_POST['ufakturid'];
             
             $pnoidslsinput="";
-            $query = "select distinct noMsales FROM MKT.msales0 where distid='$pdistid' and fakturid='$pfakturid' and tgl='$ptgljual' and ecabangid='$pcabd' and ecustid='$pecustid'";
+            $query = "select distinct noMsales FROM MKT.msales_new where distid='$pdistid' and fakturid='$pfakturid' and tgl='$ptgljual' and ecabangid='$pcabd' and ecustid='$pecustid'";
             $tampil= mysqli_query($cnms, $query);
             while ($row= mysqli_fetch_array($tampil)) {
                 if (!empty($row['noMsales'])) $pnoidslsinput .="'".$row['noMsales']."',";
@@ -170,8 +161,8 @@
             
             
             $pnoidslsinput2="";
-            $query2 = "select distinct noMsales as noMsales FROM MKT.msales1 where noMsales IN $pnoidslsinput AND iprodid='$pidprod' and nomsales IN 
-                (select distinct IFNULL(nomsales,'') FROM MKT.msales0 where distid='$pdistid' and fakturid='$pfakturid' and tgl='$ptgljual' and ecabangid='$pcabd' and ecustid='$pecustid')";
+            $query2 = "select distinct noMsales as noMsales FROM MKT.msales_new where noMsales IN $pnoidslsinput AND iprodid='$pidprod' and "
+                    . " distid='$pdistid' and fakturid='$pfakturid' and tgl='$ptgljual' and ecabangid='$pcabd' and ecustid='$pecustid'";
             $tampil2= mysqli_query($cnms, $query2);
             while ($row2= mysqli_fetch_array($tampil2)) {
                 if (!empty($row2['noMsales'])) $pnoidslsinput2 .="'".$row2['noMsales']."',";
@@ -185,14 +176,10 @@
                 //echo "$pnoidslsinput2"; 
                 //echo "$pdistid, $pcabd, $pbln, $ptgljual, brgid : $pbrgid - $pidprod, faktur : $pfakturid, ecustid : $pecustid"; 
                 
-                $query = "DELETE FROM MKT.msales1 WHERE nomsales IN $pnoidslsinput2 AND iprodid='$pidprod' AND nomsales IN "
-                        . "(select distinct IFNULL(nomsales,'') FROM MKT.msales0 where distid='$pdistid' and fakturid='$pfakturid' and tgl='$ptgljual' and ecabangid='$pcabd' and ecustid='$pecustid')";
+                $query = "DELETE FROM MKT.msales_new WHERE nomsales IN $pnoidslsinput2 AND iprodid='$pidprod' AND  "
+                        . " distid='$pdistid' and fakturid='$pfakturid' and tgl='$ptgljual' and ecabangid='$pcabd' and ecustid='$pecustid'";
                 //echo $query; exit;
                 $result = mysqli_query($cnms, $query);
-                if ($result) {
-                    $query2 = "DELETE FROM MKT.msales0 WHERE noMsales IN $pnoidslsinput2 AND distid='$pdistid' and fakturid='$pfakturid' and tgl='$ptgljual' and ecabangid='$pcabd' and ecustid='$pecustid'";
-                    $result2 = mysqli_query($cnms, $query2);
-                }
                 
                 
                 mysqli_close($cnms);
@@ -218,15 +205,9 @@
             //echo "$pdistid, $pcabd, $pbln, $ptgljual, faktur : $pfakturid, ecustid : $pecustid";  exit;
             
             if (!empty($pfakturid)) {
-                $query = "DELETE FROM MKT.msales1 WHERE nomsales IN "
-                        . " (select DISTINCT IFNULL(nomsales,'') from MKT.msales0 where distid='$pdistid' and "
-                        . " fakturid='$pfakturid' and tgl='$ptgljual' and ecabangid='$pcabd' and ecustid='$pecustid')";
+                $query = "DELETE FROM MKT.msales_new WHERE distid='$pdistid' and "
+                        . " fakturid='$pfakturid' and tgl='$ptgljual' and ecabangid='$pcabd' and ecustid='$pecustid'";
                 $result = mysqli_query($cnms, $query);
-                if ($result) {
-                    $query2 = "DELETE FROM MKT.msales0 WHERE distid='$pdistid' and "
-                            . " fakturid='$pfakturid' and tgl='$ptgljual' and ecabangid='$pcabd' and ecustid='$pecustid'";
-                    $result2 = mysqli_query($cnms, $query2);
-                }
                 
                 mysqli_close($cnms);
                 echo "berhasil";

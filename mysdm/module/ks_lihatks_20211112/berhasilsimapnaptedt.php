a0c4c9a51804ec03f1bd7d7d545b5ee8b48f4e97
myhuspan <huspannasrulloh@gmail.com>
2021-11-12 13:36:12 +0700

    Create berhasilsimapnaptedt.php

---
+++ mysdm/module/ks_lihatks_20211112/berhasilsimapnaptedt.php
@@ -0,0 +1,14 @@
+<HTML>
+<head>
+  <title>Berhasil Simpan Edit Apotik KS</title>
+    <meta http-equiv="Expires" content="Mon, 01 Jan 2030 1:00:00 GMT">
+    <meta http-equiv="Pragma" content="no-cache">
+    <?php header("Cache-Control: no-cache, must-revalidate"); ?>
+    <link rel="shortcut icon" href="images/icon.ico" />
+</head>
+
+
+<BODY class="nav-md">
+    data berhasil disimpan, silakan tutup halaman ini...!!!
+</BODY>
+</HTML>
